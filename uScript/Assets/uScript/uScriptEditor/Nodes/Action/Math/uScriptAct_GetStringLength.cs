261d75f6ff87d299d1c068a32260d3185506239d
Scott Blinn <scott@detoxstudios.com>
2011-03-28 07:51:03 +0000

    Added friendly name attributes to nodes
    uScriptAct_GetStringLength.cs
    Added friendly attribute info to this node as a test for the new commit auto-email I set up (may as well get actual work done for the test!).
    
    Please let me know if you would like me to change anything regarding these automated emails. I hope you like them!
    
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@69 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript/Assets/uScript/uScriptEditor/Nodes/Action/Math/uScriptAct_GetStringLength.cs
+++ uScript/Assets/uScript/uScriptEditor/Nodes/Action/Math/uScriptAct_GetStringLength.cs
@@ -1,16 +1,25 @@
 // uScript Action Node
-// (C) 2010 Detox Studios LLC
+// (C) 2011 Detox Studios LLC
 // Desc: Returns the number of characters in a string as a float, integer, and string.
 
 using UnityEngine;
 using System.Collections;
 
+[NodePath("Action/Math")]
+[NodeLicense("http://www.detoxstudios.com/legal/eula.html")]
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip( "Returns the number of characters in a string as a float, integer, and string.")]
+[NodeDescription("Returns the number of characters in a string as a float, integer, and string.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://uscript.net/manual/node_nodoc.html")]
+
+[FriendlyName("Get String Length")]
 public class uScriptAct_GetStringLength : uScriptLogic
 {
 
    public bool Out { get { return true; } }
 
-   public void In(string Target, out int IntValue, out float FloatValue, out string StringValue)
+   public void In(string Target, [FriendlyName("Int Value")] out int IntValue, [FriendlyName("Float Value")] out float FloatValue, [FriendlyName("String Value")] out string StringValue)
    {
 
       int m_CountInt;
