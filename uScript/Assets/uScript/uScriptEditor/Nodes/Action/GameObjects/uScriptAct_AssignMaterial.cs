7fb78270664c17cfacbbe47d415726be4873faa5
Scott Blinn <scott@detoxstudios.com>
2011-04-10 06:14:08 +0000

    Created a new node to assign materials to GameObjects on a specified material channel.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@201 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript/Assets/uScript/uScriptEditor/Nodes/Action/GameObjects/uScriptAct_AssignMaterial.cs
@@ -0,0 +1,136 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+// Desc: Assigns the specified Material (by name) to the GameObject on the specifed material channel.
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Action/GameObjects")]
+[NodeLicense("http://www.detoxstudios.com/legal/eula.html")]
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Assigns the specified Material to the GameObject.")]
+[NodeDescription("Assigns the specified Material (by name) to the GameObject on the specifed material channel.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://uscript.net/manual/node_nodoc.html")]
+
+[FriendlyName("Assign Material")]
+public class uScriptAct_AssignMaterial : uScriptLogic
+{
+   private Material m_NewMaterial;
+
+   public bool Out { get { return true; } }
+
+   public void In(GameObject[] Target, string materialName, string resourcePath, int MatChannel)
+   {
+      
+
+      //Get the Material
+      try
+      {
+         m_NewMaterial = Resources.Load(GetFullPath(materialName, resourcePath), typeof(Material)) as Material;
+      }
+      catch (System.Exception e)
+      {
+         uScriptDebug.Log("(Node = Assign Material) Could not load the specified material. Check your material name and path.\nError output: " + e.ToString(), uScriptDebug.Type.Error);
+      }
+
+      foreach (GameObject tmpTarget in Target)
+      {
+         try
+         {
+            // Get the materials on the Target
+            Material[] tmpMaterials = tmpTarget.renderer.materials;
+
+            tmpMaterials[MatChannel] = m_NewMaterial;
+            tmpTarget.renderer.materials = tmpMaterials;
+         }
+         catch (System.Exception e)
+         {
+            uScriptDebug.Log("(Node = Assign Material) Could not assign the material to '" + tmpTarget.name + "'. Please verify you are assigning to a valid material channel.\nError output: " + e.ToString(), uScriptDebug.Type.Error);
+         }
+
+      }
+
+   }
+
+   string GetFullPath(string FileName, string ResourcePath)
+   {
+
+      // Build final ResourcePath string
+      if (ResourcePath != "")
+      {
+         // Make sure all the slashes are correct
+         if (ResourcePath.Contains("\\"))
+         {
+            ResourcePath = ResourcePath.Replace("\\", "/");
+         }
+
+         // Prune any begining or ending slashes
+         if (ResourcePath.StartsWith("/") || ResourcePath.StartsWith(@"\"))
+         {
+            ResourcePath = ResourcePath.Remove(0, 1);
+         }
+         if (ResourcePath.EndsWith("/") || ResourcePath.EndsWith(@"\"))
+         {
+            int stringLength = ResourcePath.Length - 1;
+            ResourcePath = ResourcePath.Remove(stringLength, 1);
+         }
+
+         //prune Assets text if user added it
+         if (ResourcePath.StartsWith("Assets") || ResourcePath.StartsWith("assets"))
+         {
+            ResourcePath = ResourcePath.Remove(0, 6);
+         }
+
+         //prune Resources text if user added it
+         if (ResourcePath.StartsWith("Resources") || ResourcePath.StartsWith("resources"))
+         {
+            ResourcePath = ResourcePath.Remove(0, 9);
+         }
+
+
+      }
+
+      // Build final PrefabName string
+      if (FileName != "")
+      {
+         // Make sure all the slashes are correct
+         if (FileName.Contains("\\"))
+         {
+            FileName = FileName.Replace("\\", "/");
+         }
+
+         // Prune any begining or ending slashes
+         if (FileName.StartsWith("/") || FileName.StartsWith(@"\"))
+         {
+            FileName = FileName.Remove(0, 1);
+         }
+         if (FileName.EndsWith("/") || FileName.EndsWith(@"\"))
+         {
+            int stringLength = FileName.Length - 1;
+            FileName = FileName.Remove(stringLength, 1);
+         }
+
+
+         FileName = System.IO.Path.GetFileNameWithoutExtension(FileName);
+
+      }
+
+      // Build final fullPath
+      string fullPath = "";
+
+      if (ResourcePath != "")
+      {
+         fullPath = ResourcePath + "/" + FileName;
+      }
+      else
+      {
+         // Must be in the root of Resources
+         fullPath = FileName;
+      }
+
+      return fullPath;
+   }
+
+
+}
