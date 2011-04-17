643490350b077c5ac0dfeac977e5d99db691cfeb
Anthony Silva <anthony@detoxstudios.com>
2011-04-17 01:15:43 +0000

    Change all Power Of 2 Software references to Detox Studios LLC. Fix the EULA window popping up when it shouldn't be on recompile.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@276 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptEditor/Editor/uScript.cs
+++ uScript_Dev/Assets/uScript/uScriptEditor/Editor/uScript.cs
@@ -163,34 +163,34 @@ public class uScript : EditorWindow
 
 End User License Agreement
 
-This End User License Agreement (""EULA"") is a legal agreement between you (either an individual or a single entity) and Power Of 2 Software with regard to the copyrighted Software (herein referred to as ""SOFTWARE PRODUCT"" or ""SOFTWARE"") provided with this EULA.   The SOFTWARE PRODUCT includes computer software, the associated media, any printed materials, and any ""online"" or electronic documentation. Use of any software and related documentation (""Software"") provided to you by Power Of 2 Software in whatever form or media, will constitute your acceptance of these terms, unless separate terms are provided by the software supplier, in which case certain additional or different terms may apply. If you do not agree with the terms of this EULA, do not download, install, copy or use the Software. By installing, copying or otherwise using the SOFTWARE PRODUCT, you agree to be bound by the terms of this EULA.  If you do not agree to the terms of this EULA, Power Of 2 Software is unwilling to license the SOFTWARE PRODUCT to you. 
+This End User License Agreement (""EULA"") is a legal agreement between you (either an individual or a single entity) and Detox Studios LLC with regard to the copyrighted Software (herein referred to as ""SOFTWARE PRODUCT"" or ""SOFTWARE"") provided with this EULA.   The SOFTWARE PRODUCT includes computer software, the associated media, any printed materials, and any ""online"" or electronic documentation. Use of any software and related documentation (""Software"") provided to you by Detox Studios LLC in whatever form or media, will constitute your acceptance of these terms, unless separate terms are provided by the software supplier, in which case certain additional or different terms may apply. If you do not agree with the terms of this EULA, do not download, install, copy or use the Software. By installing, copying or otherwise using the SOFTWARE PRODUCT, you agree to be bound by the terms of this EULA.  If you do not agree to the terms of this EULA, Detox Studios LLC is unwilling to license the SOFTWARE PRODUCT to you. 
 
 1.  Eligible Licensees. This Software is available for license solely to SOFTWARE owners, with no right of duplication or further distribution, licensing, or sub-licensing.  IF YOU DO NOT OWN THE SOFTWARE, THEN DO NOT DOWNLOAD, INSTALL, COPY OR USE THE SOFTWARE.
  
-2.  License Grant.  Power Of 2 Software grants to you a personal, non-transferable and non-exclusive right to use the copy of the Software provided with this EULA. You agree you will not copy the Software except as necessary to use it on a single computer. You agree that you may not copy the written materials accompanying the Software. Modifying, translating, renting, copying, transferring or assigning all or part of the Software, or any rights granted hereunder, to any other persons and removing any proprietary notices, labels or marks from the Software is strictly prohibited.  Furthermore, you hereby agree not to create derivative works based on the Software.  You may not transfer this Software.
+2.  License Grant.  Detox Studios LLC grants to you a personal, non-transferable and non-exclusive right to use the copy of the Software provided with this EULA. You agree you will not copy the Software except as necessary to use it on a single computer. You agree that you may not copy the written materials accompanying the Software. Modifying, translating, renting, copying, transferring or assigning all or part of the Software, or any rights granted hereunder, to any other persons and removing any proprietary notices, labels or marks from the Software is strictly prohibited.  Furthermore, you hereby agree not to create derivative works based on the Software.  You may not transfer this Software.
 
-3.  Copyright.  The Software is licensed, not sold.  You acknowledge that no title to the intellectual property in the Software is transferred to you. You further acknowledge that title and full ownership rights to the Software will remain the exclusive property of Power Of 2 Software and/or its suppliers, and you will not acquire any rights to the Software, except as expressly set forth above. All copies of the Software will contain the same proprietary notices as contained in or on the Software. All title and copyrights in and to the SOFTWARE PRODUCT (including but not limited to any images, photographs, animations, video, audio, music, text and ""applets,"" incorporated into the SOFTWARE PRODUCT), the accompanying printed materials, and any copies of the SOFTWARE PRODUCT, are owned by Power Of 2 Software or its suppliers.  The SOFTWARE PRODUCT is protected by copyright laws and international treaty provisions.  You may not copy the printed materials 
+3.  Copyright.  The Software is licensed, not sold.  You acknowledge that no title to the intellectual property in the Software is transferred to you. You further acknowledge that title and full ownership rights to the Software will remain the exclusive property of Detox Studios LLC and/or its suppliers, and you will not acquire any rights to the Software, except as expressly set forth above. All copies of the Software will contain the same proprietary notices as contained in or on the Software. All title and copyrights in and to the SOFTWARE PRODUCT (including but not limited to any images, photographs, animations, video, audio, music, text and ""applets,"" incorporated into the SOFTWARE PRODUCT), the accompanying printed materials, and any copies of the SOFTWARE PRODUCT, are owned by Detox Studios LLC or its suppliers.  The SOFTWARE PRODUCT is protected by copyright laws and international treaty provisions.  You may not copy the printed materials 
 accompanying the SOFTWARE PRODUCT.
 
-4.  Reverse Engineering.  You agree that you will not attempt, and if you are a corporation, you will use your best efforts to prevent your employees and contractors from attempting to reverse compile, modify, translate or disassemble the Software in whole or in part. Any failure to comply with the above or any other terms and conditions contained herein will result in the automatic termination of this license and the reversion of the rights granted hereunder to Power Of 2 Software.
+4.  Reverse Engineering.  You agree that you will not attempt, and if you are a corporation, you will use your best efforts to prevent your employees and contractors from attempting to reverse compile, modify, translate or disassemble the Software in whole or in part. Any failure to comply with the above or any other terms and conditions contained herein will result in the automatic termination of this license and the reversion of the rights granted hereunder to Detox Studios LLC.
 
-5.  Disclaimer of Warranty. The Software is provided ""AS IS"" without warranty of any kind. Power Of 2 Software and its suppliers disclaim and make no express or implied warranties and specifically disclaim the warranties of merchantability, fitness for a particular purpose and non-infringement of third-party rights. The entire risk as to the quality and performance of the Software is with you. Neither Power Of 2 Software nor its suppliers warrant that the functions contained in the Software will meet your requirements or that the operation of the Software will be uninterrupted or error-free. Power Of 2 Software IS NOT OBLIGATED TO PROVIDE ANY UPDATES TO THE SOFTWARE.
+5.  Disclaimer of Warranty. The Software is provided ""AS IS"" without warranty of any kind. Detox Studios LLC and its suppliers disclaim and make no express or implied warranties and specifically disclaim the warranties of merchantability, fitness for a particular purpose and non-infringement of third-party rights. The entire risk as to the quality and performance of the Software is with you. Neither Detox Studios LLC nor its suppliers warrant that the functions contained in the Software will meet your requirements or that the operation of the Software will be uninterrupted or error-free. Detox Studios LLC IS NOT OBLIGATED TO PROVIDE ANY UPDATES TO THE SOFTWARE.
 
-6.  Limitation of Liability. Power Of 2 Software's entire liability and your exclusive remedy under this EULA shall not exceed the price paid for the Software, if any.  In no event shall Power Of 2 Software or its suppliers be liable to you for any consequential, special, incidental or indirect damages of any kind arising out of the use or inability to use the software, even if Power Of 2 Software or its supplier has been advised of the possibility of such damages, or 
+6.  Limitation of Liability. Detox Studios LLC's entire liability and your exclusive remedy under this EULA shall not exceed the price paid for the Software, if any.  In no event shall Detox Studios LLC or its suppliers be liable to you for any consequential, special, incidental or indirect damages of any kind arising out of the use or inability to use the software, even if Detox Studios LLC or its supplier has been advised of the possibility of such damages, or 
 any claim by a third party. 
 
 7.  Rental.  You may not loan, rent, or lease the SOFTWARE.  
 
 8.  Upgrades.  If the SOFTWARE is an upgrade from an earlier release or previously released version, you now may use that upgraded product only in accordance with this EULA.  If the SOFTWARE PRODUCT is an upgrade of a software program which you licensed as a single product, the SOFTWARE PRODUCT may be used only as part of that single product package and may not be separated for use on more than one computer.
 
-9.  OEM Product Support. Product support for the SOFTWARE PRODUCT IS provided by Power Of 2 Software.  For product support, please call Power Of 2 Software.  Should you have any questions concerning this, please refer to the address provided in the documentation.
+9.  OEM Product Support. Product support for the SOFTWARE PRODUCT IS provided by Detox Studios LLC.  For product support, please call Detox Studios LLC.  Should you have any questions concerning this, please refer to the address provided in the documentation.
 
-10.  No Liability for Consequential Damages.  In no event shall Power Of 2 Software or its suppliers be liable for any damages whatsoever (including, without limitation, incidental, direct, indirect special and consequential damages, damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use or inability to use this ""Your Company"" product, even if Power Of 2 Software has been advised of the possibility of such damages.  Because some states/countries do not allow the exclusion or limitation of liability for consequential or incidental damages, the above limitation may not apply to you.
+10.  No Liability for Consequential Damages.  In no event shall Detox Studios LLC or its suppliers be liable for any damages whatsoever (including, without limitation, incidental, direct, indirect special and consequential damages, damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use or inability to use this ""Your Company"" product, even if Detox Studios LLC has been advised of the possibility of such damages.  Because some states/countries do not allow the exclusion or limitation of liability for consequential or incidental damages, the above limitation may not apply to you.
 
-11.  Indemnification By You.  If you distribute the Software in violation of this Agreement, you agree to indemnify, hold harmless and defend Power Of 2 Software and its suppliers from and against any claims or lawsuits, including attorney's fees that arise or result from the use or distribution of the Software in violation of this Agreement.
+11.  Indemnification By You.  If you distribute the Software in violation of this Agreement, you agree to indemnify, hold harmless and defend Detox Studios LLC and its suppliers from and against any claims or lawsuits, including attorney's fees that arise or result from the use or distribution of the Software in violation of this Agreement.
 
-Power Of 2 Software
-http://www.powerof2software.com";
+Detox Studios LLC
+http://www.detoxstudios.com";
    #endregion
 
 
@@ -234,6 +234,14 @@ http://www.powerof2software.com";
       m_AppData.Set(key, value);
       m_AppData.Save(uScriptConfig.Paths.RootFolder + "/uScript.settings");
    }
+   
+   static public void LoadSettings()
+   {
+      if (System.IO.File.Exists(uScriptConfig.Paths.RootFolder + "/uScript.settings"))
+      {
+         m_AppData.Load(uScriptConfig.Paths.RootFolder + "/uScript.settings");
+      }
+   }
 
    static void Status_StatusUpdate(Detox.Utility.StatusUpdateEventArgs e)
    {
@@ -259,11 +267,8 @@ http://www.powerof2software.com";
       isPro = ( SystemInfo.supportsRenderTextures );
 
       EditorApplication.playmodeStateChanged = OnPlaymodeStateChanged;
-
-      if (System.IO.File.Exists(uScriptConfig.Paths.RootFolder + "/uScript.settings"))
-      {
-         m_AppData.Load(uScriptConfig.Paths.RootFolder + "/uScript.settings");
-      }
+      
+      LoadSettings();
 
       _statusbarMessage = "Unity " + (isPro ? "Pro" : "Indie") + " (version " + Application.unityVersion + ")";
    }
@@ -334,7 +339,9 @@ http://www.powerof2software.com";
    {
       if (!_EULAagreed)
       {
-         GUI.enabled = false;
+         LoadSettings();
+         _EULAagreed = (bool) uScript.GetSetting( "EULA\\Agreed", false );
+         GUI.enabled = _EULAagreed;
       }
 		
       // Set the default mouse region
@@ -463,13 +470,21 @@ http://www.powerof2software.com";
 
       if (!_EULAagreed)
       {
-         int w = 550;
-         int h = Math.Max(300, (int)position.height - 400);
-         Rect r = new Rect((position.width-w)/2, (position.height-h)/2, w, h);
-			
-         GUI.Window(110, r, DoWindowEULA, "End User License Agreement");
+         LoadSettings();
+         
+         _EULAagreed = (bool) uScript.GetSetting( "EULA\\Agreed", false );
+         
+         if (!_EULAagreed)
+         {
+            int w = 550;
+            int h = Math.Max(300, (int)position.height - 400);
+            Rect r = new Rect((position.width-w)/2, (position.height-h)/2, w, h);
+   			
+            GUI.Window(110, r, DoWindowEULA, "End User License Agreement");
+         }
       }
-      else
+      
+      if (_EULAagreed)
       {
          if ( true == contextActive )
          {
