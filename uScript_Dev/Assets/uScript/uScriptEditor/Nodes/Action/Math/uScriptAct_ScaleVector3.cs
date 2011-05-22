fdcae6747b24775b69df7e72739a4531c60d3d89
Trapper McFerron <trapper@detoxstudios.com>
2011-05-22 07:19:12 +0000

    new node
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@623 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptEditor/Nodes/Action/Math/uScriptAct_ScaleVector3.cs
@@ -0,0 +1,25 @@
+// uScript Action Node
+// (C) 2010 Detox Studios LLC
+// Desc: Multiplies two float variables together and returns the result.
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Action/Math")]
+[NodeLicense("http://www.detoxstudios.com/legal/eula.html")]
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Scales a Vector3.")]
+[NodeDescription("Scales a Vector3")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://uscript.net/manual/node_nodoc.html")]
+
+[FriendlyName("Scale Vector")]
+public class uScriptAct_ScaleVector3 : uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public void In([FriendlyName("Vector")] Vector3 v, [FriendlyName("Scale")] float s, [FriendlyName("Vector Resut")] out Vector3 result)
+   {
+      result = v * s;
+   }
+}
