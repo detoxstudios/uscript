27520421efcef8bc2c5b5d6582958c78b83cf2d7
Trapper McFerron <trapper@detoxstudios.com>
2011-05-22 03:51:27 +0000

    updated comments.. again
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@614 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptEditor/Nodes/Action/GameObjects/uScriptAct_IsometricCharacterController.cs
+++ uScript_Dev/Assets/uScript/uScriptEditor/Nodes/Action/GameObjects/uScriptAct_IsometricCharacterController.cs
@@ -9,7 +9,7 @@ using System.Collections;
 [NodeLicense("http://www.detoxstudios.com/legal/eula.html")]
 [NodeCopyright("Copyright 2011 by Detox Studios LLC")]
 [NodeToolTip("Isometric Character Controller.")]
-[NodeDescription("Simple character controller.  Character always moves along its forward vector and back along its back vector.")]
+[NodeDescription("Simple character controller.  Character always moves forward and backwards along its forward vector.")]
 [NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
 [NodeHelp("http://uscript.net/manual/node_nodoc.html")]
 
