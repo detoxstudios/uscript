c650ea0e0507bf872b7235d32cff4cc2cf7c5978
Scott Blinn <scott@detoxstudios.com>
2011-05-30 20:24:46 +0000

    Updated build number. Because of massive node reorg effort, moving to 0.4.x series
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@675 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptEditor/Editor/uScript.cs
+++ uScript_Dev/Assets/uScript/uScriptEditor/Editor/uScript.cs
@@ -15,10 +15,10 @@ public class uScript : EditorWindow
 {
                                            //format is MAJOR.MINOR.YYMMDDa
                                            //(where 'a' is incremented if we have more than 1 daily build we release)
-   public string    uScriptBuild           { get { return "0.3.110522b"; } }
+   public string    uScriptBuild           { get { return "0.4.110530a"; } }
    public string    RequiredUnityBuild     { get { return  "3.3.0f4"; } }
    public string    RequiredUnityBetaBuild { get { return "3.4.0b2"; } }
-   public DateTime  ExpireDate             { get { return new DateTime( 2011, 6, 6 ); } }
+   public DateTime  ExpireDate             { get { return new DateTime( 2011, 6, 13 ); } }
 
    private enum MouseRegion
    {
