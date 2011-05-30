ad24752ca896762c2be65aa56b1e3a662d931f1e
Scott Blinn <scott@detoxstudios.com>
2011-05-30 05:59:27 +0000

    Moved 'Play Animation' node into Actions/Animation folder
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@656 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptEditor/Nodes/Actions/Animation/uScriptAct_PlayAnimation.cs
@@ -0,0 +1,111 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+// Desc: Play the specified animation on the target object. Animation must exist in the GameObject's AnimationClip.
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/Animation")]
+[NodeLicense("http://www.detoxstudios.com/legal/eula.html")]
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Play the specified animation on the target object.")]
+[NodeDescription("Play the specified animation on the target object.\n \nTarget: The Target GameObject(s) to play the animation on.\nAnimation: The name of the animation to play. Animation must exist in the GameObject's AnimationClip.\nSpeed Factor: The speed at which to play the animation.\nStop Other Animation: Stop any currently playing animations before playing this one.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://uscript.net/manual/node_nodoc.html")]
+
+[FriendlyName("Play Animation")]
+public class uScriptAct_PlayAnimation : uScriptLogic
+{
+   private GameObject m_GameObject = null;
+   private AnimationClip m_Animation = null;
+
+   public delegate void uScriptEventHandler(object sender, System.EventArgs args);
+
+   public bool Out { get { return true; } }
+   
+   [FriendlyName("Finished")]
+   public event uScriptEventHandler Finished;
+
+   public void In(GameObject[] Target, AnimationClip Animation, [FriendlyName("Speed Factor"), DefaultValue(1f)] float SpeedFactor, [FriendlyName("Stop Other Animation"), DefaultValue(true)] bool StopOtherAnimations)
+   {
+      m_GameObject = null;
+
+      foreach ( GameObject currentTarget in Target )
+      {
+         if (currentTarget != null)
+         {
+            //only save one so we can ask about the animation state
+            //i don't need to save all of them in the array
+            m_GameObject = currentTarget;
+            m_Animation  = Animation;
+
+            if (SpeedFactor == 0F)
+            {
+               currentTarget.animation[Animation.name].speed = 1.0F;
+            }
+            else
+            {
+
+               currentTarget.animation[Animation.name].speed = SpeedFactor;
+            }
+
+            if (StopOtherAnimations)
+            {
+               currentTarget.animation.Play(PlayMode.StopAll);
+            }
+
+            if (SpeedFactor < 0)
+            {
+               // Needed to play in reverse with a negative speed
+               currentTarget.animation[Animation.name].time = currentTarget.animation[Animation.name].length;
+            }
+
+            currentTarget.animation[Animation.name].wrapMode = WrapMode.Once;
+            currentTarget.animation.Play(Animation.name);
+         }
+      }
+   }
+
+   public override void Update()
+   {
+      if ( null != m_GameObject )
+      {
+         if ( false == m_GameObject.animation.IsPlaying(m_Animation.name) )
+         {
+            m_GameObject = null;
+
+            if ( null != Finished ) Finished( this, new System.EventArgs( ) );
+         }
+      }
+   }
+
+
+#if UNITY_EDITOR
+   public override Hashtable EditorDragDrop(object o)
+   {
+      if (typeof(AnimationClip).IsAssignableFrom(o.GetType()))
+      {
+         AnimationClip ac = (AnimationClip)o;
+
+         Hashtable hashtable = new Hashtable();
+         hashtable["Animation"] = UnityEditor.AssetDatabase.GetAssetPath(ac.GetInstanceID());
+
+         return hashtable;
+      }
+      if (typeof(UnityEngine.GameObject).IsAssignableFrom(o.GetType()))
+      {
+         GameObject go = (GameObject)o;
+
+         Hashtable hashtable = new Hashtable();
+         hashtable["Target"] = go.name;
+
+         return hashtable;
+      }
+
+      return null;
+   }
+#endif
+
+
+
+}
