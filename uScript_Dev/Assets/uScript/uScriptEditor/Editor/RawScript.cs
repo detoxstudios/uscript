aee5649233df76ea3caad5b5e4c770e7006ed78e
Trapper McFerron <trapper@detoxstudios.com>
2011-06-08 06:18:16 +0000

    forgot to checkin this file - kudos to Tonez for being so nice about it
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@739 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptEditor/Editor/RawScript.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+
+using Detox.Utility;
+using Detox.Data;
+using Detox.Data.ScriptEditor;
+
+using UnityEngine;
+
+namespace Detox.ScriptEditor
+{
+   public class RawScript
+   {
+      public Parameter []ExternalParameters { get { return m_GeneratedCode.ExternalParameters; } }
+      public Plug      []ExternalInputs     { get { return m_GeneratedCode.ExternalInputs; } }
+      public Plug      []ExternalOutputs    { get { return m_GeneratedCode.ExternalOutputs; } }
+      public Plug      []ExternalEvents     { get { return m_GeneratedCode.ExternalEvents; } }
+      public string    []Drivens            { get { return m_GeneratedCode.Drivens; } }
+      public string      Type               { get { return m_Type; } }
+
+      private string m_Type = "";
+
+      private UnityCSharpGenerator m_GeneratedCode;
+
+      public bool Load( string path )
+      {
+         ScriptEditor scriptEditor = new ScriptEditor( "", null, null );
+         if ( false == scriptEditor.Open(path) ) return false;
+
+         m_GeneratedCode = new UnityCSharpGenerator( );
+         
+         if ( 0 != scriptEditor.Externals.Length )
+         {
+            m_GeneratedCode.GenerateLogicScript( m_Type, scriptEditor );
+         }
+
+         m_Type = Path.GetFileNameWithoutExtension(path) + uScriptConfig.Files.GeneratedCodeExtension;
+
+         return true;
+      }
+   }
+}
