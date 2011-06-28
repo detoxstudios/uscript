1198789b740ac8fc7d81ff81916a60c14767db8b
Anthony Silva <anthony@detoxstudios.com>
2011-06-28 07:54:06 +0000

    Move OnGUI to its own node and add a parameter called GUI Changed that maps to the global GUI.changed variable.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@878 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptEditor/Nodes/Events/uScript_GUI.cs
@@ -0,0 +1,45 @@
+// uScript uScript_GUI.cs
+// (C) 2010 Detox Studios LLC
+// Desc: Fires GUI-related events.
+
+using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("uScript/Event Components/GUI")]
+[NodeAutoAssignMasterInstance(true)]
+[NodeComponentType(typeof(Transform))]
+
+[NodePath("Events/Game Events")]
+[NodeLicense("http://www.detoxstudios.com/legal/eula.html")]
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Fires GUI-related events.")]
+[NodeDescription("Fires GUI-related events.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
+
+[FriendlyName("GUI Events")]
+public class uScript_GUI : uScriptEvent
+{
+   public delegate void uScriptEventHandler(object sender, GUIEventArgs args);
+
+   public class GUIEventArgs : System.EventArgs
+   {
+      private bool m_GUIChanged;
+
+      [FriendlyName("GUI Changed")]
+      public bool GUIChanged { get { return m_GUIChanged; } }
+
+      public GUIEventArgs(bool guiChanged)
+      {
+         m_GUIChanged = guiChanged;
+      }
+   }
+
+   [FriendlyName("On GUI")]
+   public event uScriptEventHandler OnGui;
+   
+   void OnGUI()
+   {
+      if ( OnGui != null ) OnGui(this, new GUIEventArgs(GUI.changed));
+   }
+}
--- uScript_Dev/Assets/uScript/uScriptEditor/Nodes/Events/uScript_Update.cs
+++ uScript_Dev/Assets/uScript/uScriptEditor/Nodes/Events/uScript_Update.cs
@@ -1,6 +1,6 @@
 // uScript uScript_Update.cs
 // (C) 2010 Detox Studios LLC
-// Desc: Fires an event signal when various global events (OnGUI, Update, LateUpdate, and FixedUpdate) take place.
+// Desc: Fires an event signal when various global events (Update, LateUpdate, and FixedUpdate) take place.
 
 using UnityEngine;
 using System.Collections;
@@ -12,8 +12,8 @@ using System.Collections;
 [NodePath("Events/Game Events")]
 [NodeLicense("http://www.detoxstudios.com/legal/eula.html")]
 [NodeCopyright("Copyright 2011 by Detox Studios LLC")]
-[NodeToolTip("Fires an event signal when various global events (OnGUI, Update, LateUpdate, and FixedUpdate) take place.")]
-[NodeDescription("Fires an event signal when various global events (OnGUI, Update, LateUpdate, and FixedUpdate) take place.")]
+[NodeToolTip("Fires an event signal when various global events (Update, LateUpdate, and FixedUpdate) take place.")]
+[NodeDescription("Fires an event signal when various global events (Update, LateUpdate, and FixedUpdate) take place.")]
 [NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
 [NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
 
@@ -22,9 +22,6 @@ public class uScript_Update : uScriptEvent
 {
    public delegate void uScriptEventHandler(object sender, System.EventArgs args);
 
-   [FriendlyName("On GUI")]
-   public event uScriptEventHandler OnGui;
-   
    [FriendlyName("On Update")]
    public event uScriptEventHandler OnUpdate;
    
@@ -33,11 +30,6 @@ public class uScript_Update : uScriptEvent
 
    [FriendlyName("On FixedUpdate")]
    public event uScriptEventHandler OnFixedUpdate;
-
-   void OnGUI()
-   {
-      if ( OnGui != null ) OnGui(this, new System.EventArgs());
-   }
 
    void Update()
    {
