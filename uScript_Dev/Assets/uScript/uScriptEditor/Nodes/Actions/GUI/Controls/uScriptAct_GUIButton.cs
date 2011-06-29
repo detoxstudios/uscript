9257c67da966d9110d7ea75846f0782796d2bea0
Anthony Silva <anthony@detoxstudios.com>
2011-06-29 05:37:45 +0000

    Move GUI controls into Controls directory.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@895 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptEditor/Nodes/Actions/GUI/Controls/uScriptAct_GUIButton.cs
+++ uScript_Dev/Assets/uScript/uScriptEditor/Nodes/Actions/GUI/Controls/uScriptAct_GUIButton.cs
@@ -5,7 +5,7 @@
 using UnityEngine;
 using System.Collections;
 
-[NodePath("Actions/GUI")]
+[NodePath("Actions/GUI/Controls")]
 [NodeLicense("http://www.detoxstudios.com/legal/eula.html")]
 [NodeCopyright("Copyright 2011 by Detox Studios LLC")]
 [NodeToolTip("Shows a GUIButton on the screen and allows responses when held down, released, and clicked.")]
--- uScript_Dev/Assets/uScript/uScriptEditor/Nodes/Actions/GUI/Controls/uScriptAct_GUILabel.cs
+++ uScript_Dev/Assets/uScript/uScriptEditor/Nodes/Actions/GUI/Controls/uScriptAct_GUILabel.cs
@@ -5,7 +5,7 @@
 using UnityEngine;
 using System.Collections;
 
-[NodePath("Actions/GUI")]
+[NodePath("Actions/GUI/Controls")]
 [NodeLicense("http://www.detoxstudios.com/legal/eula.html")]
 [NodeCopyright("Copyright 2011 by Detox Studios LLC")]
 [NodeToolTip("Shows a GUILabel on the screen.")]
