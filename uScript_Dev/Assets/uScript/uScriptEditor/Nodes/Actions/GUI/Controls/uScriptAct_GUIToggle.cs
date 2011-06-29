c84b5a8a587e86fce2ad7b66547778074dff0e34
Anthony Silva <anthony@detoxstudios.com>
2011-06-29 08:46:01 +0000

    Add a GUI toggle control. Update comments.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@900 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptEditor/Nodes/Actions/GUI/Controls/uScriptAct_GUIToggle.cs
@@ -0,0 +1,58 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+// Desc: Shows a GUIToggle on the screen and allows responses when changed.
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/GUI/Controls")]
+[NodeLicense("http://www.detoxstudios.com/legal/eula.html")]
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Shows a GUIToggle on the screen and allows responses when changed.")]
+[NodeDescription("Shows a GUIToggle on the screen and allows responses when changed.\n \nValue (in/out): The value of this toggle.\nText: The text you want to display with the toggle. \nPosition: The position and size of the toggle.\nTexture: The background image to use for the toggle.\nControl Name: Name to give to this toggle GUI control.\nTool Tip: The tool tip to display when the toggle is being hovered over.\nGUI Style: The name of a custom GUI style to use when displaying this toggle.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
+
+[FriendlyName("GUI Toggle")]
+public class uScriptAct_GUIToggle : uScriptLogic
+{
+   [FriendlyName("Changed")]
+   public bool Changed { get { return m_Changed; } }
+   private bool m_Changed = false;
+
+   public bool Out { get { return true; } }
+
+   public void In(
+      ref bool Value,
+      string Text,
+      Rect Position,
+      Texture2D Texture,
+      [FriendlyName("Control Name"), DefaultValue(""), SocketState(false, false)] string ControlName,
+      [FriendlyName("Tool Tip"), DefaultValue(""), SocketState(false, false)] string ToolTip,
+      [FriendlyName("GUI Style"), DefaultValue(""), SocketState(false, false)] string guiStyle
+      )
+   {
+      GUIContent content = new GUIContent(Text, Texture, ToolTip);
+      bool value = false;
+      m_Changed = false;
+      
+      if (!string.IsNullOrEmpty(ControlName)) GUI.SetNextControlName(ControlName);            
+
+      if (string.IsNullOrEmpty(guiStyle))
+      {
+         value = GUI.Toggle(Position, Value, content);
+      }
+      else
+      {
+         value = GUI.Toggle(Position, Value, content, GUI.skin.GetStyle(guiStyle));
+      }
+      
+      // changed event
+      if (Value != value)
+      {
+         m_Changed = true;
+      }
+
+      Value = value;
+   }
+}
