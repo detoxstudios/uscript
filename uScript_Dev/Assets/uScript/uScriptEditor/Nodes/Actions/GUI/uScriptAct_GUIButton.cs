8453f1a6c8c95a9276eedda350c6d7e1511d0f40
Anthony Silva <anthony@detoxstudios.com>
2011-06-29 04:37:35 +0000

    Use RepeatButton so that we can have Down, Held, and Up events, in addition to Clicked. Thanks for the tip, Mike!
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@891 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptEditor/Nodes/Actions/GUI/uScriptAct_GUIButton.cs
+++ uScript_Dev/Assets/uScript/uScriptEditor/Nodes/Actions/GUI/uScriptAct_GUIButton.cs
@@ -1,6 +1,6 @@
 // uScript Action Node
 // (C) 2011 Detox Studios LLC
-// Desc: Shows a GUIButton on the screen and allows responses when clicked and hovered in/out.
+// Desc: Shows a GUIButton on the screen and allows responses when held down, released, and clicked.
 
 using UnityEngine;
 using System.Collections;
@@ -8,17 +8,29 @@ using System.Collections;
 [NodePath("Actions/GUI")]
 [NodeLicense("http://www.detoxstudios.com/legal/eula.html")]
 [NodeCopyright("Copyright 2011 by Detox Studios LLC")]
-[NodeToolTip("Shows a GUIButton on the screen and allows responses when clicked and hovered in/out.")]
-[NodeDescription("Shows a GUIButton on the screen and allows responses when clicked and hovered in/out.\n \nText: The text you want to display on the button. \nPosition: The position and size of the button.\nTexture: The background image to use for the button.\nTool Tip: The tool tip to display when the button is being hovered over.\nGUI Style: The name of a custom GUI style to use when displaying this button.")]
+[NodeToolTip("Shows a GUIButton on the screen and allows responses when held down, released, and clicked.")]
+[NodeDescription("Shows a GUIButton on the screen and allows responses when held down, released, and clicked..\n \nText: The text you want to display on the button. \nPosition: The position and size of the button.\nTexture: The background image to use for the button.\nTool Tip: The tool tip to display when the button is being hovered over.\nGUI Style: The name of a custom GUI style to use when displaying this button.")]
 [NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
 [NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
 
 [FriendlyName("GUI Button")]
 public class uScriptAct_GUIButton : uScriptLogic
 {
+   private bool m_ButtonDown = false;
+   
+   public delegate void uScriptEventHandler(object sender, System.EventArgs args);
+
+   [FriendlyName("Button Down")]
+   public event uScriptEventHandler OnButtonDown;
+ 
+   [FriendlyName("Button Held")]
+   public event uScriptEventHandler OnButtonHeld;
+ 
+   [FriendlyName("Button Up")]
+   public event uScriptEventHandler OnButtonUp;
+ 
    [FriendlyName("Button Clicked")]
-   public bool OnButtonClicked { get { return m_ButtonClicked; } }
-   private bool m_ButtonClicked = false;
+   public event uScriptEventHandler OnButtonClicked;
 
    public bool Out { get { return true; } }
 
@@ -30,15 +42,34 @@ public class uScriptAct_GUIButton : uScriptLogic
       [FriendlyName("GUI Style"), DefaultValue(""), SocketState(false, false)] string guiStyle
       )
    {
-      GUIContent content = new GUIContent(Text, Texture, ToolTip);
-
-      if (string.IsNullOrEmpty(guiStyle))
+      if (Event.current.type == EventType.Repaint || Event.current.isMouse)
       {
-         m_ButtonClicked = GUI.Button(Position, content);
-      }
-      else
-      {
-         m_ButtonClicked = GUI.Button(Position, content, GUI.skin.GetStyle(guiStyle));
+         GUIContent content = new GUIContent(Text, Texture, ToolTip);
+         bool buttonDown = false;
+   
+         if (string.IsNullOrEmpty(guiStyle))
+         {
+            buttonDown = GUI.RepeatButton(Position, content);
+         }
+         else
+         {
+            buttonDown = GUI.RepeatButton(Position, content, GUI.skin.GetStyle(guiStyle));
+         }
+         
+         // down event
+         if (!m_ButtonDown && buttonDown && OnButtonDown != null) OnButtonDown( this, new System.EventArgs() );
+     
+         // held event
+         if (m_ButtonDown && buttonDown && OnButtonHeld != null) OnButtonHeld( this, new System.EventArgs() );
+     
+         // up/clicked event
+         if (m_ButtonDown && !buttonDown)
+         {
+            if (OnButtonUp != null) OnButtonUp( this, new System.EventArgs() );
+            if (OnButtonClicked != null) OnButtonClicked( this, new System.EventArgs() );
+         }
+         
+         m_ButtonDown = buttonDown;
       }
    }
 }
