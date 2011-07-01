79f96d388466a79a541f3ad06101854ad92cbc65
Scott Blinn <scott@detoxstudios.com>
2011-07-01 06:39:36 +0000

    Updated build number (0.9.0948) Synced up EULA dates
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@948 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptEditor/Editor/uScript.cs
+++ uScript_Dev/Assets/uScript/uScriptEditor/Editor/uScript.cs
@@ -14,7 +14,7 @@ using Detox.FlowChart;
 public class uScript : EditorWindow
 {
    //format is MAJOR.MINOR.FOURDIGITSVNCOMMITNUMBER
-   public string uScriptBuild { get { return "0.9.0947"; } }
+   public string uScriptBuild { get { return "0.9.0948"; } }
    public string RequiredUnityBuild { get { return "3.3"; } }
    public string RequiredUnityBetaBuild { get { return "3.4"; } }
    public DateTime ExpireDate { get { return new DateTime(2011, 7, 18); } }
@@ -236,7 +236,7 @@ info@detoxstudios.com
 http://www.detoxstudios.com
 http://uscript.net
 
-(06/30/2011)";
+(07/01/2011)";
    #endregion
 
    public bool IsAttachedToMaster
