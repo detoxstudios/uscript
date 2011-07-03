8f7c0db31c04755e2254ceeb53042f58e9623965
Scott Blinn <scott@detoxstudios.com>
2011-07-03 05:11:56 +0000

    Added new nodes: Get Rect Components Set Rect Components
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@957 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Math/uScriptAct_SetRectComponents.cs
@@ -0,0 +1,25 @@
+// uScript Action Node
+// (C) 2010 Detox Studios LLC
+// Desc: Sets a Rect to the defined Left, Top, Width and Height float component values.
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/Set Variable")]
+[NodeLicense("http://www.detoxstudios.com/legal/eula.html")]
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Sets a Rect to the defined Left, Top, Width and Height float component values.")]
+[NodeDescription("Sets a Rect to the defined Left, Top, Width and Height float component values.\n \nLeft (in): Left value to use for the Output Rect.\nTop (in): Top value to use for the Output Rect.\nWidth (in): Width value to use for the Output Rect.\nHeight (in): Height value to use for the Output Rect.\nOutput Rect (out): Rect variable built from the specified Left, Top, Width, and Height.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Set_Vector4_Components")]
+
+[FriendlyName("Set Rect Components")]
+public class uScriptAct_SetRectComponents : uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public void In(float Left, float Top, [DefaultValue(32f)] float Width, [DefaultValue(32f)] float Height, [FriendlyName("Output Rect")] out Rect OutputRect)
+   {
+      OutputRect = new Rect(Left, Top, Width, Height);
+   }
+}
