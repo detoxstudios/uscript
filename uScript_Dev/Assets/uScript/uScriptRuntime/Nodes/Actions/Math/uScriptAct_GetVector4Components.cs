8271297003e785d0655085b1a29ebce22ebed668
Scott Blinn <scott@detoxstudios.com>
2011-07-03 05:11:30 +0000

    Fixed description mis-wording in the Get Vector4 Component
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@956 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Math/uScriptAct_GetVector4Components.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Math/uScriptAct_GetVector4Components.cs
@@ -9,7 +9,7 @@ using System.Collections;
 [NodeLicense("http://www.detoxstudios.com/legal/eula.html")]
 [NodeCopyright("Copyright 2011 by Detox Studios LLC")]
 [NodeToolTip("Gets the components of a Vector4 as floats.")]
-[NodeDescription("Gets the components of a Vector4 as floats.\n \n\n \nInput Vector3: The input vector to get components of.\nX: The x value of the Input Vector4.\nY: The y value of the Input Vector4.\nZ: The z value of the Input Vector4.")]
+[NodeDescription("Gets the components of a Vector4 as floats.\n \n\n \nInput Vector4: The input vector to get components of.\nX: The x value of the Input Vector4.\nY: The y value of the Input Vector4.\nZ: The z value of the Input Vector4.\nW: The w value of the Input Vector4.")]
 [NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
 [NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Get_Vector4_Components")]
 
