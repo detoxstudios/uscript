626b758b5b93ebfe3abc3dcd56360719b6decc44
Scott Blinn <scott@detoxstudios.com>
2011-07-10 03:27:17 +0000

    Reverted from commit 981 for Beta 4 build. Due to loosing Key Code pulldown in the node's proerties panel.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@986 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Event/Filters/uScriptAct_OnInputEventFilter.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Event/Filters/uScriptAct_OnInputEventFilter.cs
@@ -6,9 +6,9 @@ using UnityEngine;
 using System.Collections;
 
 [NodePath("Actions/Events/Filters")]
-
+[NodeLicense("http://www.detoxstudios.com/legal/eula.html")]
 [NodeCopyright("Copyright 2011 by Detox Studios LLC")]
-[NodeToolTip( "Filters the On Input Event output from the Input Events node to a specific input (key, mouse, joystick) pressed down, held, or released.")]
+[NodeToolTip("Filters the On Input Event output from the Input Events node to a specific input (key, mouse, joystick) pressed down, held, or released.")]
 [NodeDescription("Filters the On Input Event output from the Input Events node to a specific input (key, mouse, joystick) pressed down, held, or released.\n \nKey Code: The key to listen for events from.")]
 [NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
 [NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Input_Events_Filter")]
@@ -29,17 +29,10 @@ public class uScriptAct_OnInputEventFilter : uScriptLogic
    [FriendlyName("Input Up")]
    public bool KeyUp { get { return m_InputUp; } }
 
-   public void In([FriendlyName("Key Code")] UnityEngine.KeyCode[] KeyCodes) 
-   { 
-      m_InputHeld = false;
-      m_InputDown = false;
-      m_InputUp   = false;
-
-      foreach (KeyCode code in KeyCodes) 
-      { 
-         m_InputDown |= UnityEngine.Input.GetKeyDown(code); 
-         m_InputHeld |= UnityEngine.Input.GetKey(code); 
-         m_InputUp   |= UnityEngine.Input.GetKeyUp(code); 
-      } 
+   public void In([FriendlyName("Key Code")] UnityEngine.KeyCode KeyCode)
+   {
+      m_InputDown = UnityEngine.Input.GetKeyDown(KeyCode);
+      m_InputHeld = UnityEngine.Input.GetKey(KeyCode);
+      m_InputUp = UnityEngine.Input.GetKeyUp(KeyCode);
    }
 }
