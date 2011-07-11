a31e0ab9c48818145bbc24a9dd4c138537352c5b
Trapper McFerron <trapper@detoxstudios.com>
2011-07-11 06:37:19 +0000

    Added Name as an optional parameter to CompareGameObjects
    requested by post: http://www.uscript.net/forum/viewtopic.php?f=9&t=123&p=528#p528
    
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@993 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Conditions/Comparison/uScriptCon_CompareGameObjects.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Conditions/Comparison/uScriptCon_CompareGameObjects.cs
@@ -9,8 +9,15 @@ using System.Collections;
 [NodePath("Conditions/Comparison")]
 
 [NodeCopyright("Copyright 2011 by Detox Studios LLC")]
-[NodeToolTip("Compares the unique InstanceID of the attached GameObject variables and outputs accordingly.")]
-[NodeDescription("Compares the unique InstanceID of the attached GameObject variables and outputs accordingly. Optionally you can compare by a GameObject's tag.\n \nA: The first GameObject to compare.\nB: The seconds GameObject to compare.\nCompare By Tag: Whether or not to compare the GameObjects' tags instead of the objects themselves.")]
+[NodeToolTip("Compares the unique tag, name and InstanceID of the attached GameObject variables and outputs accordingly.")]
+[NodeDescription("Compares the unique InstanceID of the attached GameObject variables and outputs accordingly. Optionally you can compare by a GameObject's tag and/or name instead.\n \n" +
+                 "A: The first GameObject to compare.\n" +
+                 "B: The seconds GameObject to compare.\n" +
+                 "Compare By Tag: Whether or not to compare the GameObjects' tags instead of the objects themselves.\n" +
+                 "Compare By Name: Whether or not to compare the GameObjects' names instead of the objects themselves.\n\n" +
+                 "Please note, if Compare By Name and Compare By Tag are both checked, they both much match for the 'Same' signal to fire.")]
+
+
 [NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
 [NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Compare_GameObjects")]
 
@@ -22,15 +29,26 @@ public class uScriptCon_CompareGameObjects : uScriptLogic
    public bool Same { get { return m_CompareValue; } }
    public bool Different { get { return !m_CompareValue; } }
 
-   public void In(GameObject A, GameObject B, [FriendlyName("Compare By Tag"), SocketState(false, false)] bool CompareByTag)
+   public void In(GameObject A, GameObject B, [FriendlyName("Compare By Tag"), SocketState(false, false)] bool CompareByTag, [FriendlyName("Compare By Name"), SocketState(false, false)] bool CompareByName)
    {
-      if (!CompareByTag)
+      m_CompareValue = false;
+   
+      if (true == CompareByTag || CompareByName)
       {
-         m_CompareValue = A.GetInstanceID() == B.GetInstanceID();
+         m_CompareValue = true;
+   
+         if (true == CompareByTag)
+         {
+            m_CompareValue = m_CompareValue && A.tag == B.tag;
+         }
+         if (true == CompareByName)
+         {
+            m_CompareValue = m_CompareValue && A.name == B.name;
+         }
       }
       else
       {
-         m_CompareValue = A.tag == B.tag;
+         m_CompareValue = A.GetInstanceID() == B.GetInstanceID();
       }
    }
 }
