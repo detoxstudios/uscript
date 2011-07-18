dafc386d2746fccac632c3fcf6a5a73bdb156bd9
Scott Blinn <scott@detoxstudios.com>
2011-07-18 06:26:42 +0000

    Updated node description to let the user know they need the Target setup to use particles for thisnode to work.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1026 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Particles/uScriptAct_Particles.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Particles/uScriptAct_Particles.cs
@@ -9,7 +9,7 @@ using System.Collections;
 
 [NodeCopyright("Copyright 2011 by Detox Studios LLC")]
 [NodeToolTip("Start and stop particle emitters. Optionally clear all particles before starting or after stopping.")]
-[NodeDescription("Start and stop particle emitters.\n \nTarget: The Target GameObject(s) to start/stop particles on.\nClear Particles: Whether or not to immediately clear all the particles already emitted by Target emitters - will be cleared before starting or after stopping.")]
+[NodeDescription("Start and stop particle emitters.\n \nTarget: The Target GameObject(s) to start/stop particles on. Note: The Target GameObject must be setup in Unity to use particles (have the appropriate particle component assigned and set up).\nClear Particles: Whether or not to immediately clear all the particles already emitted by Target emitters - will be cleared before starting or after stopping.")]
 [NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
 [NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Particles")]
 
