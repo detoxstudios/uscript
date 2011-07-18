06101f801c5c790893160c9102d6ab2b0e4e6bd8
Scott Blinn <scott@detoxstudios.com>
2011-07-18 05:32:50 +0000

    Moved various Get/Set nodes into Actions/Variables and gave them better names so that I can depricate the old/poorly named ones.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1018 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Set Variable/uScriptAct_GetComponentsVector4.cs
@@ -0,0 +1,30 @@
+// uScript Action Node
+// (C) 2010 Detox Studios LLC
+// Desc: Gets the components of a Vector4 as floats.
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/Variables")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Gets the components of a Vector4 as floats.")]
+[NodeDescription("Gets the components of a Vector4 as floats.\n \n\n \nInput Vector4: The input vector to get components of.\nX: The x value of the Input Vector4.\nY: The y value of the Input Vector4.\nZ: The z value of the Input Vector4.\nW: The w value of the Input Vector4.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Get_Vector4_Components")]
+
+[FriendlyName("Get Components (Vector4)")]
+public class uScriptAct_GetComponentsVector4 : uScriptLogic
+{
+   // How many outputs defined here
+   public bool Out { get { return true; } }
+
+   // Do logic here
+   public void In([FriendlyName("Input Vector4")] Vector4 InputVector4, out float X, out float Y, out float Z, out float W)
+   {
+      X = InputVector4.x;
+      Y = InputVector4.y;
+      Z = InputVector4.z;
+      W = InputVector4.w;
+   }
+}
---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Set Variable/uScriptAct_SetComponentsVector3.cs
@@ -0,0 +1,27 @@
+// uScript Action Node
+// (C) 2010 Detox Studios LLC
+// Desc: Sets a Vector3 to the defined X, Y, and Z float component values.
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/Variables")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Sets a Vector3 to the defined X, Y, and Z float component values.")]
+[NodeDescription("Sets a Vector3 to the defined X, Y, and Z float component values.\n \nX: X value to use for the Output Vector.\nY: Y value to use for the Output Vector.\nZ: Z value to use for the Output Vector.\nOutput Vector3 (out): Vector3 variable built from the specified X, Y, and Z.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Set_Vector3_Components")]
+
+[FriendlyName("Set Components (Vector3)")]
+public class uScriptAct_SetComponentsVector3 : uScriptLogic
+{
+   // How many outputs defined here
+   public bool Out { get { return true; } }
+
+   // Do logic here
+   public void In(float X, float Y, float Z, [FriendlyName("Output Vector3")] out Vector3 OutputVector3)
+   {
+      OutputVector3 = new Vector3(X, Y, Z);
+   }
+}
