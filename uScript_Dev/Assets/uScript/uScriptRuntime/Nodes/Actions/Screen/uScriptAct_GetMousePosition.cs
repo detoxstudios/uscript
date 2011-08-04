e61cb3745fd269f732e240aee820062c21d9e706
Scott Blinn <scott@detoxstudios.com>
2011-08-04 17:26:14 +0000

    Updated Get Mouse Position node.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1081 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Screen/uScriptAct_GetMousePosition.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Screen/uScriptAct_GetMousePosition.cs
@@ -9,7 +9,7 @@ using System.Collections;
 
 [NodeCopyright("Copyright 2011 by Detox Studios LLC")]
 [NodeToolTip("Gets the position of the mouse.")]
-[NodeDescription("Gets the position of the mouse.\n\nPosition(Out): Returns the position of the mouse.")]
+[NodeDescription("Gets the position of the mouse.\n\nInvert Y: Flips the Y value, effectivly making the top left corner of the screen be (0,0) instead of the bottom left corner.\nPosition (out): Returns the position of the mouse as a Vector2.\nX Position (out): Returns just the X position of the mouse as a float.\nY Position (out): Returns just the Y position of the mouse as a float.\nPosition (Vector 3) (out): Returns the position of the mouse as a Vector3.")]
 [NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
 [NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Get_Mouse_Position")]
 
@@ -20,9 +20,35 @@ public class uScriptAct_GetMousePosition : uScriptLogic
    public bool Out { get { return true; } }
 
    public void In(
-      [FriendlyName("Position")] out Vector3 position
+      [FriendlyName("Invert Y"), DefaultValue(true), SocketState(false, false)] bool InvertY,
+	  [FriendlyName("Position")] out Vector2 positionV2,
+	  [FriendlyName("X Position")] out float XPosition,
+	  [FriendlyName("Y Position")] out float YPosition,
+	  [FriendlyName("Position (Vector 3)"), SocketState(false, false)] out Vector3 position
    )
    {
-      position = Input.mousePosition;
+		Vector3 tmpPosition = Input.mousePosition;
+		float XPos = tmpPosition.x;
+		float YPos;
+		
+		if (InvertY)
+		{
+			
+		    YPos = Screen.height - tmpPosition.y;
+			positionV2 = new Vector2(XPos, YPos);
+		    position = new Vector3(XPos, YPos, tmpPosition.z);
+			YPosition = YPos;
+			XPosition = XPos;
+		}
+		else
+		{
+		    YPos = tmpPosition.y;
+			positionV2 = new Vector2(XPos, YPos);
+		    position = tmpPosition;
+			YPosition = YPos;
+			XPosition = XPos;
+		}
+		
+		
    }
 }
