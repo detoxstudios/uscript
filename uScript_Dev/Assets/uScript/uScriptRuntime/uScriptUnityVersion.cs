9f849156b61f433649e0c2e282cdc922876090f5
Trapper McFerron <trapper@detoxstudios.com>
2011-08-20 05:02:04 +0000

    changed 4_0 to 3_4
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1142 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptRuntime/uScriptUnityVersion.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/uScriptUnityVersion.cs
@@ -9,8 +9,8 @@ public class uScriptUnityVersion : uScriptIUnityVersion
 	public float Version { get { return 3.2f; } }
 #elif UNITY_3_3
 	public float Version { get { return 3.3f; } }
-#elif UNITY_4_0
-	public float Version { get { return 4.0f; } }
+#elif UNITY_3_4
+	public float Version { get { return 3.4f; } }
 #else
  	public float Version { get { return 0.0f; } }
 #endif
