114a021c6ab8814a52639e9ef2246e863518defe
Scott Blinn <scott@detoxstudios.com>
2011-09-05 00:50:47 +0000

    Added that new helper component to send uScript graph's Custom Event messages. Updated DLL project to contain new component.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1214 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Components/uScript_BroadcastCustomEvent.cs
@@ -0,0 +1,15 @@
+// Copyright 2011 Detox Studios LLC
+// Desc: Assign this component to GameObjects you wish to have broadcast
+// Custom Events that uScript can listen for.
+
+using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("uScript/Events/Broadcast Custom Event")]
+public class uScript_BroadcastCustomEvent : uScriptEvent
+{
+   void uScript_Broadcast_Custom_Event(string eventName)
+   {
+      uScriptCustomEvent.BroadcastCustomEvent(eventName, null, gameObject);
+   }
+}
