7d09c56b2d5c345de1cd562d8473af1dce7cf20f
Michael Ryan <michael@detoxstudios.com>
2011-09-09 18:59:31 +0000

    Added DefaultValue support for Color, Vector3, and Vector4 using fewer than the maximum supported parameters.  For example, you can pass 3 or 4 floats to Color().  The Color(r, g, b) constructor automatically fills in 1 as the alpha.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1242 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptRuntime/Components/uScript_Runtime.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Components/uScript_Runtime.cs
@@ -49,11 +49,20 @@ public class DefaultValue : Attribute
    public DefaultValue(Type t, float[] f)
    {
       // Custom handlers for specific Unity data types
-      if (t == typeof(Vector2) && f.Length == 2)      Default = new Vector2(f[0], f[1]);
-      else if (t == typeof(Vector3) && f.Length == 3) Default = new Vector3(f[0], f[1], f[2]);
-      else if (t == typeof(Vector4) && f.Length == 4) Default = new Vector4(f[0], f[1], f[2], f[3]);
+      if (t == typeof(Rect) && f.Length == 4)         Default = new Rect(f[0], f[1], f[2], f[3]);
+
+      else if (t == typeof(Color) && f.Length == 3)   Default = new Color(f[0], f[1], f[2]);
       else if (t == typeof(Color) && f.Length == 4)   Default = new Color(f[0], f[1], f[2], f[3]);
-      else if (t == typeof(Rect) && f.Length == 4)    Default = new Rect(f[0], f[1], f[2], f[3]);
+
+      else if (t == typeof(Vector2) && f.Length == 2) Default = new Vector2(f[0], f[1]);
+
+      else if (t == typeof(Vector3) && f.Length == 2) Default = new Vector3(f[0], f[1]);
+      else if (t == typeof(Vector3) && f.Length == 3) Default = new Vector3(f[0], f[1], f[2]);
+
+      else if (t == typeof(Vector4) && f.Length == 2) Default = new Vector4(f[0], f[1]);
+      else if (t == typeof(Vector4) && f.Length == 3) Default = new Vector4(f[0], f[1], f[2]);
+      else if (t == typeof(Vector4) && f.Length == 4) Default = new Vector4(f[0], f[1], f[2], f[3]);
+
       else
          Debug.LogError("Unhandled DefaultValue type and float[] length pair:\n\t" + t.ToString() + " cannot have " + f.Length.ToString() + " parameters or the type isn't yet supported.\n");
    }
