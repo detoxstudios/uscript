03daad9c74fc0dd68ebe2a62d891744b46f68469
Scott Blinn <scott@detoxstudios.com>
2011-09-17 00:18:50 +0000

    Tools to create the watermark byte data to embed in uScript_MasterComponent.cs
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1268 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/DetoxDevTools/PLE_Watermark_Creation/GetWatermarkData.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class GetWatermarkData : MonoBehaviour
+{
+
+   private bool keybool = true;
+
+
+   // Use this for initialization
+   void Start()
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+
+      if (Input.GetKey("up") & keybool)
+      {
+         keybool = false;
+         Debug.Log("Creating Watermark byte text file...\n");
+         // Import the texture data
+         
+         // Take the .PNG file and rename the exstension to ".bytes"
+         // Specify the filename to use:
+         string ImageFileName = "uScript_WatermarkBytes"; // uScript_WatermarkBytes.bytes
+         TextAsset WatermarkDataOut = Resources.Load(ImageFileName) as TextAsset;
+
+         // Write out the bytes to a text file:
+         string allBytes = "";
+         foreach (byte singleByte in WatermarkDataOut.bytes)
+         {
+            allBytes = allBytes + singleByte.ToString() + ",";
+         }
+         using (StreamWriter writer = new StreamWriter("WatermarkDataOutput.txt")) // File will appear in root project folder.
+         {
+            writer.Write(allBytes);
+         }
+         Debug.Log("File created!\n");
+         keybool = true;
+      }
+
+   }
+
+
+  
+}
