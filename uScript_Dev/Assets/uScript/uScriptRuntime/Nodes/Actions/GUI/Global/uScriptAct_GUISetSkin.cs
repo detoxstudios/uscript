3a800e81246b8c4a80e9f4e2084da50bada12183
Anthony Silva <anthony@detoxstudios.com>
2011-09-20 21:02:10 +0000

    Add node to set gui skin.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1286 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/GUI/Global/uScriptAct_GUISetSkin.cs
@@ -0,0 +1,25 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+// Desc: Sets the current skin for the GUI.
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/GUI/Global")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Sets the current skin for the GUI.")]
+[NodeDescription("Sets the current skin for the GUI. NOTE: This skin selection only lasts for the current frame or until another skin is set.\n \nSkin: The skin to render the GUI with.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#GUI_Set_Skin")]
+
+[FriendlyName("GUI Set Skin")]
+public class uScriptAct_GUISetSkin : uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public void In([FriendlyName("Skin")] GUISkin skin)
+   {
+      GUI.skin = skin;
+   }
+}
