a34d6df91964b5adedc64f6172da8a9119fa6fba
Scott Blinn <scott@detoxstudios.com>
2011-10-06 05:37:50 +0000

    New Node - Set Layer
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1336 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/GameObjects/uScriptAct_SetLayer.cs
@@ -0,0 +1,70 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+// Desc: Sets the layer for the target GameObjects.
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[NodePath("Actions/GameObjects")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Sets the layer for the target GameObjects.")]
+[NodeDescription("Sets the layer for the target GameObjects.\n\nTarget: The GameObject(s) you wish to set the layer for.\nLayer: The Layer you wish to set the Target(s) to.\nApply To Children: Specify if the Layer should also be assigned to any children GameObjects of the Target if found.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Get_GameObject_By_Name")]
+
+[FriendlyName("Set Layer")]
+public class uScriptAct_SetLayer : uScriptLogic
+{
+   private bool m_ApplyToChildren;
+	public bool Out { get { return true; } }
+
+   public void In(
+                   [FriendlyName("Target")] GameObject[] Target,
+                   [FriendlyName("Layer")] LayerMask Layer,
+	               [FriendlyName("Apply To Children"), SocketState(false, false), DefaultValue(true)] bool ApplyToChildren
+                   )
+   {
+		m_ApplyToChildren = ApplyToChildren;
+		
+      foreach (GameObject obj in Target)
+	  {
+			Transform objTrans = obj.transform;
+	     SetGameObjectLayer(objTrans, Layer.value);
+	  }
+		
+   }
+	
+	
+   private void SetGameObjectLayer(Transform obj, int newLayer)
+   {
+      obj.gameObject.layer = newLayer;
+		
+		if(m_ApplyToChildren)
+		{
+			foreach(Transform child in obj)
+			{
+				SetGameObjectLayer(child, newLayer);
+			}
+		}
+   }
+	
+}
+
+
+
+
+
+/*
+	function SetLayerRecursively( obj : GameObject, newLayer : int  )
+{
+	obj.layer = newLayer;
+	
+	for( var child : Transform in obj.transform )
+	{
+		SetLayerRecursively( child.gameObject, newLayer );
+	}
+}
+	
+	*/
