0701a1274b1029632d1db28cffd40ca24ddf4f63
Trapper McFerron <trapper@detoxstudios.com>
2011-10-10 04:50:05 +0000

    Fixed a bug where NodeDescription and NodeToolTip were reversed
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1372 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Events/uScript_ProxyController.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Events/uScript_ProxyController.cs
@@ -10,8 +10,8 @@ using System.Collections;
 [NodePath("Events/Physics Events")]
 
 [NodeCopyright("Copyright 2011 by Detox Studios LLC")]
-[NodeToolTip("Fires an event signal when a CharacterController collides with an object.\n \nCharacter Controller: The controller that hit Collider.\nCollider: The collider that was hit by Character Controller.\nRigid Body: The rigidbody that was hit by Character Controller.\nInstigator: The GameObject that was hit by Character Controller.\nTransform: The transform that was hit by Character Controller.\nPoint: The impact point in world space.\nNormal: The normal of the surface we collided with in world space.\nMove Direction: Approximately the direction from the center of the capsule to the point we touch.\nMove Length: How far the character has travelled until it hit the collider.\n")]
-[NodeDescription("Fires an event signal when a CharacterController collides with an object.")]
+[NodeToolTip("Fires an event signal when a CharacterController collides with an object.")]
+[NodeDescription("Fires an event signal when a CharacterController collides with an object.\n \nCharacter Controller: The controller that hit Collider.\nCollider: The collider that was hit by Character Controller.\nRigid Body: The rigidbody that was hit by Character Controller.\nInstigator: The GameObject that was hit by Character Controller.\nTransform: The transform that was hit by Character Controller.\nPoint: The impact point in world space.\nNormal: The normal of the surface we collided with in world space.\nMove Direction: Approximately the direction from the center of the capsule to the point we touch.\nMove Length: How far the character has travelled until it hit the collider.\n")]
 [NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
 [NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Controller_Collision")]
 
