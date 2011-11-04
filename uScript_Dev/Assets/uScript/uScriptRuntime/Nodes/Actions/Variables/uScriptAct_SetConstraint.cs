0da709fb9e8360a4d24f935291df904056d1b0d1
Trapper McFerron <trapper@detoxstudios.com>
2011-11-04 05:44:02 +0000

    Updated SetConstraints to take an array of constraints
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1410 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Variables/uScriptAct_SetConstraint.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Variables/uScriptAct_SetConstraint.cs
@@ -8,8 +8,8 @@ using System.Collections;
 [NodePath("Actions/Variables/Constraint")]
 
 [NodeCopyright("Copyright 2011 by Detox Studios LLC")]
-[NodeToolTip("Sets the value of a Constraint variable using the value of another Constraint variable.")]
-[NodeDescription("Sets the value of a Constraint variable using the value of another Constraint variable.n \nValue: The variable you wish to use to set the target's value.\nTarget (out): The Target variable you wish to set.")]
+[NodeToolTip("Sets the value(s) of a Constraint variable using the value of another Constraint variable.")]
+[NodeDescription("Sets the value(s) of a Constraint variable using the value of another Constraint variable.n \nValue: The variable(s) you wish to use to set the target's value.\nTarget (out): The Target variable you wish to set.")]
 [NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
 [NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
 
@@ -18,8 +18,13 @@ public class uScriptAct_SetConstraint : uScriptLogic
 {
    public bool Out { get { return true; } }
 
-   public void In(UnityEngine.RigidbodyConstraints Value, [FriendlyName("Target")] out UnityEngine.RigidbodyConstraints Target)
+   public void In(UnityEngine.RigidbodyConstraints []Value, [FriendlyName("Target")] out UnityEngine.RigidbodyConstraints Target)
    {
-      Target = Value;
+      Target = RigidbodyConstraints.None;
+
+      foreach ( UnityEngine.RigidbodyConstraints c in Value )
+      {
+         Target |= c;
+      }
    }
 }
