ef3a64faedbc06c95f817328d2d67c61a7b96239
Michael Ryan <michael@detoxstudios.com>
2011-12-02 00:35:57 +0000

    The reference text for this node stated that the Max value was exclusive, but the code added 1 to the value prior to calling Random.Range(min, max).
    I updated the text to say that it is inclusive, and removed a redundant check to make sure min <= max.
    
    It Max really should be exclusive, let me know, and I'll swtich it around.
    
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1473 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Variables/uScriptAct_SetRandomInt.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Variables/uScriptAct_SetRandomInt.cs
@@ -8,23 +8,29 @@ using System.Collections;
 
 [NodeCopyright("Copyright 2011 by Detox Studios LLC")]
 [NodeToolTip("Randomly sets the value of a Int variable.")]
-[NodeDescription("Randomly sets the value of a Int variable.\n \nMin: Minimum allowable integer value.\nMax: Maximum allowable integer value. This value is exclusive - that means if you want it to show up in random results, you will need to add 1 to it.\nTarget Integer (out): The integer value that gets set.")]
 [NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
 [NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Set_Random_Int")]
 
-[FriendlyName("Set Random Int")]
+[FriendlyName("Set Random Int", "Randomly sets the value of a Int variable.")]
 public class uScriptAct_SetRandomInt : uScriptLogic
 {
    public bool Out { get { return true; } }
 
    public void In(
-      [FriendlyName("Min"), SocketState(false, false)] int Min,
-      [FriendlyName("Max"), SocketState(false, false)] int Max,
-      [FriendlyName("Target Int")] out int TargetInt)
+      [FriendlyName("Min", "Minimum allowable integer value.")]
+      [SocketState(false, false)]
+      int Min,
+
+      [FriendlyName("Max", "Maximum allowable integer value. This value is inclusive and may be returned.")]
+      [SocketState(false, false)]
+      int Max,
+      
+      [FriendlyName("Target Int", "The integer value that gets set.")]
+      out int TargetInt
+      )
    {
       // Make sure we don't have min > max (or other way around)
       if (Min > Max) { Min = Max; }
-      if (Max < Min) { Max = Min; }
 
       TargetInt = Random.Range(Min, Max + 1);
    }
