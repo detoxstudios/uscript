5a9cdbeb0c27607caaf71759a01a4a67466647b1
Trapper McFerron <trapper@detoxstudios.com>
2011-12-09 07:20:57 +0000

    Dot product nodes
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1483 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Math/uScriptAct_DotVector3.cs
@@ -0,0 +1,33 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/Math")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip( "Dot product of two Vector3 variables.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Dot_Vector3")]
+
+[FriendlyName("Dot Vector3", "Dot Product of two Vector3 variables." +
+ "\n\n[ Dot(A,B) ]")]
+public class uScriptAct_DotVector3 : uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public void In(
+      [FriendlyName("A", "The first variable.")]
+      Vector3 A,
+
+      [FriendlyName("B", "The second variable.")]
+      Vector3 B,
+
+      [FriendlyName("Result", "The float result of the operation.")]
+      out float Result
+      )
+   {
+      Result = Vector3.Dot(A, B);
+   }
+}
---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Math/uScriptAct_DotVector4.cs
@@ -0,0 +1,33 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/Math")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip( "Dot product of two Vector4 variables.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Dot_Vector4")]
+
+[FriendlyName("Dot Vector4", "Dot Product of two Vector4 variables." +
+ "\n\n[ Dot(A,B) ]")]
+public class uScriptAct_DotVector4 : uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public void In(
+      [FriendlyName("A", "The first variable.")]
+      Vector4 A,
+
+      [FriendlyName("B", "The second variable.")]
+      Vector4 B,
+
+      [FriendlyName("Result", "The float result of the operation.")]
+      out float Result
+      )
+   {
+      Result = Vector4.Dot(A, B);
+   }
+}
