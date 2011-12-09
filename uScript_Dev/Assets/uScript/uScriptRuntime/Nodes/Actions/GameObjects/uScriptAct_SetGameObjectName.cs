292a3f8f8c95eae857e283a8fef27a9819687f64
Trapper McFerron <trapper@detoxstudios.com>
2011-12-09 20:15:32 +0000

    Added node to change the name of a GameObject
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1486 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/GameObjects/uScriptAct_SetGameObjectName.cs
@@ -0,0 +1,29 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/GameObjects")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip( "Sets a GameObject's name.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Set_GameObject_Name")]
+
+[FriendlyName("Set GameObject Name", "Sets the name of a GameObject.")]
+public class uScriptAct_SetGameObjectName: uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public void In(
+      [FriendlyName("Target", "The GameObject's name to change.")]
+      GameObject Target,
+
+      [FriendlyName("Name", "The new name.")]
+      string Name
+      )
+   {
+      Target.name = Name;
+   }
+}
