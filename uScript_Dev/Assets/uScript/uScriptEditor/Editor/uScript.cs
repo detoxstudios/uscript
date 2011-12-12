b089ac0a830aaf4ee1fabca3a224d23a8ed38f8f
Scott Blinn <scott@detoxstudios.com>
2011-12-12 15:27:15 +0000

    Updated the build number for Retail Beta 8
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1505 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptEditor/Editor/uScript.cs
+++ uScript_Dev/Assets/uScript/uScriptEditor/Editor/uScript.cs
@@ -42,7 +42,7 @@ public class uScript : EditorWindow
    private ComplexData m_ComplexData = null;
 
    //format is MAJOR.MINOR.FOURDIGITSVNCOMMITNUMBER
-   public string BuildNumber { get { return "0.9.1501"; } }
+   public string BuildNumber { get { return "0.9.1505"; } }
 #if FREE_PLE_BUILD
     static string ProductName { get { return "Personal Learning Edition"; } }
 #else
