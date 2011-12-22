aac58e00d36e7d2c54191964e3375f8f91013160
Scott Blinn <scott@detoxstudios.com>
2011-12-22 02:32:12 +0000

    New node - Get Transform
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1528 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/GameObjects/uScriptAct_GetTransform.cs
@@ -0,0 +1,35 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[NodePath("Actions/GameObjects")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Returns the transform of a GameObject.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
+
+[FriendlyName("Get Transform", "Returns the transform of a GameObject.")]
+public class uScriptAct_GetTransform : uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public void In (
+                   [FriendlyName("Target", "The GameObject you wish to get the transform for.")] GameObject Target,
+                   [FriendlyName("Transform", "The transform of the target GameObject.")] out Transform targetTransform)
+   {
+      
+      if ( null != Target )
+      {
+         targetTransform = Target.transform;
+      }
+      else
+      {
+         uScriptDebug.Log ("(Node - Get Transform): The specified Target GameObject was null.", uScriptDebug.Type.Warning);
+         targetTransform = new Transform();
+      }
+   }
+}
