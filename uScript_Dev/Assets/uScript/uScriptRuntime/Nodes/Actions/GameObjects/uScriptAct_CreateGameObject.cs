5d68cb6c9b8dba35600f4df47b8912ffc15f2109
Scott Blinn <scott@detoxstudios.com>
2011-12-24 00:06:48 +0000

    New node - Create GameObject - creates a new/empty GameObject of the specified name in the specified location.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1538 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/GameObjects/uScriptAct_CreateGameObject.cs
@@ -0,0 +1,28 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/GameObjects")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Creates a new GameObject at the specified location.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
+
+[FriendlyName("Create GameObject", "Creates a new GameObject at the specified location.")]
+public class CreateGameObject: uScriptLogic
+{
+	public bool Out { get { return true; } }
+	
+	public void In (
+	                [FriendlyName("Name", "The name given to the new GameObject.")] string Name,
+	                [FriendlyName("Location", "The world location where to place the new GameObject."), SocketState(false, false)] Vector3 Location,
+	                [FriendlyName("GameObject", "The new GameObject.")] out GameObject newGameObject
+	                )
+	{
+		newGameObject = new GameObject(Name);
+		newGameObject.transform.position = Location;
+	}
+}
