2395c0e88d3f1b085cdd18a9855e715469432d03
Michael Ryan <michael@detoxstudios.com>
2012-01-06 20:24:03 +0000

    "GUI Selection Grid" allows for the creation of a grid of buttons, where only a single button in the grid can be selected at one time.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1597 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/GUI/Controls/uScriptAct_GUISelectionGrid.cs
@@ -0,0 +1,72 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/GUI/Controls")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Make a grid of buttons.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#GUI_Text_Area")]
+
+[FriendlyName("GUI Selection Grid", "Make a grid of buttons.")]
+public class uScriptAct_GUISelectionGrid : uScriptLogic
+{
+/*
+   TODO: Add support for passing Texture[] and GUIContent[] to the buttons
+*/
+
+   [FriendlyName("Changed")]
+   public bool Changed { get { return m_Changed; } }
+   private bool m_Changed = false;
+
+   public bool Out { get { return true; } }
+
+   public void In(
+      [FriendlyName("Selected", "The index of the selected grid button.")]
+      ref int Value,
+      
+      [FriendlyName("Position", "Rectangle on the screen to use for the grid.")]
+      Rect Position,
+
+      [FriendlyName("Text Content", "An array of strings to show on the grid buttons.")]
+      string[] Content,
+
+      [FriendlyName("xCount", "How many elements to fit in the horizontal direction. The controls will be scaled to fit unless the style defines a fixedWidth to use.")]
+      [DefaultValue(50)]
+      int xCount,
+      
+      [FriendlyName("Control Name", "Name to give to this text area GUI control.")]
+      [DefaultValue(""), SocketState(false, false)]
+      string ControlName,
+
+      [FriendlyName("GUI Style", "The name of a custom GUI style to use when displaying this text aera.")]
+      [DefaultValue(""), SocketState(false, false)]
+      string guiStyle
+      )
+   {
+      int value;
+      m_Changed = false;
+
+      if (!string.IsNullOrEmpty(ControlName)) GUI.SetNextControlName(ControlName);
+
+      if (string.IsNullOrEmpty(guiStyle))
+      {
+         value = GUI.SelectionGrid(Position, Value, Content, xCount);
+      }
+      else
+      {
+         value = GUI.SelectionGrid(Position, Value, Content, xCount, GUI.skin.GetStyle(guiStyle));
+      }
+
+      // changed event
+      if (Value != value)
+      {
+         m_Changed = true;
+      }
+
+      Value = value;
+   }
+}
