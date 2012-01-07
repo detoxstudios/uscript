daa312c2639de0403a96aba56be1323366784371
Michael Ryan <michael@detoxstudios.com>
2012-01-07 09:07:39 +0000

    "Escape String" and "UnEscape String" allow for the conversion to and from URL-safe strings.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1605 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Web/uScriptAct_EscapeURL.cs
@@ -0,0 +1,55 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/Web/String")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Encodes string into a URL-friendly format.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#EscapeString")]
+
+[FriendlyName("Escape String", "Encodes string into a URL-friendly format by replacing illegal" +
+   " characters in the Target string with the correct URL-escaped code. Useful when building web page parameters.")]
+public class uScriptAct_EscapeURL : uScriptLogic
+{
+   // ================================================================================
+   //    Output Sockets
+   // ================================================================================
+   //
+   [FriendlyName("Out")]
+   public bool Out { get { return true; } }
+
+
+   // ================================================================================
+   //    Input Sockets and Node Parameters
+   // ================================================================================
+   //
+   // Parameter Attributes are applied below in Reset()
+   public void In(
+      [FriendlyName("Target", "The string to be escaped.")]
+      string Target,
+
+      [FriendlyName("Result", "A new string with all illegal characters replaced with %xx where xx is the hexadecimal code for the character code.")]
+      out string Result
+      )
+   {
+      if (Target == null)
+      {
+         Result = string.Empty;
+      }
+      else
+      {
+         Result = WWW.EscapeURL(Target);
+      }
+   }
+
+
+   // ================================================================================
+   //    Miscellaneous Node Funtionality
+   // ================================================================================
+   //
+
+}
---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Web/uScriptAct_UnEscapeURL.cs
@@ -0,0 +1,54 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/Web/String")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Decodes string from a URL-friendly format.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#UnEscapeString")]
+
+[FriendlyName("UnEscape String", "Decodes string from a URL-friendly format.")]
+public class uScriptAct_UnEscapeURL : uScriptLogic
+{
+   // ================================================================================
+   //    Output Sockets
+   // ================================================================================
+   //
+   [FriendlyName("Out")]
+   public bool Out { get { return true; } }
+
+
+   // ================================================================================
+   //    Input Sockets and Node Parameters
+   // ================================================================================
+   //
+   // Parameter Attributes are applied below in Reset()
+   public void In(
+      [FriendlyName("Target", "The URL-escaped string to be converted.")]
+      string Target,
+
+      [FriendlyName("Result", "A new string with all occurrences of %xx replaced with the corresponding character.")]
+      out string Result
+      )
+   {
+      if (Target == null)
+      {
+         Result = string.Empty;
+      }
+      else
+      {
+         Result = WWW.UnEscapeURL(Target);
+      }
+   }
+
+
+   // ================================================================================
+   //    Miscellaneous Node Funtionality
+   // ================================================================================
+   //
+
+}
