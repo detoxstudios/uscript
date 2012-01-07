2828b754bb9c4a7561856f53a26447bd3ba16f93
Michael Ryan <michael@detoxstudios.com>
2012-01-07 09:14:08 +0000

    "Form Update Field" allows the user to create WWWForm data for web POST requests.  This node handle simple text data.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1606 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Web/uScriptAct_FormUpdateField.cs
@@ -0,0 +1,37 @@
+// uScript Action Node
+// (C) 2010 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/Web/Form")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Add a simple field to the form.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#WebFormUpdateField")]
+
+[FriendlyName("Form Update Field", "Add a simple field to the form.")]
+public class uScriptAct_FormUpdateField : uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public void In(
+      [FriendlyName("Form", "The Form to modify.")]
+      ref WWWForm Form,
+
+      [FriendlyName("Field Name", "The field name.")]
+      string Field,
+
+      [FriendlyName("Value", "The field value. Non-string objects will be convertd to a string using ToString().")]
+      object Value
+      )
+   {
+      if (Form == null)
+      {
+         Form = new WWWForm();
+      }
+
+      Form.AddField(Field, Value.ToString());
+   }
+}
