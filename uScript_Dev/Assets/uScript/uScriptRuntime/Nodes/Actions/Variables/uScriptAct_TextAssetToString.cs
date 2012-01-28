db0c5d10b187f3a2800f2cdaaaf690b92a17dd28
Scott Blinn <scott@detoxstudios.com>
2012-01-28 15:23:05 +0000

    TextAsset To String - new node that allows you to dump the contents of a TextAsset into a string to it can be used by our string manipulation nodes.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1649 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Variables/uScriptAct_TextAssetToString.cs
@@ -0,0 +1,33 @@
+// uScript Action Node
+// (C) 2010 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/Variables/TextAsset")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Converts a TextAsset into a string.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
+
+[FriendlyName("TextAsset To String", "Converts a TextAsset into a string. It will also return the name of the TextAsset.")]
+public class uScriptAct_TextAssetToString : uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public void In(
+      [FriendlyName("Value", "The variable you wish to use to set the target's value.")]
+      TextAsset Value,
+      
+      [FriendlyName("Target", "The Target variable you wish to set.")]
+      out string Target,
+	  
+	  [FriendlyName("Name", "The name of the TextAsset."), SocketState(false, false)]
+      out string FileName
+      )
+   {
+       Target = Value.text;
+	   FileName = Value.name;
+   }
+}
