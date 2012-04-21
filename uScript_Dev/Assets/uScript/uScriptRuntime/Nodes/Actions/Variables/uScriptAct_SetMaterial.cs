6da99dda79f6ca19fdf7ff2cd75e95ea66579c63
Scott Blinn <scott@detoxstudios.com>
2012-04-21 15:30:20 +0000

    Created Set Material node.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1805 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Variables/uScriptAct_SetMaterial.cs
@@ -0,0 +1,29 @@
+// uScript Action Node
+// (C) 2010 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/Variables/Material")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Sets the value of a Color variable using the value of another Color variable.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
+
+[FriendlyName("Set Material", "Sets the value of a Material variable using the value of another Material variable.")]
+public class uScriptAct_SetMaterial : uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public void In(
+      [FriendlyName("Value", "The variable you wish to use to set the target's value.")]
+      Material Value,
+      
+      [FriendlyName("Target", "The Target variable you wish to set.")]
+      out Material TargetMat
+      )
+   {
+      TargetMat = Value;
+   }
+}
