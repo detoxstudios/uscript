c8f37b6e085826990d093bf07bb5c3f03b81338e
Scott Blinn <scott@detoxstudios.com>
2012-05-06 00:25:58 +0000

    New nodes: added Get List Size nodes for all supported list types.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1836 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Variables/Lists/uScriptAct_GetListSizeTexture2D.cs
@@ -0,0 +1,33 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[NodePath("Actions/Variables/Lists/Texture2D")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Get the number of things currently in the list.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
+
+[FriendlyName("Get List Size (Texture2D)", "Get the number of things currently in the list.")]
+public class uScriptAct_GetListSizeTexture2D : uScriptLogic
+{
+
+   public bool Out { get { return true; } }
+
+   public void In(
+      [FriendlyName("Target", "The list to get the size on.")]
+      Texture2D[] List,
+
+      [FriendlyName("Size", "The size of the list specified.")]
+      out int ListSize
+      )
+   {
+      ListSize = List.Length;
+   }
+
+}
