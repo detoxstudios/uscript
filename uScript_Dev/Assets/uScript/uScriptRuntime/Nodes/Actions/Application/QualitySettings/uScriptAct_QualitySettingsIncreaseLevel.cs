d19e4b22575e19413126179dedf3924d7a3a1dba
Scott Blinn <scott@detoxstudios.com>
2012-05-06 20:13:52 +0000

    Fixed a bug in the socket name.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1846 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Application/QualitySettings/uScriptAct_QualitySettingsIncreaseLevel.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Application/QualitySettings/uScriptAct_QualitySettingsIncreaseLevel.cs
@@ -17,7 +17,7 @@ public class uScriptAct_QualitySettingsIncreaseLevel : uScriptLogic
    public bool Out { get { return true; } }
 
    public void In(
-      [FriendlyName("Should expensive changes be applied (Anti-aliasing, etc.). Applying some changes the quality level can be an expensive operation if the new level has a different anti-aliasing setting. It's fine to change the level when applying in-game quality options, but if you want to dynamically adjust quality level at runtime, it is best to have this unchecked so that expensive changes are not always applied.")]
+      [FriendlyName("Apply Expensive Changes", "Should expensive changes be applied (Anti-aliasing, etc.). Applying some changes the quality level can be an expensive operation if the new level has a different anti-aliasing setting. It's fine to change the level when applying in-game quality options, but if you want to dynamically adjust quality level at runtime, it is best to have this unchecked so that expensive changes are not always applied.")]
       [SocketState(false, false)]
       [DefaultValue(false)]
       bool applyExpensiveChanges
