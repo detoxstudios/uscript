cdd68ef9495b36eb94efbcc8b3cede629e57d37f
Scott Blinn <scott@detoxstudios.com>
2012-05-06 18:04:44 +0000

    New nodes: Cross (Vector3) Get Angle (Vector2) Get Angle (Vector3)
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1840 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Math/uScriptAct_CrossVector3.cs
@@ -0,0 +1,33 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/Math/Vectors")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip( "Cross product of two Vector3 variables.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
+
+[FriendlyName("Cross (Vector3)", "Cross Product of two Vector3 variables." +
+ "\n\n[ Cross(A,B) ]")]
+public class uScriptAct_CrossVector3 : uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public void In(
+      [FriendlyName("A", "The first variable.")]
+      Vector3 A,
+
+      [FriendlyName("B", "The second variable.")]
+      Vector3 B,
+
+      [FriendlyName("Result", "The resulting cross product.")]
+      out Vector3 Result
+      )
+   {
+      Result = Vector3.Cross(A, B);
+   }
+}
---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Math/uScriptAct_GetAngleVector3.cs
@@ -0,0 +1,26 @@
+// uScript Action Node
+// (C) 2010 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/Math/Angles")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Returns the angle in degrees between target A and target B.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
+
+[FriendlyName("Get Angle (Vector3)", "Returns the angle in degrees between target A and target B.")]
+public class uScriptAct_GetAngleVector3 : uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public void In(
+      [FriendlyName("A", "The first target.")] Vector3 A,
+      [FriendlyName("B", "The second target.")] Vector3 B,
+      [FriendlyName("Angle", "The resulting angle between the two targets in degrees.")] out float Angle)
+   {
+      Angle = Vector3.Angle(A, B);
+   }
+}
