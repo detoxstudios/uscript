673dddcf1b4747830b67fb9ee345b83b7c3ac989
Scott Blinn <scott@detoxstudios.com>
2012-05-06 18:01:00 +0000

    New nodes: added Get Axis and Get Button input filter nodes.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1837 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Event/Filters/uScriptAct_GetAxis.cs
@@ -0,0 +1,34 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/Events/Filters")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Returns the value of the virtual axis identified by Axis Name.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
+
+[FriendlyName("Get Axis", "Returns the value of the virtual axis identified by Axis Name. The value will be in the range -1...1 for keyboard and joystick input. If the axis is setup to be delta mouse movement, the mouse delta is multiplied by the axis sensitivity and the range is not -1...1. \n\nFor use with the Input Events event node.")]
+public class uScriptAct_GetAxis : uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public void In(
+      [FriendlyName("Axis Name")]
+      string axisName,
+
+      [FriendlyName("Result", "Returns the value of the virtual axis identified by axisName. Examples - 'Verticle' and 'Mouse X'")]
+      out float result,
+
+      [FriendlyName("Raw Result", "Returns the value of the virtual axis identified by axisName with no smoothing filtering applied. Since input is not smoothed, keyboard input will always be either -1, 0 or 1. This is useful if you want to do all smoothing of keyboard input processing yourself.")]
+      [SocketState(false, false)]
+      out float rawResult
+      )
+   {
+      result = Input.GetAxis(axisName);
+      rawResult = Input.GetAxisRaw(axisName);
+   }
+}
---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Event/Filters/uScriptAct_GetButton.cs
@@ -0,0 +1,29 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/Events/Filters")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Returns true while the virtual button identified by Button Name is held down.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
+
+[FriendlyName("Get Button", "Returns true while the virtual button identified by Button Name is held down. This will return true as long as the button is held down. Note, for detecting standard keyboard and mouse button presses, you should use the Input Filter node.\n\nFor use with the Input Events event node.")]
+public class uScriptAct_GetButton : uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public void In(
+      [FriendlyName("Button Name", "The name of the virtual button. Example - 'Fire1'")]
+      string buttonName,
+      
+      [FriendlyName("Result", "Returns true while the virtual button specified is held down. Think auto fire - this will return true as long as the button is held down.")]
+      out bool result
+      )
+   {
+      result = Input.GetButton(buttonName);
+   }
+}
