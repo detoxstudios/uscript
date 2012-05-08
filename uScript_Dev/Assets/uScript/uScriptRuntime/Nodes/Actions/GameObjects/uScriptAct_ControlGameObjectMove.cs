4d899618c9a0d91e5b81ec6e922eefb5439db9e7
Scott Blinn <scott@detoxstudios.com>
2012-05-08 22:51:17 +0000

    Two new nodes: Control GameObject (Move) Control GameObject (Rotate)
    Now we just need a simple Jump one and people should be able to use them in interesting ways...
    
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1856 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/GameObjects/uScriptAct_ControlGameObjectMove.cs
@@ -0,0 +1,66 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/GameObjects/Movement")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Moves a GameObject in the specified direction.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
+
+[FriendlyName("Control GameObject (Move)", "Moves a GameObject in the specified direction (local to the GameObject). Please note that this is a simple move node that brute-forces the movement of the GameObject's position-- it does not use the physics system. It is recomended you create your own game-specific character controller if you need more functionality.")]
+public class uScriptAct_ControlGameObjectMove : uScriptLogic
+{
+   public enum Direction { Forward, Backward, Left, Right, Up, Down };
+   public bool Out { get { return true; } }
+
+   public void In(
+      [FriendlyName("Target", "The Target GameObject to be moved.")]
+      GameObject Target,
+
+      [FriendlyName("Direction", "The direction to move the target.")]
+      [SocketState(false, false)]
+      Direction moveDirection,
+
+      [FriendlyName("Speed", "The speed you wish to move the target per tick. This uses a relativly small value for most cases.")]
+      [DefaultValue(0.01f)]
+      float Speed
+      )
+   {
+      if (null != Target && Speed != 0f)
+      {
+         switch (moveDirection)
+         {
+            case Direction.Forward:
+               Target.transform.Translate(Vector3.forward * Speed);
+               break;
+
+            case Direction.Backward:
+               Target.transform.Translate(Vector3.back * Speed);
+               break;
+
+            case Direction.Left:
+               Target.transform.Translate(Vector3.left * Speed);
+               break;
+
+            case Direction.Right:
+               Target.transform.Translate(Vector3.right * Speed);
+               break;
+
+            case Direction.Up:
+               Target.transform.Translate(Vector3.up * Speed);
+               break;
+
+            case Direction.Down:
+               Target.transform.Translate(Vector3.down * Speed);
+               break;
+
+            default:
+               break;
+         }
+      }
+   }
+}
---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/GameObjects/uScriptAct_ControlGameObjectRotate.cs
@@ -0,0 +1,66 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/GameObjects/Movement")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Turns a GameObject in the specified direction.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
+
+[FriendlyName("Control GameObject (Rotate)", "Turns a GameObject in the specified direction (on the local axis of the GameObject). Please note that this is a simple move node that brute-forces the rotation of the GameObject-- it does not use the physics system. It is recomended you create your own game-specific character controller if you need more functionality.")]
+public class uScriptAct_ControlGameObjectRotate : uScriptLogic
+{
+   public enum Direction { Forward, Backward, Left, Right, TiltLeft, TiltRight };
+   public bool Out { get { return true; } }
+
+   public void In(
+      [FriendlyName("Target", "The Target GameObject to be moved.")]
+      GameObject Target,
+
+      [FriendlyName("Direction", "The direction to move the target.")]
+      [SocketState(false, false)]
+      Direction rotateDirection,
+
+      [FriendlyName("Speed", "The speed you wish to move the target per tick. This uses a relativly small value for most cases.")]
+      [DefaultValue(0.1f)]
+      float Speed
+      )
+   {
+      if (null != Target && Speed != 0f)
+      {
+         switch (rotateDirection)
+         {
+            case Direction.Forward:
+               Target.transform.Rotate(Vector3.left * Speed);
+               break;
+
+            case Direction.Backward:
+               Target.transform.Rotate(Vector3.right * Speed);
+               break;
+
+            case Direction.Left:
+               Target.transform.Rotate(Vector3.up * Speed);
+               break;
+
+            case Direction.Right:
+               Target.transform.Rotate(Vector3.down * Speed);
+               break;
+
+            case Direction.TiltLeft:
+               Target.transform.Rotate(Vector3.forward * Speed);
+               break;
+
+            case Direction.TiltRight:
+               Target.transform.Rotate(Vector3.back * Speed);
+               break;
+
+            default:
+               break;
+         }
+      }
+   }
+}
