86108ed36f6551523af61bbdea5dc70d3bddfef0
Scott Blinn <scott@detoxstudios.com>
2012-05-09 17:35:54 +0000

    Updated the Set Tag node to only allow for a single Tag to be specified (Unity limitation).
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1859 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/GameObjects/uScriptAct_SetTag.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/GameObjects/uScriptAct_SetTag.cs
@@ -8,30 +8,24 @@ using System.Collections;
 
 [NodeCopyright("Copyright 2011 by Detox Studios LLC")]
 [NodeToolTip("Sets the tag of a GameObject.")]
-[NodeAuthor("Detox Studios LLC. Original node by SvdV on the uScript Community Forum", "http://www.detoxstudios.com")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
 [NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
 
-[FriendlyName("Set Tag", "Sets the tag of a GameObject. Tags must be declared in the tag manager before using them with this node.")]
+[FriendlyName("Set Tag", "Sets the tag of a GameObject. The tag must be created in Unity's tag manager before using them with this node. Use \"Untagged\" for the tag name if you wish to remove an existing tag.")]
 public class uScriptAct_SetTag : uScriptLogic
 {
    public bool Out { get { return true; } }
 
    public void In(
       [FriendlyName("Target", "The GameObject(s) you wish to assign the existing tag to.")]GameObject[] Target, 
-      [FriendlyName("Tag", "The tag(s) you wish to assign to the Target GameObject(s)")] string[] Tags)
+      [FriendlyName("Tag", "The tag(s) you wish to assign to the Target GameObject(s)")] string Tag)
    {
       foreach (GameObject obj in Target)
       {
          if (null != obj)
          {
-            foreach (string Tag in Tags)
-            {
-               obj.tag = Tag;
-            }
-            
+            obj.tag = Tag;
          }
-         
       }
-		
    }
 }
