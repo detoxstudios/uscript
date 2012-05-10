91c18b20e8fc4ea15af099a460e7152949cf96e6
Scott Blinn <scott@detoxstudios.com>
2012-05-10 05:26:55 +0000

    New Nodes: Multiply Vector3 With Float Devide Vector3 With Float
    Both are good fo doing things like multiplying/dividing a velocity against Delta Time.
    
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1865 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Math/uScriptAct_DivideVector3WithFloat.cs
@@ -0,0 +1,32 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/Math/Vectors")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Divides a Vector3 with a Float.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
+
+[FriendlyName("Divide Vector3 With Float", "Divides a Vector3 with a Float. This is useful for dividing things like Delta Time with a Vector3 velocity for example.")]
+public class uScriptAct_DivideVector3WithFloat : uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public void In(
+      [FriendlyName("Vector3", "The Vector3 you wish to divide with.")]
+      Vector3 targetVector3,
+
+      [FriendlyName("Float", "The Float you wish to divide with.")]
+      float targetFloat,
+
+      [FriendlyName("Result", "The Vector3 result of the operation.")]
+      out Vector3 Result
+      )
+   {
+      Result = targetVector3 / targetFloat;
+   }
+}
