2431adc7eb125308502ded1879be7ce00a0f4b18
Scott Blinn <scott@detoxstudios.com>
2012-05-12 17:20:01 +0000

    Updated the Delay node's description to inform about how it resets the countdown when a signal is received on the In socket.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1868 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Time/uScriptAct_Delay.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Time/uScriptAct_Delay.cs
@@ -11,17 +11,17 @@ using System.Collections;
 [NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
 [NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Delay")]
 
-[FriendlyName("Delay", "Delays execution of a script but can also fire off an immediate response.")]
+[FriendlyName("Delay", "Delays execution of a script but can also fire off an immediate response.\n\nNOTE: each time a signal is recieved on the In socket, the delay node will reset its countdown (start over). This can cause the Delayed Out socket to never fire if it is recieving signals on the In socket faster than the Duration specified.\n\nIf you are looking for a time-based filter for something that sends out a signal each tick (like On Update), you should use the Timed Gate node instead.")]
 public class uScriptAct_Delay : uScriptLogic
 {
    private float m_TimeToTrigger;
    private bool  m_DelayedOut;
    private bool  m_ImmediateOut;
 
-   [FriendlyName("Immediate Out")]
+   [FriendlyName("Immediate")]
    public bool Immediate { get { return m_ImmediateOut; } }
   
-   [FriendlyName("Delayed Out")]
+   [FriendlyName("After Delay")]
    public bool AfterDelay { get { return m_DelayedOut; } }
    
    [FriendlyName("In")]
