1b3ca2c935207a9363d7b6cb4b5cff36daeeff2d
Scott Blinn <scott@detoxstudios.com>
2012-05-12 17:46:03 +0000

    Updated the Play AudioSource nodes: 1. Updated descriptions 2. Removed volume and the Update Colume socket from these nodes as users should use reflected properties of the AudioSource to change/update anything they want (they have access to the AudioSource via reflection unlike the Play Sound nodes).
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1870 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Audio/uScriptAct_PlayAudioSource.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Audio/uScriptAct_PlayAudioSource.cs
@@ -12,7 +12,7 @@ using System.Collections.Generic;
 [NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
 [NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
 
-[FriendlyName("Play AudioSource", "Plays the specified AudioSource on the target GameObject(s). Playing the AudioSource will immediately replace any existing sound playing from it.\n\nNOTE: To use this node, you must have already setup an AudioSource component on the target GameObject(s). If you want to simply play a sound with default AudioSource settings without needing to have an existing AudioSource component on the GameObject, use the Play Sound node.")]
+[FriendlyName("Play AudioSource", "Plays the specified AudioSource on the target GameObject(s). Playing the AudioSource will immediately replace any existing sound playing from it. If you wish to change settings on the AudioSource (such as volume or pitch) at runtime, use the reflected properties to do so.\n\nNOTE: To use this node, you must have already setup an AudioSource component on the target GameObject(s). If you want to simply play a sound with default AudioSource settings without needing to have an existing AudioSource component on the GameObject, use the Play Sound node.")]
 public class uScriptAct_PlayAudioSource : uScriptLogic
 {
    private List<AudioSource> m_AudioSources = new List<AudioSource>();
@@ -22,52 +22,38 @@ public class uScriptAct_PlayAudioSource : uScriptLogic
    public event System.EventHandler Finished;
 
    // Parameter Attributes are applied below in Stop()
-   public void Play(GameObject[] target, AudioClip audioClip, float volume)
+   public void Play(GameObject[] target, AudioClip audioClip)
    {
       if (target.Length > 0)
       {
 
          for (int i = 0; i < target.Length; i++)
          {
-            AudioSource source;
-            if (null != target[i].GetComponent<AudioSource>())
+            if (null != target[i])
             {
-               source = target[i].GetComponent<AudioSource>();
 
-               // Override the AudioSources AudioClip if one is specified.
-               if (null != audioClip)
+               AudioSource source;
+               if (null != target[i].GetComponent<AudioSource>())
                {
-                  source.clip = audioClip;
-               }
+                  source = target[i].GetComponent<AudioSource>();
 
-               // Use the volume specified if not the default -1
-               if (volume >= 0f)
+                  // Override the AudioSources AudioClip if one is specified.
+                  if (null != audioClip)
+                  {
+                     source.clip = audioClip;
+                  }
+
+                  source.Play();
+                  m_AudioSources.Add(source);
+
+               }
+               else
                {
-                  source.volume = volume;
+                  uScriptDebug.Log("[Play AudioSource] The target GameObject (" + target[i].name + ") does not have an existing AudioSource. Please add an AudioSource component to the target or try using the Play Sound node instead.", uScriptDebug.Type.Warning);
                }
-
-               source.Play();
-               m_AudioSources.Add(source);
-
             }
-            else
-            {
-               uScriptDebug.Log("[Play AudioSource] The target GameObject (" + target[i].name + ") does not have an existing AudioSource. Please add an AudioSource component to the target or try using the Play Sound node instead.", uScriptDebug.Type.Warning);
-            }
-
-
 
          }
-      }
-   }
-
-   // Parameter Attributes are applied below in Stop()
-   [FriendlyName("Update Volume")]
-   public void UpdateVolume(GameObject[] target, AudioClip audioClip, float volume)
-   {
-      foreach (AudioSource a in m_AudioSources)
-      {
-         a.volume = volume;
       }
    }
 
@@ -77,11 +63,7 @@ public class uScriptAct_PlayAudioSource : uScriptLogic
 
       [FriendlyName("Audio Clip", "(optional) You can specify an AudioClip to play instead of the deault AudioClip assigned to the target's AudioSource component.")]
       [RequiresLink]
-      AudioClip audioClip,
-
-      [FriendlyName("Volume", "(optional) The volume level to play the audio clip at (0.0 - 1.0). Set to -1 to use the assigned volume value set on the AudioSource component.")]
-      [DefaultValue(-1f), SocketState(false, false)]
-      float volume
+      AudioClip audioClip
       )
    {
       if (null != m_AudioSources)
--- uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Audio/uScriptAct_PlayAudioSourceOneShot.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Audio/uScriptAct_PlayAudioSourceOneShot.cs
@@ -12,7 +12,7 @@ using System.Collections.Generic;
 [NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
 [NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
 
-[FriendlyName("Play AudioSource (One Shot)", "Plays the specified AudioSource on the target GameObject(s) using Unity's PlayOneShot option (fire and forget).\n\nNOTE: To use this node, you must have already setup an AudioSource component on the target GameObject(s). If you want to simply play a sound with default AudioSource settings without needing to have an existing AudioSource component on the GameObject, use the Play Sound node.")]
+[FriendlyName("Play AudioSource (One Shot)", "Plays the specified AudioSource on the target GameObject(s) using Unity's PlayOneShot option (fire and forget). If you wish to change settings on the AudioSource (such as volume or pitch) at runtime, use the reflected properties to do so.\n\nNOTE: To use this node, you must have already setup an AudioSource component on the target GameObject(s). If you want to simply play a sound with default AudioSource settings without needing to have an existing AudioSource component on the GameObject, use the Play Sound node.")]
 public class uScriptAct_PlayAudioSourceOneShot : uScriptLogic
 {
    public bool Out { get { return true; } }
@@ -24,51 +24,39 @@ public class uScriptAct_PlayAudioSourceOneShot : uScriptLogic
 
       [FriendlyName("Audio Clip", "(optional) You can specify an AudioClip to play instead of the deault AudioClip assigned to the target's component.")]
       [RequiresLink]
-      AudioClip audioClip,
-
-      [FriendlyName("Volume", "(optional) The volume level to play the audio clip at (0.0 - 1.0). Set to -1 to use the assigned volume value set on the AudioSource component.")]
-      [DefaultValue(-1f), SocketState(false, false)]
-      float volume
+      AudioClip audioClip
       )
    {
       if (target.Length > 0)
       {
          AudioClip[] localClip = new AudioClip[target.Length];
-         float[] localVolume = new float[target.Length];
 
          for (int i = 0; i < target.Length; i++)
          {
-            AudioSource source;
-            if (null != target[i].GetComponent<AudioSource>())
+            if (null != target[i])
             {
-               source = target[i].GetComponent<AudioSource>();
-
-               // Override the AudioSources AudioClip if one is specified.
-               if (null != audioClip)
+               AudioSource source;
+               if (null != target[i].GetComponent<AudioSource>())
                {
-                  localClip[i] = audioClip;
+                  source = target[i].GetComponent<AudioSource>();
+
+                  // Override the AudioSources AudioClip if one is specified.
+                  if (null != audioClip)
+                  {
+                     localClip[i] = audioClip;
+                  }
+                  else
+                  {
+                     localClip[i] = source.clip;
+                  }
+
+                  source.PlayOneShot(localClip[i], source.volume);
+
                }
                else
                {
-                  localClip[i] = source.clip;
+                  uScriptDebug.Log("[Play AudioSource (One Shot)] The target GameObject (" + target[i].name + ") does not have an existing AudioSource. Please add an AudioSource component to the target or try using the Play Sound node instead.", uScriptDebug.Type.Warning);
                }
-
-               // Use the volume specified if not the default -1
-               if (volume >= 0f)
-               {
-                  localVolume[i] = volume;
-               }
-               else
-               {
-                  localVolume[i] = source.volume;
-               }
-
-               source.PlayOneShot(localClip[i], localVolume[i]);
-
-            }
-            else
-            {
-               uScriptDebug.Log("[Play AudioSource (One Shot)] The target GameObject (" + target[i].name + ") does not have an existing AudioSource. Please add an AudioSource component to the target or try using the Play Sound node instead.", uScriptDebug.Type.Warning);
             }
 
          }
