690e084319e8b4820380d2470032974e12269702
Scott Blinn <scott@detoxstudios.com>
2012-06-10 01:36:43 +0000

    Added blank/template uScriptUserTypes script file to the project.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1894 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/uScriptUserTypes.cs
@@ -0,0 +1,7 @@
+// This file allows you to expose advanced things to uScript's Reflection() node section of the Toolbox panel.
+
+public class uScriptUserTypes 
+{
+   public string Types = "UnityEngine.GUIText";
+}
+
