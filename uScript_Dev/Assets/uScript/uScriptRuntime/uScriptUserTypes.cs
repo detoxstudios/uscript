fe27d9977bfb60d5d116662f510574c35506a97d
Scott Blinn <scott@detoxstudios.com>
2012-06-10 16:38:07 +0000

    Removed the uScriptUserTypes.cs file to make way for new system.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1898 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptRuntime/uScriptUserTypes.cs
+++
@@ -1,7 +0,0 @@
-// This file allows you to expose advanced things to uScript's Reflection() node section of the Toolbox panel.
-
-public class uScriptUserTypes 
-{
-   public string Types = "UnityEngine.GUIText";
-}
-
