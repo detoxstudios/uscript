dd57b02989b0427b1290c48f08f1931ae74922d6
Scott Blinn <scott@detoxstudios.com>
2012-06-19 02:37:37 +0000

    A couple of nodes in mid-development
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1913 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Debug/uScriptCon_IsEmpty.cs
@@ -0,0 +1,59 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[NodePath("Conditions/Comparison")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Checks to see if the target variable is empty/null.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
+
+[FriendlyName("Is Empty", "Checks to see if the target variable is empty/null. If more than one variable is hooked up to the Target socket, it will only return Not Empty if all the provided targets are not empty.")]
+public class uScriptCon_IsEmpty : uScriptLogic
+{
+   private bool m_Empty = false;
+   private bool m_NotEmpty = false;
+
+   [FriendlyName("Empty")]
+   public bool Empty { get { return m_Empty; } }
+   [FriendlyName("Not Empty")]
+   public bool NotEmpty { get { return m_NotEmpty; } }
+    
+   public void In(
+      [FriendlyName("Target", "The variable(s) you wish to check.")]
+      object[] Targets
+      )
+   {
+		m_NotEmpty = false;
+		m_Empty = false;
+		
+		bool nullFound = false;
+		
+		foreach ( object obj in Targets )
+		{
+			
+			if ( null == obj )
+			{
+				nullFound = true;
+				break;
+				//fix
+			}
+			
+		}
+		
+		// Fire out the correct output socket
+		if ( nullFound )
+		{
+			m_Empty = true;
+		}
+		else
+		{
+			m_NotEmpty = true;
+		}
+      
+   }
+}
---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Debug/uScript_VariableChangedBool.cs
@@ -0,0 +1,51 @@
+// uScript Event Node
+// (C) 2012 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodeAutoAssignMasterInstance(true)]
+
+[NodePath("Events/Variables")]
+
+[NodeCopyright("Copyright 2012 by Detox Studios LLC")]
+[NodeToolTip("Fires an event signal when the specified variable(s) changed.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
+
+[FriendlyName("Variable Changed (Bool)", "Fires an event signal when the specified variable(s) changed.")]
+public class uScript_VariableChangedBool : uScriptEvent
+{
+#pragma warning disable 67
+#pragma warning disable 414
+
+
+   private bool m_VariableChanged = false;
+
+   
+   public delegate void uScriptEventHandler(object sender, System.EventArgs args);
+
+
+   [FriendlyName("On Changed")]
+   public event uScriptEventHandler OnChanged;
+   
+#pragma warning restore 414
+#pragma warning restore 67
+
+   void Update()
+   {
+
+
+      if (!m_VariableChanged)
+      {
+
+            if ( null != OnChanged ) OnChanged( this, new System.EventArgs() );     
+
+      }
+      
+      //m_LastKeyboardOut = iPhoneKeyboard.visible;
+   
+
+
+   }
+}
