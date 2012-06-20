fef6ea38714f3ff5873c87c8284c883eb976e8f1
Scott Blinn <scott@detoxstudios.com>
2012-06-20 05:57:55 +0000

    Updated the String Contains compare node to take string lists.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1919 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Conditions/Comparison/uScriptCon_StringContains.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Conditions/Comparison/uScriptCon_StringContains.cs
@@ -11,7 +11,7 @@ using System.Collections;
 [NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
 [NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Get_Game_Time")]
 
-[FriendlyName("String Contains", "Determines if the target string contains the specified text.")]
+[FriendlyName("String Contains", "Determines if the target string contains the specified text. If multiple targets and/or values are used, the True socket will fire if ANY one of the targets contain ANY one of the values.")]
 public class uScriptCon_StringContains : uScriptLogic
 {
    private bool m_ContainsValue = false;
@@ -24,12 +24,25 @@ public class uScriptCon_StringContains : uScriptLogic
 
    public void In(
       [FriendlyName("Target", "The target string you wish to check.")]
-      string Target,
+      string[] Target,
       
       [FriendlyName("Value", "The text you want to search for in the Target string.")]
-      string Value
+      string[] Value
       )
    {
-		m_ContainsValue = Target.Contains(Value);
+		bool matchFound = false;
+		
+		foreach (string tmpTarget in Target)
+		{
+			foreach (string tmpValue in Value)
+			{
+				if (tmpTarget.Contains(tmpValue))
+				{
+					matchFound = true;
+				}
+			}
+		}
+		
+		m_ContainsValue = matchFound;
    }
 }
