9e20490e407d53b97222f2dda72eebfcac92561b
Anthony Silva <anthony@detoxstudios.com>
2012-07-16 01:52:44 +0000

    New node for checking to see if a Vector2 is contained within a Rect.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1929 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Conditions/Comparison/uScriptCon_PointInRect.cs
@@ -0,0 +1,28 @@
+// uScript Action Node
+// (C) 2010 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Conditions/Comparison")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Checks to see if the Target Vector2 is within a Rect.")]
+[NodeAuthor("Detox Studios LLC. Original node by SvdV on the uScript Community Forum", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
+
+[FriendlyName("Point In Rect", "Checks to see if the Target Vector2 is within a Rect.")]
+public class uScriptCon_PointInRect : uScriptLogic
+{
+   private bool m_InRect = false;
+
+   public bool True { get { return m_InRect; } }
+   public bool False { get { return !m_InRect; } }
+
+   public void In(
+      [FriendlyName("Target", "The target Vector2 variable to compare against the rect.")] Vector2 Target,
+      [FriendlyName("Test", "The Rect to test Target against.")] Rect Test)
+   {
+      m_InRect = Test.Contains( Target );
+   }
+}
