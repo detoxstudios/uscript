7450c532f864ac8fa71ca6fee1947fda49db887f
Scott Blinn <scott@detoxstudios.com>
2012-08-05 14:13:03 +0000

    Took a pass at fixing the string contains node. Awaitng Trapper's review.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1934 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Conditions/Comparison/uScriptCon_StringContains.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Conditions/Comparison/uScriptCon_StringContains.cs
@@ -9,9 +9,9 @@ using System.Collections;
 [NodeCopyright("Copyright 2011 by Detox Studios LLC")]
 [NodeToolTip("Determines if the target string contains the specified text.")]
 [NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
-[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Get_Game_Time")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
 
-[FriendlyName("String Contains", "Determines if the target string contains the specified text. If multiple targets and/or values are used, the True socket will fire if ANY one of the targets contain ANY one of the values.")]
+[FriendlyName("String Contains", "Determines if the target string contains the specified text. If multiple targets and/or values are used, the True socket will fire if ANY one of the targets contain ANY one of the values. Please use the String List input socket when using Stirng List variables.")]
 public class uScriptCon_StringContains : uScriptLogic
 {
    private bool m_ContainsValue = false;
@@ -26,15 +26,23 @@ public class uScriptCon_StringContains : uScriptLogic
       [FriendlyName("Target", "The target string you wish to check.")]
       string[] Target,
       
-      [FriendlyName("Value", "The text you want to search for in the Target string.")]
-      string[] Value
+      [FriendlyName("String", "The string variable (text) you want to search for in the Target string.")]
+      string Value,
+		
+	  [FriendlyName("String List", "A string list variable you want to search for in the Target string.")]
+      string[] Values
       )
    {
 		bool matchFound = false;
 		
 		foreach (string tmpTarget in Target)
 		{
-			foreach (string tmpValue in Value)
+			if (tmpTarget == Value)
+			{
+				matchFound = true;
+			}
+			
+			foreach (string tmpValue in Values)
 			{
 				if (tmpTarget.Contains(tmpValue))
 				{
