2982e95cd5960fb5765a6c39fe5651c4ae90594d
Scott Blinn <scott@detoxstudios.com>
2012-08-07 00:33:32 +0000

    Reverted the node to NOT use String List
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1936 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Conditions/Comparison/uScriptCon_StringContains.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Conditions/Comparison/uScriptCon_StringContains.cs
@@ -11,7 +11,7 @@ using System.Collections;
 [NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
 [NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
 
-[FriendlyName("String Contains", "Determines if the target string contains the specified text. If multiple targets and/or values are used, the True socket will fire if ANY one of the targets contain ANY one of the values. Please use the String List input socket when using Stirng List variables.")]
+[FriendlyName("String Contains", "Determines if the target string contains the specified text.")]
 public class uScriptCon_StringContains : uScriptLogic
 {
    private bool m_ContainsValue = false;
@@ -24,33 +24,12 @@ public class uScriptCon_StringContains : uScriptLogic
 
    public void In(
       [FriendlyName("Target", "The target string you wish to check.")]
-      string[] Target,
+      string Target,
       
-      [FriendlyName("String", "The string variable (text) you want to search for in the Target string.")]
-      string Value,
-		
-	  [FriendlyName("String List", "A string list variable you want to search for in the Target string.")]
-      string[] Values
+      [FriendlyName("Value", "The text you want to search for in the Target string.")]
+      string Value
       )
    {
-		bool matchFound = false;
-		
-		foreach (string tmpTarget in Target)
-		{
-			if (tmpTarget == Value)
-			{
-				matchFound = true;
-			}
-			
-			foreach (string tmpValue in Values)
-			{
-				if (tmpTarget.Contains(tmpValue))
-				{
-					matchFound = true;
-				}
-			}
-		}
-		
-		m_ContainsValue = matchFound;
+		m_ContainsValue = Target.Contains(Value);
    }
 }
