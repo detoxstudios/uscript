8eaf77166cbe2cd2df7defe28760283ccfabbd73
Scott Blinn <scott@detoxstudios.com>
2012-09-29 18:57:25 +0000

    Added some scenes (under ProfileSetup directory) and a uScript graph for profile testing.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@1992 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScriptProjectFiles/uScripts/_GeneratedCode/Profile_SimpleGraph.cs
@@ -0,0 +1,205 @@
+//uScript Generated Code - Build 0.9.1986
+//Generated with Debug Info
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[NodePath("Graphs")]
+[System.Serializable]
+[FriendlyName("Untitled", "")]
+public class Profile_SimpleGraph : uScriptLogic
+{
+
+   #pragma warning disable 414
+   GameObject parentGameObject = null;
+   uScript_GUI thisScriptsOnGuiListener = null; 
+   delegate void ContinueExecution();
+   ContinueExecution m_ContinueExecution;
+   bool m_Breakpoint = false;
+   const int MaxRelayCallCount = 1000;
+   int relayCallCount = 0;
+   
+   //externally exposed events
+   
+   //external parameters
+   
+   //local nodes
+   System.String local_1_System_String = "Profile_SimpleGraph";
+   
+   //owner nodes
+   
+   //logic nodes
+   //pointer to script instanced logic node
+   uScriptAct_Log logic_uScriptAct_Log_uScriptAct_Log_0 = new uScriptAct_Log( );
+   System.Object logic_uScriptAct_Log_Prefix_0 = "";
+   System.Object[] logic_uScriptAct_Log_Target_0 = new System.Object[] {};
+   System.Object logic_uScriptAct_Log_Postfix_0 = "";
+   bool logic_uScriptAct_Log_Out_0 = true;
+   
+   //event nodes
+   UnityEngine.GameObject event_UnityEngine_GameObject_Instance_4 = null;
+   
+   //property nodes
+   
+   //method nodes
+   #pragma warning restore 414
+   
+   //functions to refresh properties from entities
+   
+   void SyncUnityHooks( )
+   {
+      SyncEventListeners( );
+   }
+   
+   void SyncEventListeners( )
+   {
+      if ( null == event_UnityEngine_GameObject_Instance_4 )
+      {
+         event_UnityEngine_GameObject_Instance_4 = uScript_MasterComponent.LatestMaster;
+         if ( null != event_UnityEngine_GameObject_Instance_4 )
+         {
+            {
+               uScript_Global component = event_UnityEngine_GameObject_Instance_4.GetComponent<uScript_Global>();
+               if ( null == component )
+               {
+                  component = event_UnityEngine_GameObject_Instance_4.AddComponent<uScript_Global>();
+               }
+               if ( null != component )
+               {
+                  component.uScriptStart += Instance_uScriptStart_4;
+               }
+            }
+         }
+      }
+   }
+   
+   void UnregisterEventListeners( )
+   {
+      if ( null != event_UnityEngine_GameObject_Instance_4 )
+      {
+         {
+            uScript_Global component = event_UnityEngine_GameObject_Instance_4.GetComponent<uScript_Global>();
+            if ( null != component )
+            {
+               component.uScriptStart -= Instance_uScriptStart_4;
+            }
+         }
+      }
+   }
+   
+   public override void SetParent(GameObject g)
+   {
+      parentGameObject = g;
+      
+      logic_uScriptAct_Log_uScriptAct_Log_0.SetParent(g);
+   }
+   public void Awake()
+   {
+      
+   }
+   
+   public void Start()
+   {
+      SyncUnityHooks( );
+      
+   }
+   
+   public void Update()
+   {
+      //reset each Update, and increments each method call
+      //if it ever goes above MaxRelayCallCount before being reset
+      //then we assume it is stuck in an infinite loop
+      if ( relayCallCount < MaxRelayCallCount ) relayCallCount = 0;
+      if ( null != m_ContinueExecution )
+      {
+         ContinueExecution continueEx = m_ContinueExecution;
+         m_ContinueExecution = null;
+         m_Breakpoint = false;
+         continueEx( );
+         return;
+      }
+      UpdateEditorValues( );
+      //other scripts might have added GameObjects with event scripts
+      //so we need to verify all our event listeners are registered
+      SyncEventListeners( );
+      
+   }
+   
+   public void OnDestroy()
+   {
+      UnregisterEventListeners( );
+   }
+   
+   void Instance_uScriptStart_4(object o, System.EventArgs e)
+   {
+      //reset event call
+      //if it ever goes above MaxRelayCallCount before being reset
+      //then we assume it is stuck in an infinite loop
+      if ( relayCallCount < MaxRelayCallCount ) relayCallCount = 0;
+      
+      //fill globals
+      //relay event to nodes
+      Relay_uScriptStart_4( );
+   }
+   
+   void Relay_In_0()
+   {
+      if ( relayCallCount++ < MaxRelayCallCount )
+      {
+         if (true == CheckDebugBreak("f9a81570-a110-4a3e-b2df-447f6dc45ed3", "Log", Relay_In_0)) return; 
+         {
+            int index;
+            index = 0;
+            if ( logic_uScriptAct_Log_Target_0.Length <= index)
+            {
+               System.Array.Resize(ref logic_uScriptAct_Log_Target_0, index + 1);
+            }
+            logic_uScriptAct_Log_Target_0[ index++ ] = local_1_System_String;
+            
+         }
+         logic_uScriptAct_Log_uScriptAct_Log_0.In(logic_uScriptAct_Log_Prefix_0, logic_uScriptAct_Log_Target_0, logic_uScriptAct_Log_Postfix_0);
+         
+         //save off values because, if there are multiple, our relay logic could cause them to change before the next value is tested
+         
+      }
+      else
+      {
+         uScriptDebug.Log( "Possible infinite loop detected in uScript Profile_SimpleGraph.uscript at Log.  If this is in error you can change the Maximum Node Recursion in the Preferences Panel and regenerate the script.", uScriptDebug.Type.Error);
+      }
+   }
+   
+   void Relay_uScriptStart_4()
+   {
+      if (true == CheckDebugBreak("86842b78-2a18-4f02-b7e5-57ef714fdc4a", "uScript Events", Relay_uScriptStart_4)) return; 
+      Relay_In_0();
+   }
+   
+   private void UpdateEditorValues( )
+   {
+      uScript_MasterComponent.LatestMasterComponent.UpdateNodeValue( "Profile_SimpleGraph.uscript:1", local_1_System_String);
+      uScript_MasterComponent.LatestMasterComponent.UpdateNodeValue( "6d9cd4ac-373c-43f1-83b8-433bae879ea4", local_1_System_String);
+   }
+   bool CheckDebugBreak(string guid, string name, ContinueExecution method)
+   {
+      if (true == m_Breakpoint) return true;
+      
+      if (true == uScript_MasterComponent.LatestMasterComponent.HasBreakpoint(guid))
+      {
+         if (uScript_MasterComponent.LatestMasterComponent.CurrentBreakpoint == guid)
+         {
+            uScript_MasterComponent.LatestMasterComponent.CurrentBreakpoint = "";
+         }
+         else
+         {
+            uScript_MasterComponent.LatestMasterComponent.CurrentBreakpoint = guid;
+            UpdateEditorValues( );
+            UnityEngine.Debug.Log("uScript BREAK Node:" + name + " ((Time: " + Time.time + "");
+            UnityEngine.Debug.Break();
+            m_ContinueExecution = new ContinueExecution(method);
+            m_Breakpoint = true;
+            return true;
+         }
+      }
+      return false;
+   }
+}
