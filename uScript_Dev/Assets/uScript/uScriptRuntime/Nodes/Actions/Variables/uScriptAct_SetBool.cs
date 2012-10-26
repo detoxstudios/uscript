cdbb1c7940baa32da1c76cdd01aecd7a43108682
Scott Blinn <scott@detoxstudios.com>
2012-10-26 10:35:21 +0000

    Updated the Set Bool node to have more output sockets (Set True and Set False), removing the need to have a compare bool right after (which is a common case).
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@2030 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Variables/uScriptAct_SetBool.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Variables/uScriptAct_SetBool.cs
@@ -1,6 +1,5 @@
 // uScript Action Node
 // (C) 2010 Detox Studios LLC
-
 using UnityEngine;
 using System.Collections;
 
@@ -14,34 +13,53 @@ using System.Collections;
 [FriendlyName("Set Bool", "Sets a boolean to the defined value.")]
 public class uScriptAct_SetBool : uScriptLogic
 {
-   // ================================================================================
-   //    Output Sockets
-   // ================================================================================
-   //
-   public bool Out { get { return true; } }
+	// ================================================================================
+	//    Output Sockets
+	// ================================================================================
+	//
+	public bool Out { get { return true; } }
+	
+	private bool _setTrue = false;
+
+	[FriendlyName("Set True", "Fired only if the variable was set to true.")]
+	public bool SetTrue { get { return _setTrue; } }
+	
+	private bool _setFalse = false;
+
+	[FriendlyName("Set False", "Fired only if the variable was set to false.")]
+	public bool SetFalse { get { return _setFalse; } }
 
 
-   // ================================================================================
-   //    Input Sockets and Node Parameters
-   // ================================================================================
-   //
-   // Parameter Attributes are applied below in False()
-   public void True(out bool Target)
-   {
-      Target = true;
-   }
+	// ================================================================================
+	//    Input Sockets and Node Parameters
+	// ================================================================================
+	//
+	// Parameter Attributes are applied below in False()
+	public void True (out bool Target)
+	{
+		_setFalse = false;
+		_setTrue = false;
+		
+		Target = true;
+		_setTrue = true;
+		
+	}
 
-   public void False(
+	public void False (
       [FriendlyName("Target", "The value that has been set for this variable.")]
       out bool Target
       )
-   {
-      Target = false;
-   }
+	{
+		_setFalse = false;
+		_setTrue = false;
+		
+		Target = false;
+		_setFalse = true;
+	}
 
 
-   // ================================================================================
-   //    Miscellaneous Node Functionality
-   // ================================================================================
-   //
+	// ================================================================================
+	//    Miscellaneous Node Functionality
+	// ================================================================================
+	//
 }
