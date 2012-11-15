fb3341076edcdada0a63284c85a401a6324a189e
Michael Ryan <michael@detoxstudios.com>
2012-11-15 22:29:35 +0000

    + Updated the test editor so that it highlights another issue as well.  With a dynamic GUI, GUI.GetNameOfFocusedControl() can return the name of a control that doesn't even exist any longer.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@2077 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/DetoxDevTools/Editor/ControlNameTestEditor.cs
+++ uScript_Dev/Assets/DetoxDevTools/Editor/ControlNameTestEditor.cs
@@ -17,22 +17,25 @@ public class ControlNameTestEditor : EditorWindow
 		}
 	}
 	
-	[MenuItem ("Detox Tools/Internal/ControlName Test Editor &%c")]
+	[MenuItem ("Detox Tools/Internal/Dynamic GUI Test Editor &%g")]
 	private static void Init()
 	{
-		_instance = (ControlNameTestEditor)EditorWindow.GetWindow(typeof(ControlNameTestEditor), false, "ControlName");
+		_instance = (ControlNameTestEditor)EditorWindow.GetWindow(typeof(ControlNameTestEditor), true, "Dynamic GUI Test Editor");
+      Vector2 panelSize = new Vector2(256, 224);
+      _instance.minSize = panelSize;
+      _instance.maxSize = panelSize;
 	}
 
 	public int _gridIndex = 0;
 	public string[] _gridOptions = new string[] {"A", "B", "C", "D"};
-	private int _intField;
-	private float _floatField;
-	private string _textField;
-	private Color _colorField;
+	private int _intField = 42;
+	private float _floatField = Mathf.PI;
+	private string _textField = "TEXT";
+	private Color _colorField = Color.blue;
 	
 	void OnGUI()
 	{
-		EditorGUILayout.BeginVertical(GUILayout.Width(256));
+		EditorGUILayout.BeginVertical();
 		{
 			_gridIndex = GUILayout.SelectionGrid(_gridIndex, _gridOptions, 4);
 		
@@ -70,7 +73,7 @@ public class ControlNameTestEditor : EditorWindow
 						_textField = EditorGUILayout.TextField(ControlName("TextField_09"), _textField);
 						_textField = EditorGUILayout.TextField(ControlName("TextField_0A"), _textField);
 						_textField = EditorGUILayout.TextField(ControlName("TextField_0B"), _textField);
-						_textField = EditorGUILayout.TextField(ControlName("TextField_0C"), _textField);
+                  GUILayout.Space(19);
 						break;
 				}
 			}
