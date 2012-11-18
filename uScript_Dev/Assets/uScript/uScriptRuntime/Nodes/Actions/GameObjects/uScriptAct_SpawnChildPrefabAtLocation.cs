c828372e778a046cad0d795f059df0e944f2269b
Scott Blinn <scott@detoxstudios.com>
2012-11-18 20:23:24 +0000

    Added a couple of nodes I had done so they will be in the next official build.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@2082 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/GameObjects/uScriptAct_SpawnChildPrefabAtLocation.cs
@@ -0,0 +1,176 @@
+// uScript Action Node
+// (C) 2010 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/GameObjects")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Create an instance of a Prefab as a child of the parent GameObject at the specified spawn point.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Spawn_Prefab_At_Location")]
+
+[FriendlyName("Spawn Child Prefab At Location", "Create (instantiate) an instance of a Prefab as a child of the parent GameObject at the specified Vector3 location at runtime (must be in the Resources folder structure).")]
+public class uScriptAct_SpawnChildPrefabAtLocation : uScriptLogic
+{
+   // @TODO: Needed functionality includes:
+   // 1. Ability to spawn multiple Prefabs at once with an optional delay between spawns.
+   // 2. Ability to put spawned object into a GameObject variable or list.
+   // 3. Ability to set multiple spawn point locations.
+   // 4. Ability to set a Vector3 location in option of GameObject location.
+   // 5. Multiple inputs (Spawn Prefab, Enable, Disable, and Toggle).
+   // 6. Logic to check for a legal spawn collision check before doing a spwan Prefab (would have Aborted out).
+   // 7. Would it be better to have another master uScript script to hold an array of prefabs to spawn (instead of Resources folder)? Both?
+
+   public delegate void uScriptEventHandler(object sender, System.EventArgs args);
+   private bool m_FinishedSpawning = false;
+
+   public bool Immediate { get { return true; } }
+
+   [FriendlyName("Finished Spawning")]
+   public event uScriptEventHandler FinishedSpawning;
+
+   public void In(
+      [FriendlyName("Prefab Name", "The name of the prefab to spawn.")]
+      string PrefabName,
+
+      [FriendlyName("Resource Path", "The resource path to look in for the prefab.")]
+      [SocketState(false, false)]
+      string ResourcePath,
+
+      [FriendlyName("Spawn Position", "The position to spawn prefab(s) from.")]
+      Vector3 SpawnPosition,
+
+      [FriendlyName("Spawn Rotation", "The rotation to spawn prefab(s) from.")]
+      [SocketState(false, false)]
+      Quaternion SpawnRotation,
+
+      [FriendlyName("Spawned Name", "The name given to newly spawned prefab(s).")]
+      [SocketState(false, false)]
+      string SpawnedName,
+		
+		[FriendlyName("Parent", "The parent GameObject.")]
+      GameObject Parent,
+
+// The offset from the Spawn Point to spawn prefab(s) from.
+
+      [FriendlyName("Spawned GameObject", "The GameObject that gets spawned.")]
+      out GameObject SpawnedGameObject,
+
+      [FriendlyName("Spawned InstancedID", "The instance ID of the spawned GameObject.")]
+      [SocketState(false, false)]
+      out int SpawnedInstancedID
+      )
+   {
+      //Get Spawn point location and rotation
+
+      Vector3 spawnPointPosition = SpawnPosition;
+      Quaternion spawnPointRotation = SpawnRotation;
+
+      // Build final ResourcePath string
+      if (!string.IsNullOrEmpty(ResourcePath))
+      {
+         // Make sure all the slashes are correct
+         if (ResourcePath.Contains("\\"))
+         {
+            ResourcePath = ResourcePath.Replace("\\", "/");
+         }
+         
+         // Prune any begining or ending slashes
+         if (ResourcePath.StartsWith("/") || ResourcePath.StartsWith(@"\"))
+         {
+            ResourcePath = ResourcePath.Remove(0, 1);
+         }
+         if (ResourcePath.EndsWith("/") || ResourcePath.EndsWith(@"\"))
+         {
+            int stringLength = ResourcePath.Length - 1;
+            ResourcePath = ResourcePath.Remove(stringLength, 1);
+         }
+         
+         //prune Assets text if user added it
+         if (ResourcePath.StartsWith("Assets") || ResourcePath.StartsWith("assets"))
+         {
+            ResourcePath = ResourcePath.Remove(0, "Assets".Length);
+         }
+
+         //prune Resources text if user added it
+         if (ResourcePath.StartsWith("Resources") || ResourcePath.StartsWith("resources"))
+         {
+            ResourcePath = ResourcePath.Remove(0, "Resources".Length);
+         }
+      }
+
+      // Build final PrefabName string
+      if (!string.IsNullOrEmpty(PrefabName))
+      {
+         // Make sure all the slashes are correct
+         if (PrefabName.Contains("\\"))
+         {
+            PrefabName = PrefabName.Replace("\\", "/");
+         }
+
+         // Prune any begining or ending slashes
+         if (PrefabName.StartsWith("/") || PrefabName.StartsWith(@"\"))
+         {
+            PrefabName = PrefabName.Remove(0, 1);
+         }
+         if (PrefabName.EndsWith("/") || PrefabName.EndsWith(@"\"))
+         {
+            int stringLength = PrefabName.Length - 1;
+            PrefabName = PrefabName.Remove(stringLength, 1);
+         }
+      }
+
+      // Build final fullPrefabPath
+      string fullPrefabPath = "";
+      if (!string.IsNullOrEmpty(ResourcePath))
+      {
+         fullPrefabPath = ResourcePath + "/" + PrefabName;
+      }
+      else
+      {
+         // Must be in the root of Resources
+         fullPrefabPath = PrefabName;
+      }
+      
+      // Spawn the Prefab
+      try
+      {
+         GameObject spawnedPrefab = ScriptableObject.Instantiate(Resources.Load(fullPrefabPath), spawnPointPosition, spawnPointRotation) as GameObject;
+			
+			if (null != Parent)
+			{
+				spawnedPrefab.transform.parent = Parent.transform;
+			}
+			
+
+         if (!string.IsNullOrEmpty(SpawnedName))
+         {
+            spawnedPrefab.name = SpawnedName;
+         }
+
+         SpawnedGameObject = spawnedPrefab;
+         SpawnedInstancedID = spawnedPrefab.GetInstanceID();
+
+         m_FinishedSpawning = true;
+      }
+      catch (System.Exception e)
+      {
+         uScriptDebug.Log(e.ToString(), uScriptDebug.Type.Error);
+         SpawnedGameObject = null;
+         SpawnedInstancedID = 0;
+      }
+
+   }
+
+   public void Update()
+   {
+      if (m_FinishedSpawning)
+      {
+         m_FinishedSpawning = false;
+         if ( FinishedSpawning != null ) FinishedSpawning(this, new System.EventArgs());
+      }
+   }
+
+}
---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Variables/Lists/uScriptAct_ModifyListByIndexGameObject.cs
@@ -0,0 +1,102 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[NodePath("Actions/Variables/Lists/GameObject")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Modify a list by specifying a specific slot number (index) in the list.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
+
+[FriendlyName("Modify List By Index (GameObject)", "Modify a list by specifying a specific slot number (index) in the list.")]
+public class uScriptAct_ModifyListByIndexGameObject : uScriptLogic
+{
+	// ================================================================================
+	//    Output Sockets
+	// ================================================================================
+	//
+	public bool Out { get { return true; } }
+    
+
+	// ================================================================================
+	//    Input Sockets and Node Parameters
+	// ================================================================================
+
+	[FriendlyName("Insert Into List", "Inserts an item into the list at the specified index.")]
+	public void InsertIntoList (ref GameObject[] GameObjectList, int Index, GameObject[] Target, out int ListCount)
+	{
+		List<GameObject> list = new List<GameObject> (GameObjectList);
+		
+		if (Index < 0) {
+			Index = 0;
+		}
+		
+		if (list.Count == 0) {
+			foreach (GameObject go in Target) {
+				list.Add (go);
+			}
+			
+		} else {
+			if (Index + 1 >= list.Count) {	
+				foreach (GameObject go in Target) {
+					list.Add (go);
+				}
+				
+			} else {
+			
+				foreach (GameObject go in Target) {
+					list.Insert (Index, go);
+				}
+
+			}
+		}
+		
+		GameObjectList = list.ToArray ();
+		ListCount = GameObjectList.Length;
+      
+	}	
+	
+	// Parameter Attributes are applied below in EmptyList()
+	[FriendlyName("Remove From List", "Removes an item from the list at the specified index.")]
+	public void RemoveFromList (
+	  [FriendlyName("List", "The list to modify.")]
+      ref GameObject[] GameObjectList,
+		
+		[FriendlyName("Index", "The index number where you wish to perform the modification. If the number is larger than the elements contained in the list, the action will be performed on the largest valid index number of the list. Negative values are automatically converted to 0.")]
+	  int Index,
+
+      [FriendlyName("Insert Target", "The Target variable(s) to add or remove from the list. This socket is ignored when using the Remove From List input socket.")]
+      GameObject[] Target,
+	
+      [FriendlyName("List Size", "The remaining number of items in the list after the modification has taken place.")]
+      out int ListCount
+      )
+	{
+		List<GameObject> list = new List<GameObject> (GameObjectList);
+		
+		if (Index < 0) {
+			Index = 0;
+		}
+		
+		if (list.Count > 0) {
+		
+			if (Index >= list.Count) {
+				list.RemoveAt (list.Count - 1);
+			} else {
+				list.RemoveAt (Index);
+			}
+		}
+      
+		GameObjectList = list.ToArray ();
+		ListCount = GameObjectList.Length;
+	}
+
+
+	// ================================================================================
+	//    Miscellaneous Node Functionality
+	// ================================================================================
+	//
+}
