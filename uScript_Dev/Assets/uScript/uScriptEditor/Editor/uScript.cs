a609848e8e72520862e51edafdb72cdb710bc233
Scott Blinn <scott@detoxstudios.com>
2012-11-19 00:00:52 +0000

    Updated build name and number for RB29
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@2086 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptEditor/Editor/uScript.cs
+++ uScript_Dev/Assets/uScript/uScriptEditor/Editor/uScript.cs
@@ -46,9 +46,9 @@ public class uScript : EditorWindow
    // #
 
    // Set version - format is MAJOR.MINOR.FOURDIGITSVNCOMMITNUMBER
-   static public string BuildNumber { get { return "0.9.2067"; } }
+   static public string BuildNumber { get { return "0.9.2086"; } }
 
-   static public string BuildName { get { return "Retail Beta 28"; } }
+   static public string BuildName { get { return "Retail Beta 29"; } }
    static public string BuildNamePLE { get { return "Personal Learning Edition"; } }
 
 #if FREE_PLE_BUILD
