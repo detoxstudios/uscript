1c756d0751bd58d6a519e965f7b31154203d9015
Michael Ryan <michael@detoxstudios.com>
2013-01-05 06:05:39 +0000

    + Added "Int To String" with support for all the standard numeric formatting, custom formatting and culture variants. + Updated "Float To String", adding support for all the standard numeric formatting, custom formatting and culture variants.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@2147 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Variables/uScriptAct_IntToString.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Variables/uScriptAct_IntToString.cs
@@ -3,15 +3,15 @@
 using UnityEngine;
 using System.Globalization;
 
-[NodePath("Actions/Variables/Float")]
+[NodePath("Actions/Variables/Int")]
 
 [NodeCopyright("Copyright 2013 by Detox Studios LLC")]
-[NodeToolTip("Converts a float to a string with advanced formatting options.")]
+[NodeToolTip("Converts an integer to a string with advanced formatting options.")]
 [NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
-[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Float_To_String")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Int_To_String")]
 
-[FriendlyName("Float To String", "Converts a float to a string with advanced formatting options.")]
-public class uScriptAct_FloatToString : uScriptLogic
+[FriendlyName("Int To String", "Converts an integer to a string with advanced formatting options.")]
+public class uScriptAct_IntToString : uScriptLogic
 {
    public bool Out { get { return true; } }
  
@@ -19,46 +19,48 @@ public class uScriptAct_FloatToString : uScriptLogic
    {
       General,
       Currency,
+      Decimal,
       Exponential,
       FixedPoint,
       Number,
       Percent,
-      RoundTrip
+      Hexadecimal
    }
 
    public void In(
-      [FriendlyName("Target", "The float you wish to convert to a string.")]
-      float Target,
+      [FriendlyName("Target", "The integer you wish to convert to a string.")]
+      int Target,
 
       [FriendlyName("Standard Format", "Standard numeric formatting string.\n\n"
-      + "The following results will be generated when the Target value is equal to 0.12345 and the Invariant Culture is used:\n\n"
-      + "\tGeneral:\t\t\t0.12345\n"
-      + "\tCurrency:\t\t$0.12\n"
-      + "\tExponential:\t1.234500E-001\n"
-      + "\tFixedPoint:\t\t0.12\n"
-      + "\tNumber:\t\t\t0.12\n"
-      + "\tPercent:\t\t\t12.35 %\n"
-      + "\tRoundTrip:\t\t0.12345"
+      + "The following results will be generated when the Target value is equal to 125 and the Invariant Culture is used:\n\n"
+      + "\tGeneral:\t\t\t125\n"
+      + "\tCurrency:\t\t$125.00\n"
+      + "\tDecimal:\t\t\t125\n"
+      + "\tExponential:\t1.250000E+002\n"
+      + "\tFixedPoint:\t\t125.00\n"
+      + "\tNumber:\t\t\t125.00\n"
+      + "\tPercent:\t\t\t12500.00 %\n"
+      + "\tHexidecimal:\t7D"
       )]
       [DefaultValue(FormatType.General), SocketState(false, false)]
       FormatType StandardFormat,
 
       [FriendlyName("Custom Format", "An optional custom numeric format string.  If none is specified, the chosen Standard Format will be used instead.\n\n"
-      + "The following results will be generated when the Target value is equal to 0.123 and the Invariant Culture is used:\n\n"
-      + "\t#.####:\t\t\t.123\n"
-      + "\t0.####:\t\t\t0.123\n"
-      + "\t0.0000:\t\t\t0.1230\n"
-      + "\t0000.0000:\t\t0000.1230\n"
-      + "\tC5:\t\t\t\t$0.12300"
+      + "The following results will be generated when the Target value is equal to 125 and the Invariant Culture is used:\n\n"
+      + "\t#.####:\t\t\t125\n"
+      + "\t0.####:\t\t\t125\n"
+      + "\t0.0000:\t\t\t125.0000\n"
+      + "\t0000.0000:\t\t0125.0000\n"
+      + "\tC5:\t\t\t\t$125.00000"
       )]
       [DefaultValue(""), SocketState(false, false)]
       string CustomFormat,
 
       [FriendlyName("Custom Culture", "An optional custom culture string.  If none is specified, the Invariant Culture will be used.\n\n"
-      + "The following results will be generated when the Target value is equal to 0.12345 and the custom culture is set to \"sv-SE\"."
+      + "The following results will be generated when the Target value is equal to 125 and the custom culture is set to \"sv-SE\"."
       + "  Note the use of ',' instead of '.' for the decimal separator and the currency symbol for Swedish Krona:\n\n"
-      + "\tGeneral:\t\t\t0,12345\n"
-      + "\tCurrency:\t\t0,12 kr"
+      + "\tGeneral:\t\t\t125\n"
+      + "\tCurrency:\t\t125,00 kr"
       )]
       [DefaultValue(""), SocketState(false, false)]
       string CustomCulture,
@@ -78,6 +80,10 @@ public class uScriptAct_FloatToString : uScriptLogic
                format = "C";
                break;
 
+            case FormatType.Decimal:
+               format = "D";
+               break;
+               
             case FormatType.Exponential:
                format = "E";
                break;
@@ -94,8 +100,8 @@ public class uScriptAct_FloatToString : uScriptLogic
                format = "P";
                break;
 
-            case FormatType.RoundTrip:
-               format = "R";
+            case FormatType.Hexadecimal:
+               format = "X";
                break;
 
             default:
