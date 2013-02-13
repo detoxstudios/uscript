97da256e5371a7f4ede8b3b47d52cfae39422ee0
Scott Blinn <scott@detoxstudios.com>
2013-02-13 12:05:40 +0000

    Updated build number for RB34.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@2231 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptEditor/Editor/uScript.cs
+++ uScript_Dev/Assets/uScript/uScriptEditor/Editor/uScript.cs
@@ -46,7 +46,7 @@ public class uScript : EditorWindow
    // #
 
    // Set version - format is MAJOR.MINOR.FOURDIGITSVNCOMMITNUMBER
-   static public string BuildNumber { get { return "0.9.2226"; } }
+   static public string BuildNumber { get { return "0.9.2231"; } }
 
    static public string BuildName { get { return "Professional (Retail Beta 34)"; } }
    static public string BuildNamePLE { get { return "Personal Learning Edition (Retail Beta 34)"; } }
