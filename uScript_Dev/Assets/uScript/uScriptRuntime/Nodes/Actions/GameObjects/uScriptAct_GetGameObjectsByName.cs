f94d43e9225886ea89833c4a7acb7ceeae17329a
Anthony Silva <anthony@detoxstudios.com>
2013-02-25 21:17:12 +0000

    Add node to return an array of nodes that all have the same name.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@2254 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/GameObjects/uScriptAct_GetGameObjectsByName.cs
@@ -0,0 +1,58 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[NodePath("Actions/GameObjects")]
+
+[NodeCopyright("Copyright 2013 by Detox Studios LLC")]
+[NodeToolTip("Returns the GameObjects in the scene with the specified name.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Get_GameObjects_By_Name")]
+
+[FriendlyName("Get GameObjects By Name",
+              "Returns the GameObjects in the scene with the specified name." +
+              "\n\nThe \"Found\" output socket will be triggered if a GameObject matching the name is found, otherwise the \"Not Found\" output socket will be triggered." +
+              "\n\nWARNING: For performance reasons, this should not be executed every frame. Also, if you know there will only be one result, you should use Get GameObject By Name.")]
+public class uScriptAct_GetGameObjectsByName : uScriptLogic
+{
+   private bool m_Out = false;
+   public bool Out { get { return m_Out; } }
+   private bool m_True = false;
+
+   [FriendlyName("Found")]
+   public bool GameObjectsFound { get { return m_True; } }
+
+   [FriendlyName("Not Found")]
+   public bool GameObjectsNotFound { get { return !m_True; } }
+
+   public void In(
+      [FriendlyName("Name", "The name of the GameObject(s) you are looking for.")]
+      string Name,
+
+      [FriendlyName("GameObjects", "Assigns found GameObjects to the attached variable.")]
+      out GameObject[] gameObjects
+      )
+   {
+      m_Out = false;
+
+      List<GameObject> list = new List<GameObject> ();
+      GameObject[] gos = (GameObject[])GameObject.FindObjectsOfType(typeof(GameObject));
+      // loop through all game objects looking for Name objects
+      foreach (GameObject go in gos) 
+      {
+         if (go && go.name == Name) 
+         {
+            list.Add(go);
+         }
+      }   
+      gameObjects = list.ToArray();
+
+      // Fire out the correct out socket
+      m_True = gameObjects != null && gameObjects.Length > 0;
+
+      m_Out = true;
+   }
+}
