d56bfc44f041a7e1ed6270bc9af8b9b0e0778ee2
Trapper McFerron <trapper@detoxstudios.com>
2013-03-21 17:27:00 +0000

    Added a node to find a loaded shader (requested from uScript_Indie)
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@2282 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Assets/uScriptAct_FindShader.cs
@@ -0,0 +1,34 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/Assets")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Assigns a Shader")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Assign_Shader")]
+
+[FriendlyName("Assign Shader", "Finds a shader with the given name.")]
+public class uScriptAct_FindShader : uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public void In(
+      [FriendlyName("Name", "The shader to find")]
+      string name,
+
+      [FriendlyName("Shader Asset", "The Shader of the specified name.")]
+      out Shader asset
+   )
+   {
+      asset = Shader.Find(name);
+
+      if ( null == asset )
+      {
+         uScriptDebug.Log( "Shader " + name + " couldn't be found, are you sure it is loaded?", uScriptDebug.Type.Warning );
+      }
+   }
+}
