fdc260e868ccc84d1a2ee95ea8ad4247f5eb0594
Michael Ryan <michael@detoxstudios.com>
2013-04-19 08:48:09 +0000

    Made a few fields readonly, added some null checks and/or asserts
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@2301 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptEditor/Editor/GUI/Ellipsis.cs
+++ uScript_Dev/Assets/uScript/uScriptEditor/Editor/GUI/Ellipsis.cs
@@ -1,3 +1,13 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="Detox Studios, LLC" file="Ellipsis.cs">
+//   Copyright 2010-2013 Detox Studios, LLC. All rights reserved.
+// </copyright>
+// <summary>
+//   Defines the Ellipsis type.
+// </summary>
+// 
+// --------------------------------------------------------------------------------------------------------------------
+
 /*
  * This script is based on the "Auto Ellipsis" project by Thomas Polaert (2009-06-20).
  *    http://www.codeproject.com/Articles/37503/Auto-Ellipsis
@@ -11,16 +21,26 @@
  *    The FormatEllipsis enum was moved to the Ellipsis class and renamed to Format.
  */
 
-using System;
-using System.IO;
-using System.Text.RegularExpressions;
-
-using UnityEngine;
-
 namespace Detox.Editor.GUI
 {
+   using System;
+   using System.IO;
+   using System.Text.RegularExpressions;
+
+   using UnityEngine;
+
+   using Debug = System.Diagnostics.Debug;
+
    public class Ellipsis
    {
+      /// <summary>
+      /// String used as a place holder for trimmed text.
+      /// </summary>
+      public static readonly string EllipsisChars = "...";
+
+      private static readonly Regex PrevWord = new Regex(@"\W*\w*$");
+      private static readonly Regex NextWord = new Regex(@"\w*\W*");
+
       /// <summary>
       /// Specifies ellipsis format and alignment.
       /// </summary>
@@ -31,22 +51,27 @@ namespace Detox.Editor.GUI
          /// Text is not modified.
          /// </summary>
          None = 0,
+
          /// <summary>
          /// Text is trimmed at the end of the string. An ellipsis (...) is drawn in place of remaining text.
          /// </summary>
          End = 1,
+
          /// <summary>
-         /// Text is trimmed at the begining of the string. An ellipsis (...) is drawn in place of remaining text.
+         /// Text is trimmed at the beginning of the string. An ellipsis (...) is drawn in place of remaining text.
          /// </summary>
          Start = 2,
+
          /// <summary>
          /// Text is trimmed in the middle of the string. An ellipsis (...) is drawn in place of remaining text.
          /// </summary>
          Middle = 3,
+
          /// <summary>
          /// Preserve as much as possible of the drive and filename information. Must be combined with alignment information.
          /// </summary>
          Path = 4,
+
          /// <summary>
          /// Text is trimmed at a word boundary. Must be combined with alignment information.
          /// </summary>
@@ -54,19 +79,11 @@ namespace Detox.Editor.GUI
       }
 
       /// <summary>
-      /// String used as a place holder for trimmed text.
-      /// </summary>
-      public static readonly string EllipsisChars = "...";
-
-      private static Regex prevWord = new Regex(@"\W*\w*$");
-      private static Regex nextWord = new Regex(@"\w*\W*");
-
-      /// <summary>
       /// Truncates a text string to fit within a given control width by replacing trimmed text with ellipses.
       /// </summary>
       /// <param name="text">String to be trimmed.</param>
-      /// <param name="style">The GUIStyle that will be used for measurements</param>
-      /// <param name="maxSize">The Rect area that the string must fit inside.</param>
+      /// <param name="style">The GUIStyle that will be used for measurements.</param>
+      /// <param name="maxSize">The area that the string must fit inside.</param>
       /// <param name="options">Format and alignment of ellipsis.</param>
       /// <returns>This function returns text trimmed to the specified width.</returns>
       public static string Compact(string text, GUIStyle style, Rect maxSize, Format options)
@@ -95,9 +112,9 @@ namespace Detox.Editor.GUI
             return text;
          }
 
-         string pre = "";
+         string pre = string.Empty;
          string mid = text;
-         string post = "";
+         string post = string.Empty;
 
          bool isPath = (Format.Path & options) != 0;
 
@@ -105,13 +122,20 @@ namespace Detox.Editor.GUI
          if (isPath)
          {
             pre = Path.GetPathRoot(text);
-            mid = Path.GetDirectoryName(text).Substring(pre.Length);
+            
+            string directoryName = Path.GetDirectoryName(text);
+            if (directoryName != null)
+            {
+               mid = directoryName.Substring(pre.Length);
+            }
+
             post = Path.GetFileName(text);
+            Debug.Assert(post != null, "post is null");
          }
 
          int len = 0;
          int seg = mid.Length;
-         string fit = "";
+         string fit = string.Empty;
 
          // find the longest string that fits into
          // the control boundaries using bisection method
@@ -143,12 +167,12 @@ namespace Detox.Editor.GUI
             {
                if ((Format.End & options) != 0)
                {
-                  left -= prevWord.Match(mid, 0, left).Length;
+                  left -= PrevWord.Match(mid, 0, left).Length;
                }
 
                if ((Format.Start & options) != 0)
                {
-                  right += nextWord.Match(mid, right).Length;
+                  right += NextWord.Match(mid, right).Length;
                }
             }
 
@@ -160,6 +184,7 @@ namespace Detox.Editor.GUI
             {
                tst = Path.Combine(Path.Combine(pre, tst), post);
             }
+
             s = style.CalcSize(new GUIContent(tst));
 
             // candidate string fits into control boundaries, try a longer string
@@ -171,7 +196,8 @@ namespace Detox.Editor.GUI
             }
          }
 
-         if (len == 0) // string can't fit into control
+         // string can't fit into control
+         if (len == 0)
          {
             // "path" mode is off, just return ellipsis characters
             if (!isPath)
@@ -196,6 +222,7 @@ namespace Detox.Editor.GUI
                fit = Path.Combine(EllipsisChars, post);
             }
          }
+
          return fit;
       }
    }
