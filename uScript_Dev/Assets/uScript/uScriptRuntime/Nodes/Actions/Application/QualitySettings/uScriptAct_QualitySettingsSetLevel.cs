dccb78a95480d2ff3b0633ec7d7456faab6464af
Scott Blinn <scott@detoxstudios.com>
2013-04-24 03:19:15 +0000

    Fixed the description text for the Set Quality Level node.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@2317 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Application/QualitySettings/uScriptAct_QualitySettingsSetLevel.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Application/QualitySettings/uScriptAct_QualitySettingsSetLevel.cs
@@ -17,10 +17,10 @@ public class uScriptAct_QualitySettingsSetLevel : uScriptLogic
    public bool Out { get { return true; } }
 
    public void In(
-      [FriendlyName("Quality Level", "Returns the names of the available Quality Settings as a string list. When building, quality levels that are not used for that platform are stripped. You should not expect a given quality setting to be at a given index. It is best to query the available quality settings first using by using the Get Index From Quality Name node and using the returned index.")]
+      [FriendlyName("Quality Level", "The quality index to set.")]
       int qualityLevel,
 
-      [FriendlyName("Should expensive changes be applied (Anti-aliasing, etc.). Applying some changes the quality level can be an expensive operation if the new level has a different anti-aliasing setting. It's fine to change the level when applying in-game quality options, but if you want to dynamically adjust quality level at runtime, it is best to have this unchecked so that expensive changes are not always applied.")]
+      [FriendlyName("Apply Expensive Changes", "Should expensive changes be applied (Anti-aliasing, etc.). Applying some changes the quality level can be an expensive operation if the new level has a different anti-aliasing setting. It's fine to change the level when applying in-game quality options, but if you want to dynamically adjust quality level at runtime, it is best to have this unchecked so that expensive changes are not always applied.")]
       [SocketState(false, false)]
       [DefaultValue(false)]
       bool applyExpensiveChanges
