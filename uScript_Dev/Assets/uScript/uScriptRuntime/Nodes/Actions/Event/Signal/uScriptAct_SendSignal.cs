92097dec83a241723c474c899d406200f028e5c9
Trapper McFerron <trapper@detoxstudios.com>
2013-06-25 20:32:43 +0000

    Added Send/Receive signal nodes.. it uses uScript and .NET events (instead of Unity's Broadcast Message) for super fast script to script (or inner script) communication.
    See this post for reference:
    http://uscript.net/forum/viewtopic.php?f=10&t=2514&p=15572#p15572
    
    
    
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@2354 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Event/Signal/uScriptAct_SendSignal.cs
@@ -0,0 +1,46 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[NodePath("Signals")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Sends a signal to a Receive Signal node.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Send_Signal")]
+
+[FriendlyName("Send Signal", "Sends a signal to a Receive Signal node.")]
+public class uScriptAct_SendSignal : uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public delegate void uScriptSendSignalDelegate(GameObject objectToSignal, string name);
+
+   private GameObject m_Parent;
+
+   [FriendlyName("Send Signal")]
+   public void SendSignal(
+      [FriendlyName("GameObject", "The GameObject to signal (null for the current GameObject).")]
+      [SocketState(false, false)]
+      GameObject objectToSignal,
+      [FriendlyName("Signal Name", "The string-based signal name.")]
+      string signalName
+      )
+   {
+      if (null == objectToSignal)
+         objectToSignal = m_Parent;
+
+      uScript_ReceiveSignal receiver = objectToSignal.GetComponent<uScript_ReceiveSignal>();
+      
+      if (null != receiver)
+         receiver.OnIncomingSignal(objectToSignal, signalName);
+   }
+
+   public override void SetParent(GameObject parent)
+   {
+      m_Parent = parent;
+   }
+}
---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Event/Signal/uScript_ReceiveSignal.cs
@@ -0,0 +1,43 @@
+// uScript uScript_Collision.cs
+// (C) 2010 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Signals")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Receives a signal from a Send Signal node and continues execution.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#On_Receive_Signal")]
+
+[FriendlyName("Receive Signal", "Receives a signal from a Send Signal node and continues execution.")]
+public class uScript_ReceiveSignal : uScriptEvent
+{
+   public class ReceiveSignalEventArgs : System.EventArgs
+   {
+      [FriendlyName("Signal", "The name of the signal.")]
+      [SocketState(true, false)]
+      public string Signal{ get { return m_Signal; } }
+
+      private string m_Signal;
+
+      public ReceiveSignalEventArgs(string signal)
+      {
+         m_Signal = signal;
+      }
+   }
+
+   public delegate void uScriptEventHandler(object sender, ReceiveSignalEventArgs args);
+
+   private GameObject m_Parent;
+
+   [FriendlyName("Receive Signal")]
+   public event uScriptEventHandler OnReceiveSignal;
+
+   public void OnIncomingSignal(GameObject o, string name)
+   {
+      if (o.gameObject == gameObject) 
+         OnReceiveSignal(this, new ReceiveSignalEventArgs(name));
+   }
+}
