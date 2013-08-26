69b3088d045aea83dfd1424dcc5505b6e0a5ccf5
Michael Ryan <michael@detoxstudios.com>
2013-08-26 22:53:04 +0000

    Added the Compare Bool State conditional comparison node.
    It combines the logic of Compare Bool Equality, Compare Bool, and Set Bool nodes and provides "Out", "Is True", "Is False", "Was True", and "Was False" output sockets.  It's effectively an enhanced Compare Bool node that can send out signals when the state of its target bool changes.
    
    
    
    
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@2417 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Conditions/Comparison/uScriptCon_CompareBoolState.cs
@@ -0,0 +1,75 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="Detox Studios, LLC" file="uScriptCon_CompareBoolState.cs">
+//   Copyright 2010-2013 Detox Studios, LLC. All rights reserved.
+// </copyright>
+// <summary>
+//   Compare Bool State - uScript Conditional Node
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+[NodePath("Conditions/Comparison")]
+
+[NodeCopyright("Copyright 2013 by Detox Studios LLC")]
+[NodeToolTip("Examines the target boolean variable and fires the appropriate output link depending on how it compares to its previous state.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Compare_Bool_State")]
+
+[FriendlyName("Compare Bool State", "Examines the target boolean variable and fires the appropriate output link depending on how it compares to its previous state.")]
+public class uScriptCon_CompareBoolState : uScriptLogic
+{
+   // ================================================================================
+   //    Output Sockets
+   // ================================================================================
+
+   public bool Out { get; private set; }
+
+   [FriendlyName("Is True", "Fired only if the variable is true.")]
+   public bool IsTrue { get; private set; }
+
+   [FriendlyName("Is False", "Fired only if the variable is to false.")]
+   public bool IsFalse { get; private set; }
+
+   [FriendlyName("Was True", "Fired only if the variable was formerly true.")]
+   public bool WasTrue { get; private set; }
+
+   [FriendlyName("Was False", "Fired only if the variable was formerly false.")]
+   public bool WasFalse { get; private set; }
+
+   // ================================================================================
+   //    Input Sockets and Node Parameters
+   // ================================================================================
+
+   public void In(
+      [FriendlyName("Bool", "The boolean value to examine.")]
+      bool target,
+      [FriendlyName("Previous State", "The previous bool state and initial value.")]
+      [SocketState(false, false)]
+      ref bool previousState)
+   {
+      this.Out = true;
+
+      this.WasTrue = false;
+      this.WasFalse = false;
+
+      if (previousState != target)
+      {
+         if (previousState)
+         {
+            this.WasTrue = true;
+         }
+         else
+         {
+            this.WasFalse = true;
+         }
+
+         previousState = target;
+      }
+
+      this.IsTrue = target;
+      this.IsFalse = !target;
+   }
+
+   // ================================================================================
+   //    Miscellaneous Node Functionality
+   // ================================================================================
+}
