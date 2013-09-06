2ca25271484bf64da9ad0b23ee436167b53362e0
Trapper McFerron <trapper@detoxstudios.com>
2013-09-06 16:50:59 +0000

    Added GetNavMeshHit Properties node
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@2433 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Physics/uScriptAct_GetNavMeshHitProperties.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/Physics")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Gets the properties of a NavMeshHit structure.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Get_NavMeshHit_Properties")]
+
+[FriendlyName("Get NavMeshHit Properties", "Gets the properties of a NavMeshHit structure.")]
+public class uScriptAct_GetNavMeshHitProperties : uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public void In(
+      [FriendlyName("NavMeshHit", "The input NavMeshHit to get the properties of.")]
+      NavMeshHit navMeshHit,
+      [FriendlyName("Distance", "Distance to the point of hit.")]
+      out float distance,
+      [FriendlyName("Hit", "Flag set when hit.")]
+      out bool hit,
+      [FriendlyName("Mask", "Mask specifying NavMeshLayers at point of hit.")]
+      out int mask,
+      [FriendlyName("Normal", "Normal at the point of hit.")]
+      out Vector3 normal,
+      [FriendlyName("Position", "Position of hit.")]
+      out Vector3 position
+      )
+   {
+      distance = navMeshHit.distance;
+      hit = navMeshHit.hit;
+      mask = navMeshHit.mask;
+      normal = navMeshHit.normal;
+      position = navMeshHit.position;      
+   }
+}
