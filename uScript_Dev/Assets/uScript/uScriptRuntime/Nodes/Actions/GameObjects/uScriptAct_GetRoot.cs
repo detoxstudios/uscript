5273468e11aa5928e382bda073301dbe6d5e1a8e
Anthony Silva <anthony@detoxstudios.com>
2013-10-16 15:01:56 +0000

    Add GetRoot node (from andus).
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@2444 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/GameObjects/uScriptAct_GetRoot.cs
@@ -0,0 +1,38 @@
+// uScript Action Node
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[NodePath("Actions/GameObjects")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Returns the Root GameObject of a hierarchy of GameObjects.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://docs.unity3d.com/Documentation/ScriptReference/Transform-root.html")]
+
+[FriendlyName("Get Root", "Returns the Root GameObject of a hierarchy of GameObjects.")]
+public class uScriptAct_GetRoot : uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public void In (
+      [FriendlyName("Target", "The child GameObject.")]
+      GameObject Target,
+      
+      [FriendlyName("Root", "The Root GameObject of Target.")]
+      out GameObject objRoot
+      )
+   {
+      
+      if ( null != Target.transform.root )
+      {
+         objRoot = Target.transform.root.gameObject;
+      }
+      else
+      {
+         uScriptDebug.Log ("(Node - Get Root): The specified Target GameObject does not have a root (was null).", uScriptDebug.Type.Warning);
+         objRoot = null;
+      }
+   }
+}
