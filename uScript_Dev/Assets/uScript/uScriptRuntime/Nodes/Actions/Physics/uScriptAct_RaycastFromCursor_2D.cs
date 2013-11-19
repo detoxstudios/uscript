f515ed3f5862adaee2c022317ede4acf834f5ada
Scott Blinn <scott@detoxstudios.com>
2013-11-19 04:06:23 +0000

    Added official versions of the 2D physics nodes from the forum contribution. I needed to clean up the text a bit and put in a #if statement so the would only compile under Unity 4.3 or higher.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@2458 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Physics/uScriptAct_RaycastFromCursor_2D.cs
@@ -0,0 +1,83 @@
+// uScript Action Node
+// (C) 2013 Detox Studios LLC
+#if !UNITY_3_5 && !UNITY_4_0 && !UNITY_4_1 && !UNITY_4_2
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/Physics")]
+
+[NodeCopyright("Copyright 2013 by Detox Studios LLC")]
+[NodeToolTip("Cast a ray from the Mouse Cursor into the scene.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
+
+[FriendlyName("2D Raycast From Mouse Cursor", "Cast a 2D ray from the Mouse Cursor into the scene, determines if anything was hit along the way, and fires the associated output link. Used with Unity's 2D Physics system.")]
+public class uScriptAct_RaycastFromCursor_2D : uScriptLogic
+{
+   private bool m_NotObstructed = false;
+   private bool m_Obstructed = false;
+
+   public bool NotObstructed { get { return m_NotObstructed; } }
+   public bool Obstructed { get { return m_Obstructed; } }
+
+   public void In(
+      [FriendlyName("Camera", "The Camera GameObject to cast the ray from.")]
+      Camera Camera,
+
+      [FriendlyName("Distance", "How far out to cast the ray.")]
+      [DefaultValue(100f)]
+      float Distance,
+
+      [FriendlyName("Layer Mask", "A Layer mask that is used to selectively ignore colliders when casting a ray.")]
+      [SocketState(false, false)]
+      LayerMask layerMask,
+
+      [FriendlyName("Include Masked Layers", "If true, the ray will test against the masked layers, otherwise it will test against all layers excluding the masked layers.")]
+      [DefaultValue(true), SocketState(false, false)]
+      bool include,
+      
+      [FriendlyName("Show Ray", "If true, the ray will be displayed as a line in the Scene view.")]
+      [SocketState(false, false)]
+      bool showRay,
+      
+      [FriendlyName("Hit GameObject", "The first GameObject that was hit by the raycast (if any).")]
+      out GameObject HitObject,
+      
+      [FriendlyName("Hit Location", "The position of the hit (if any).")]
+      out Vector2 HitLocation
+
+      )
+   {
+      bool hitTrue = false;
+      Vector2 tmpHitLocation = Vector3.zero;
+      GameObject tmpHitObject = null;
+
+      Ray ray = Camera.ScreenPointToRay(Input.mousePosition);
+
+      if (Distance <= 0) Distance = Mathf.Infinity;
+      float castDistance = Distance;
+      RaycastHit2D hit;
+      
+      if (!include) layerMask = ~layerMask;
+
+      if (true == showRay)
+      {
+         Debug.DrawLine(ray.origin, ray.origin + (ray.direction * castDistance));
+      }
+		hit = Physics2D.GetRayIntersection(ray, castDistance, layerMask);
+      if (hit)
+      {
+         tmpHitLocation = hit.point;
+         tmpHitObject = hit.collider.gameObject;
+         hitTrue = true;
+      }
+     
+      HitLocation = tmpHitLocation;
+      HitObject = tmpHitObject;
+
+      m_Obstructed = hitTrue;
+      m_NotObstructed = !m_Obstructed;
+   }
+}
+#endif
