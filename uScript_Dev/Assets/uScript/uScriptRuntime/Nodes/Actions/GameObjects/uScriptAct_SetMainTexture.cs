394ba29af6b1f85ef9bcb3aee0d9bad156f8d7d9
Trapper McFerron <trapper@detoxstudios.com>
2014-01-06 16:25:58 +0000

    added node to set the main texture of a material
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@2498 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/GameObjects/uScriptAct_SetMainTexture.cs
@@ -0,0 +1,29 @@
+// uScript Action Node
+// (C) 2011 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/GameObjects")]
+
+[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeToolTip("Sets the main texture of a Material.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Set_Main_Texture")]
+
+[FriendlyName("Set Texture", "Applys a texture to the specified Material.")]
+public class uScriptAct_SetMainTexture : uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public void In(
+      [FriendlyName("Material", "The material to set the main texture.")]
+      Material material,
+
+      [FriendlyName("Texture", "The main texture to apply.")]
+      Texture texture
+      )
+   {
+      material.mainTexture = texture;
+   }
+}
