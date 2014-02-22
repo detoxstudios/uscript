331ff604c60385a841ad21f05b98c9d0b2196197
Scott Blinn <scott@detoxstudios.com>
2014-02-22 16:58:05 +0000

    New node - Set Sprite
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@2548 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Variables/uScriptAct_SetSprite.cs
@@ -0,0 +1,32 @@
+// uScript Action Node
+// (C) 2014 Detox Studios LLC
+
+#if !UNITY_3_5 && !UNITY_4_0 && !UNITY_4_1 && !UNITY_4_2
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/Variables/Sprite")]
+
+[NodeCopyright("Copyright 2014 by Detox Studios LLC")]
+[NodeToolTip("Sets a sprite variable to the defined value.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
+
+[FriendlyName("Set Sprite", "Sets a string to the defined value.")]
+public class uScriptAct_SetSprite : uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public void In(
+      [FriendlyName("Value", "The variable you wish to use to set the target's value.")]
+      Sprite Value,
+
+      [FriendlyName("Target", "The Target variable you wish to set.")]
+      out Sprite Target
+      )
+   {
+		Target = Value;
+   }
+}
+
+#endif
