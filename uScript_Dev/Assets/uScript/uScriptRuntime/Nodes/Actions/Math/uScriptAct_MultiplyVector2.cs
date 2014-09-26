1cf47aac679f71b9fe74a7300d5968c2abf34567
Anthony Silva <anthony@detoxstudios.com>
2014-09-26 14:51:35 +0000

    Add Vector2 nodes.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@2622 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Math/uScriptAct_MultiplyVector2.cs
@@ -0,0 +1,51 @@
+// uScript Action Node
+// (C) 2014 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/Math/Vectors")]
+
+[NodeCopyright("Copyright 2014 by Detox Studios LLC")]
+[NodeToolTip( "Multiplies two Vector2 variables together and returns the result.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Multiply_Vector2")]
+
+[FriendlyName("Multiply Vector2", "Multiplies Vector2 variables together and returns the result." +
+ "\n\n[ A + B ]" +
+ "\n\nIf more than one variable is connected to A, they will be multiplied together before being multiplied to B." +
+ "\n\nIf more than one variable is connected to B, they will be multiplied together before being multiplied to A.")]
+public class uScriptAct_MultiplyVector2 : uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public void In(
+      [FriendlyName("A", "The first variable or variable list.")]
+      Vector2[] A,
+      
+      [FriendlyName("B", "The second variable or variable list.")]
+      Vector2[] B,
+
+      [FriendlyName("Result", "The Vector2 result of the operation.")]
+      out Vector2 Result
+      )
+   {
+      Vector2 a = A[0];
+      Vector2 b = B[0];
+
+      for ( int i = 1; i < A.Length; i++ )
+      {
+         a.x = a.x * A[i].x;
+         a.y = a.y * A[i].y;
+      }
+
+      for ( int i = 1; i < B.Length; i++ )
+      {
+         b.x = b.x * B[i].x;
+         b.y = b.y * B[i].y;
+      }
+
+      Result.x = a.x * b.x;
+      Result.y = a.y * b.y;
+   }
+}
---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Math/uScriptAct_MultiplyVector2WithFloat.cs
@@ -0,0 +1,32 @@
+// uScript Action Node
+// (C) 2014 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/Math/Vectors")]
+
+[NodeCopyright("Copyright 2014 by Detox Studios LLC")]
+[NodeToolTip("Multiplies a Vector2 with a Float.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
+
+[FriendlyName("Multiply Vector2 With Float", "Multiplies a Vector2 with a Float. This is useful for multiplying things like Delta Time with a Vector2 velocity for example.")]
+public class uScriptAct_MultiplyVector2WithFloat : uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public void In(
+      [FriendlyName("Vector3", "The Vector2 you wish to multiple with.")]
+      Vector2 targetVector2,
+
+      [FriendlyName("Float", "The Float you wish to multiple with.")]
+      float targetFloat,
+
+      [FriendlyName("Result", "The Vector2 result of the operation.")]
+      out Vector2 Result
+      )
+   {
+      Result = targetVector2 * targetFloat;
+   }
+}
