f99350a3576d96b1cfea96b507bb52d35d98b512
Michael Ryan <michael@detoxstudios.com>
2015-01-14 22:35:16 +0000

    Removed unused Using statement, and untabified.
    + NOTE: the Unity 5 API references to target.loadState and AudioDataLoadState were not introduced until Unity 5 Beta 13. If you're running an earlier version of Unity 5, you should update to the latest beta.
    
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@2703 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Audio/uScriptAct_GetAudioClipInfo.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Audio/uScriptAct_GetAudioClipInfo.cs
@@ -2,7 +2,6 @@
 // (C) 2011 Detox Studios LLC
 
 using UnityEngine;
-using System.Collections;
 
 [NodePath("Actions/Audio")]
 
@@ -12,41 +11,39 @@ using System.Collections;
 [NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide")]
 
 [FriendlyName("Get AudioClip Info", "Returns AudioClip properties of the target AudioClip variable.")]
-public class uScriptAct_GetAudioClipInfo : uScriptLogic {
-	
-	public bool Out {get{return true;}}
-	
-	public void In (
-        [FriendlyName("Target", "The GameObject containing the animtion clip.")]
-        AudioClip target,
+public class uScriptAct_GetAudioClipInfo : uScriptLogic
+{
+   public bool Out { get { return true; } }
 
-        [FriendlyName("Length", "The length of the audio clip in seconds.")]
-        out float clipLength,
+   public void In(
+      [FriendlyName("Target", "The GameObject containing the animtion clip.")]
+      AudioClip target,
 
-        [FriendlyName("Samples", "The length of the audio clip in samples.")]
-        [SocketState(false, false)]
-        out float clipSamples,
+      [FriendlyName("Length", "The length of the audio clip in seconds.")]
+      out float clipLength,
 
-        [FriendlyName("Channels", "Returns the number of channels the audio clip has. 1 = Mono, 2+ = Stereo.")]
-        [SocketState(false, false)]
-        out int clipChannels,
-        
-        [FriendlyName("Frequency", "The frequency of the audio clip in Hz.")]
-        [SocketState(false, false)]
-        out float clipFrequency,
-        
-        [FriendlyName("Is Ready", "Returns true if the audio clip is ready to play. This is primarily used when downloading the audio clip from a web site and returns true when there is enough data downloaded to begin play without interuptions. This always returns true for audio clips not associated with a web stream.")]
-        [SocketState(false, false)]
-        out bool clipIsReady
-	   )
-	{
+      [FriendlyName("Samples", "The length of the audio clip in samples.")]
+      [SocketState(false, false)]
+      out float clipSamples,
 
-        clipLength = target.length;
-        clipSamples = target.samples;
-        clipChannels = target.channels;
-        clipFrequency = target.frequency;
+      [FriendlyName("Channels", "Returns the number of channels the audio clip has. 1 = Mono, 2+ = Stereo.")]
+      [SocketState(false, false)]
+      out int clipChannels,
+
+      [FriendlyName("Frequency", "The frequency of the audio clip in Hz.")]
+      [SocketState(false, false)]
+      out float clipFrequency,
+      
+      [FriendlyName("Is Ready", "Returns true if the audio clip is ready to play. This is primarily used when downloading the audio clip from a web site and returns true when there is enough data downloaded to begin play without interuptions. This always returns true for audio clips not associated with a web stream.")]
+      [SocketState(false, false)]
+      out bool clipIsReady)
+   {
+      clipLength = target.length;
+      clipSamples = target.samples;
+      clipChannels = target.channels;
+      clipFrequency = target.frequency;
 #if (UNITY_3_5 || UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6)
-        clipIsReady = target.isReadyToPlay;
+      clipIsReady = target.isReadyToPlay;
 #else
         if (target.loadState == AudioDataLoadState.Loaded)
         {
@@ -57,7 +54,5 @@ public class uScriptAct_GetAudioClipInfo : uScriptLogic {
            clipIsReady = false;
         }
 #endif
-
-	}
-	
+   }
 }
