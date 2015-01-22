3a92cf16e75545faedff3c398b7f15f50090dcb8
Michael Ryan <michael@detoxstudios.com>
2015-01-22 21:22:53 +0000

    Replaced StatusIcon.cs with HierarchyWindowIcon.cs.
    + Fixed an issue (at least on Unity 5) where a console error would appear when a scene containing a uScript component was closed. A null check is now made after InstanceIDToObject() to ensure the object still exists.
    
    + Added HierarchyWindowIcon.cs to the DLL project. It doesn't look like the StatusIcon script was referenced, so it may never have been compiled into the DLL. Since it's wasn't directly referenced by other code (uses events), its absence never generated any compile warnings/errors.
    
    + Cleaned it up a bit, decreasing the GUI draws and removed unused/unneeded code. It should be 'slightly' faster now.
    
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@2734 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptEditor/Editor/GUI/HierarchyWindowIcon.cs
@@ -0,0 +1,69 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="HierarchyWindowIcon.cs" company="Detox Studios, LLC">
+//   Copyright 2010-2015 Detox Studios, LLC. All rights reserved.
+// </copyright>
+// <summary>
+//   Defines the StatusIcons type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Detox.Editor.GUI
+{
+   using UnityEditor;
+
+   using UnityEngine;
+
+   [InitializeOnLoad]
+   internal static class HierarchyWindowIcon
+   {
+      private static readonly Texture2D ComponentIcon;
+
+      static HierarchyWindowIcon()
+      {
+         ComponentIcon = uScriptGUI.GetTexture("iconScriptFile02");
+         EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowItemOnGUI;
+      }
+
+      private static void HierarchyWindowItemOnGUI(int instanceID, Rect selectionRect)
+      {
+         if (EditorApplication.isPlayingOrWillChangePlaymode)
+         {
+            return;
+         }
+
+         if (HasValidComponent(instanceID))
+         {
+            DrawIcon(selectionRect);
+         }
+      }
+
+      private static void DrawIcon(Rect rect)
+      {
+         if (ComponentIcon == null)
+         {
+            return;
+         }
+
+         rect = GetRightAligned(rect, ComponentIcon);
+         GUI.Box(rect, uScriptGUIContent.Temp(ComponentIcon, "uScript attached"), GUIStyle.none);
+      }
+
+      private static Rect GetRightAligned(Rect rect, Texture texture)
+      {
+         rect.x = rect.xMax - texture.width;
+         rect.width = texture.width;
+         rect.y += (rect.height - texture.height) / 2f;
+         rect.height = texture.height;
+
+         return rect;
+      }
+
+      private static bool HasValidComponent(int instanceID)
+      {
+         var go = (GameObject)EditorUtility.InstanceIDToObject(instanceID);
+         return go != null
+                && (go.GetComponent<uScriptEvent>() != null || go.GetComponent<uScriptCode>() != null
+                    || go.GetComponent<uScript_MasterComponent>() != null);
+      }
+   }
+}
