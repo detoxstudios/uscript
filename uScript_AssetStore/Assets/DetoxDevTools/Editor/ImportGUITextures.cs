0f364c756d3ddfb0a65bfd14435cd0c64cc9637d
Scott Blinn <scott@detoxstudios.com>
2015-01-28 01:44:43 +0000

    Getting the Asset store build submission project under source control.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@2758 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_AssetStore/Assets/DetoxDevTools/Editor/ImportGUITextures.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+class ImportGUITextures : AssetPostprocessor
+{
+   void OnPreprocessTexture()
+   {
+
+      if (assetPath.Contains("_GUI"))
+      {
+         TextureImporter textureImporter = (TextureImporter)assetImporter;
+         TextureImporterSettings textureSettings = new TextureImporterSettings();
+
+         textureImporter.textureType = TextureImporterType.Advanced;
+         textureImporter.SetPlatformTextureSettings("Standalone", 512, TextureImporterFormat.AutomaticTruecolor);
+
+         textureImporter.npotScale = TextureImporterNPOTScale.None;
+         textureImporter.ReadTextureSettings(textureSettings);
+         textureSettings.mipmapEnabled = false;
+
+
+         textureSettings.aniso = 0;
+         textureSettings.filterMode = FilterMode.Point;
+         textureSettings.wrapMode = TextureWrapMode.Clamp;
+         textureSettings.maxTextureSize = 512;
+         textureSettings.textureFormat = TextureImporterFormat.AutomaticTruecolor;
+
+
+
+
+         textureImporter.SetTextureSettings(textureSettings);
+
+      }
+
+   }
+
+}
