5f4d1ab1423679b813fd6abba4546c6ca856105c
Scott Blinn <scott@detoxstudios.com>
2015-03-09 23:04:06 +0000

    Updated build name and number for RC17.
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@2830 b191eb17-2330-428c-a13f-159e02ee8e09

--- uScript_Dev/Assets/uScript/uScriptRuntime/Components/uScript_Runtime.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Components/uScript_Runtime.cs
@@ -30,8 +30,8 @@ public static class uScriptBuild
 
    // Version Name and Version Data
    // Set version - format is MAJOR.MINOR.FOUR-DIGIT-SVN-COMMIT-NUMBER
-   public static string Number { get { return "1.0.2819"; } }
-   private static string productNameAppendText = " (RC16)";
+   public static string Number { get { return "1.0.2830"; } }
+   private static string productNameAppendText = " (RC17)";
 
 
 #if DETOX_STORE_PLE
