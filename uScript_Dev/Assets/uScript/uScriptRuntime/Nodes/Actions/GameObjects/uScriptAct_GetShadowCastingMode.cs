153c1091ab6ac5a519be0a0eeadd38d4dc93f869
Anthony Silva <anthony@detoxstudios.com>
2015-03-19 03:44:26 +0000

    Add nodes for unity5's new shadow casting mode(s).
    git-svn-id: https://www.detoxstudios.com/projectdepot/Development/Unity@2872 b191eb17-2330-428c-a13f-159e02ee8e09

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/GameObjects/uScriptAct_GetShadowCastingMode.cs
@@ -0,0 +1,34 @@
+#if UNITY_5
+// uScript Action Node
+// (C) 2015 Detox Studios LLC
+
+using UnityEngine;
+
+[NodePath("Actions/GameObjects")]
+
+[NodeCopyright("Copyright 2015 by Detox Studios LLC")]
+[NodeToolTip("Gets the shadow casting mode of the specified GameObject's renderer.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Get_Shadow_Casting_Mode")]
+
+[FriendlyName("Get Shadow Casting Mode", "Gets the shadow casting mode of the specified GameObject's renderer.")]
+public class uScriptAct_GetShadowCastingMode : uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public void In(
+      [FriendlyName("Target", "The GameObject you wish to get the shadow casting mode of."), 
+       AutoLinkType(typeof(GameObject))]
+      GameObject Target,
+      
+      [FriendlyName("Mode", "The shadow casting mode of the specified GameObject's renderer.")]
+      out UnityEngine.Rendering.ShadowCastingMode mode
+   )
+   {
+      mode = UnityEngine.Rendering.ShadowCastingMode.On; // this is not the most efficient default, but it is what unity uses as the default
+
+      Renderer r = Target.GetComponent<Renderer>();
+      if (r != null) mode = r.shadowCastingMode;
+   }
+}
+#endif
---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/GameObjects/uScriptAct_SetShadowCastingMode.cs
@@ -0,0 +1,35 @@
+#if UNITY_5
+// uScript Action Node
+// (C) 2015 Detox Studios LLC
+
+using UnityEngine;
+
+[NodePath("Actions/GameObjects")]
+
+[NodeCopyright("Copyright 2015 by Detox Studios LLC")]
+[NodeToolTip("Sets the shadow casting mode of this GameObject's renderer.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Set_Shadow_Casting_Mode")]
+
+[FriendlyName("Set Shadow Casting Mode", "Sets the shadow casting mode of this GameObject's renderer.")]
+public class uScriptAct_SetShadowCastingMode : uScriptLogic
+{
+   public bool Out { get { return true; } }
+
+   public void In(
+      [FriendlyName("Target", "The GameObject(s) you wish to set the shadow casting mode of."), 
+       AutoLinkType(typeof(GameObject))]
+      GameObject[] Target,
+      
+      [FriendlyName("Mode", "The shadow casting mode to set this GameObject's renderer to.")]
+      UnityEngine.Rendering.ShadowCastingMode mode
+   )
+   {
+      foreach (GameObject go in Target)
+      {
+         Renderer r = go.GetComponent<Renderer>();
+         if (r != null) r.shadowCastingMode = mode;
+      }
+   }
+}
+#endif
