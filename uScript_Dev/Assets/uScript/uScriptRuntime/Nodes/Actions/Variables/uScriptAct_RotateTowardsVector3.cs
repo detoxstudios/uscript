f57c00f46a1bdc6216a91e6d18e843e704a0b4f7
Anthony Silva <anthony.silva@gmail.com>
2015-04-25 16:34:43 -0700

    Add a new node that wraps the Vector3.RotateTowards function.

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Variables/uScriptAct_RotateTowardsVector3.cs
@@ -0,0 +1,40 @@
+// uScript Action Node
+// (C) 2015 Detox Studios LLC
+
+using UnityEngine;
+using System.Collections;
+
+[NodePath("Actions/Variables/Vector3")]
+
+[NodeCopyright("Copyright 2015 by Detox Studios LLC")]
+[NodeToolTip("Wrapper around the Vector3.RotateTowards function.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://www.uscript.net/docs/index.php?title=Node_Reference_Guide#Vector3_Rotate_Towards")]
+
+[FriendlyName("Rotate Towards", "Rotate a Vector3 towards another Vector3.")]
+public class uScriptAct_RotateTowardsVector3 : uScriptLogic
+{
+   // How many outputs defined here
+   public bool Out { get { return true; } }
+
+   // Do logic here
+   public void In(
+      [FriendlyName("Current", "The current Vector3.")]
+      Vector3 current,
+      
+      [FriendlyName("Target", "The target Vector3.")]
+      Vector3 target,
+
+      [FriendlyName("Max Radian Delta", "The maximum rotation allowed in radians.")]
+      float maxRadiansDelta,
+      
+      [FriendlyName("Max Magnitude Delta", "The maximum magnitude in radians.")]
+      float maxMagnitudeDelta,
+
+      [FriendlyName("Result", "The resulting Vector3.")]
+      out Vector3 result
+      )
+   {
+      result = Vector3.RotateTowards(current, target, maxRadiansDelta, maxMagnitudeDelta);
+   }
+}
