7e3a6bfbe76894c7ed91eee65a376512e975aa7c
Detox Jenkins <jenkins@detoxstudios.com>
2015-04-30 07:59:03 -0700

    Jenkins build artifacts from build #2906

--- uScript_Dev/Assets/uScript/uScriptEditor/Editor/uScriptBuild.cs
+++ uScript_Dev/Assets/uScript/uScriptEditor/Editor/uScriptBuild.cs
@@ -19,8 +19,8 @@ public static class uScriptBuild
 
    // Version Name and Version Data
    // Set version - format is MAJOR.MINOR.FOUR-DIGIT-BUILD-NUMBER
-   public static string Number { get { return "1.0.2902"; } }
-   private static string productNameAppendText = " (RC22)";
+   public static string Number { get { return "1.0.2906"; } }
+   private static string productNameAppendText = " (test)";
 
 
 #if DETOX_STORE_PLE
