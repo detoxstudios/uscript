0fedd4c29acc51ee32a4e9260977a64ff6e5df1f
Detox Jenkins <jenkins@detoxstudios.com>
2015-06-18 00:42:36 -0700

    uScriptBuild.cs from Jenkins build #2936 (TestVersionSpecific).

--- uScript_Dev/Assets/uScript/uScriptEditor/Editor/uScriptBuild.cs
+++ uScript_Dev/Assets/uScript/uScriptEditor/Editor/uScriptBuild.cs
@@ -19,8 +19,8 @@ public static class uScriptBuild
 
    // Version Name and Version Data
    // Set version - format is MAJOR.MINOR.FOUR-DIGIT-BUILD-NUMBER
-   public static string Number { get { return "1.0.2934"; } }
-   private static string productNameAppendText = " (RC24)";
+   public static string Number { get { return "1.0.2936"; } }
+   private static string productNameAppendText = " (TestVersionS)";
 
 
 #if DETOX_STORE_PLE
