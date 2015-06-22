55a50f97c7238cad35c2bfb68d73f28934e9c892
Scott Blinn <scott@detoxstudios.com>
2015-06-22 15:45:04 -0400

    Updated the version checking file to use legit UNITY version defines

--- uScript_Dev/Assets/uScript/uScriptRuntime/uScriptUnityVersion.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/uScriptUnityVersion.cs
@@ -11,12 +11,20 @@ public class uScriptUnityVersion : uScriptIUnityVersion
 {
 #if UNITY_3_5
    public float Version { get { return 3.5f; } }
-#elif UNITY_4
+#elif UNITY_4_6
    public float Version { get { return 4.6f; } }
-#elif UNITY_5
+#elif UNITY_5_0
    public float Version { get { return 5.0f; } }
 #elif UNITY_5_1
    public float Version { get { return 5.1f; } }
+#elif UNITY_5_2
+   public float Version { get { return 5.2f; } }
+#elif UNITY_5_3
+   public float Version { get { return 5.3f; } }
+#elif UNITY_5_4
+   public float Version { get { return 5.4f; } }
+#elif UNITY_5_5
+   public float Version { get { return 5.5f; } }
 #else
    public float Version { get { return 0.0f; } }
 #endif
