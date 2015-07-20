bbe182938e646e8c2656170ee868df370683587e
Michael Ryan <michael@detoxstudios.com>
2015-07-20 16:30:47 -0700

    Added the JobManager class, which can be used to handle asynchronous events in a non-blocking manner.
    This should be used in the editor place of Unity Coroutines, since Coroutines only work in Play mode.

---
+++ uScript_Dev/Assets/uScript/uScriptEditor/Editor/JobManager.cs
@@ -0,0 +1,84 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="JobManager.cs" company="Detox Studios, LLC">
+//   Copyright 2010-2015 Detox Studios, LLC. All rights reserved.
+// </copyright>
+// <summary>
+//   Defines the JobManager type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+// REFERENCED:
+// ----------
+//    http://answers.unity3d.com/questions/221651/yielding-with-www-in-editor.html
+//
+// USAGE:
+// ~~~~~
+//    var www = new WWW("someURL");
+//    ContinuationManager.Add(() => www.isDone, () =>
+//    {
+//       if (!string.IsNullOrEmpty(www.error))
+//       {
+//          Debug.Log("WWW failed: " + www.error);
+//       }
+//       Debug.Log("WWW result : " + www.text);
+//    });
+
+namespace Detox.Editor
+{
+   using System;
+   using System.Collections.Generic;
+   using System.Linq;
+
+   using UnityEditor;
+
+   internal static class JobManager
+   {
+      private static readonly List<Job> Jobs = new List<Job>();
+
+      /// <summary>
+      /// The Job has a completed condition that when met will execute a final task.
+      /// </summary>
+      /// <param name="completed">The completion condition.</param>
+      /// <param name="continueWith">The function that will be executed upon completion.</param>
+      public static void Add(Func<bool> completed, Action continueWith)
+      {
+         if (Jobs.Any() == false)
+         {
+            EditorApplication.update += Update;
+         }
+
+         Jobs.Add(new Job(completed, continueWith));
+      }
+
+      private static void Update()
+      {
+         for (var i = 0; i >= 0; --i)
+         {
+            var job = Jobs[i];
+            if (job.Completed())
+            {
+               job.ContinueWith();
+               Jobs.RemoveAt(i);
+            }
+         }
+
+         if (Jobs.Any() == false)
+         {
+            EditorApplication.update -= Update;
+         }
+      }
+
+      private class Job
+      {
+         public Job(Func<bool> completed, Action continueWith)
+         {
+            this.Completed = completed;
+            this.ContinueWith = continueWith;
+         }
+
+         public Func<bool> Completed { get; private set; }
+
+         public Action ContinueWith { get; private set; }
+      }
+   }
+}
