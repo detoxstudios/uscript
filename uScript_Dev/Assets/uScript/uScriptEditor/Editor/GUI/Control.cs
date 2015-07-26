baa4970bd97a82bbf6a9dfc6d831a5d2abc68a37
Michael Ryan <mryan@somethingstrange.com>
2015-07-26 11:42:11 -0700

    Added Control and Property static classes for use with the Property panel and custom GUI controls.

---
+++ uScript_Dev/Assets/uScript/uScriptEditor/Editor/GUI/Control.cs
@@ -0,0 +1,15 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Control.cs" company="Detox Studios, LLC">
+//   Copyright 2010-2015 Detox Studios, LLC. All rights reserved.
+// </copyright>
+// <summary>
+//   Defines the Control type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Detox.Editor.GUI
+{
+   public static class Control
+   {
+   }
+}
---
+++ uScript_Dev/Assets/uScript/uScriptEditor/Editor/GUI/Property.cs
@@ -0,0 +1,15 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Property.cs" company="Detox Studios, LLC">
+//   Copyright 2010-2015 Detox Studios, LLC. All rights reserved.
+// </copyright>
+// <summary>
+//   Defines the Property type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Detox.Editor.GUI
+{
+   public static class Property
+   {
+   }
+}
