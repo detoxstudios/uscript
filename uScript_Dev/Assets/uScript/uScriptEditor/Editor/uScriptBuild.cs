66cc7f39969178ba6e179875f29faa3d431ae1f9
Detox Jenkins <jenkins@detoxstudios.com>
2015-08-21 00:02:02 -0700

    uScriptBuild.cs from Jenkins build #2983 ().

--- uScript_Dev/Assets/uScript/uScriptEditor/Editor/uScriptBuild.cs
+++ uScript_Dev/Assets/uScript/uScriptEditor/Editor/uScriptBuild.cs
@@ -19,7 +19,7 @@ public static class uScriptBuild
 
    // Version Name and Version Data
    // Set version - format is MAJOR.MINOR.FOUR-DIGIT-BUILD-NUMBER
-   public static string Number { get { return "1.0.2982"; } }
+   public static string Number { get { return "1.0.2983"; } }
    private static string productNameAppendText = "";
 
 
