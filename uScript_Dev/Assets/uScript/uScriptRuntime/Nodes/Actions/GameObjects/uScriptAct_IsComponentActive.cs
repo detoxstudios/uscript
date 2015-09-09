1016ff3eb7ff15e7634c74b220a8c747afc6ad79
Scott Blinn <scott@detoxstudios.com>
2015-09-09 16:56:29 -0400

    Revert "Some minor updates to the node I am about to revert (why am I reverting? Because Unity!)"
    This reverts commit 46c01333c5edbc1de4fcbaec76f7f93282f5a6a9.

--- uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/GameObjects/uScriptAct_IsComponentActive.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/GameObjects/uScriptAct_IsComponentActive.cs
@@ -9,82 +9,61 @@
 
 using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 
 [NodePath("Actions/GameObjects")]
 
 [NodeCopyright("Copyright 2011 by Detox Studios LLC")]
-[NodeToolTip("Fires out the correct out socket based on the active state of the specified Component.")]
+[NodeToolTip("Gets the active state of a GameObject.")]
 [NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
 [NodeHelp("http://docs.uscript.net/#3-Working_With_uScript/3.4-Nodes.htm")]
 
-[FriendlyName("Is Component Active", "Fires out the correct out socket based on the active state of the specified Component. If the specified Component is not on the Target, the 'Not Found' out socket will fire.")]
+[FriendlyName("Is Component Active", "Gets the active state of a Component.")]
 public class uScriptAct_IsComponentActive : uScriptLogic
 {
    private bool m_IsActive;
-   private bool m_IsNotActive;
-   private bool m_NotFound;
-
+   
    public bool Out { get { return true; } }
    
    public bool Active   { get { return m_IsActive; } }
-   public bool Inactive { get { return m_IsNotActive; } }
-   [FriendlyName("Not Found")]
-   public bool NotFound { get { return m_NotFound; } }
+   public bool Inactive { get { return !m_IsActive; } }
 
    public void In(
       [FriendlyName("Target", "GameObject which contains the component.")]
       GameObject Target,
-
-      [FriendlyName("Component Name", "Component type which to check. Please provide the actualy Unity name for the component.")]
+      [FriendlyName("Component", "Component type which to check.")]
       string component
       )
    {
+      Component comp = Target.GetComponent( component );
+
       m_IsActive = false;
-      m_IsNotActive = false;
-      m_NotFound = false;
-      component = component.ToLower(); // Convert everything to lowercase so casing doesn't matter.
-
-      if (null != Target)
+      
+      if (null != comp )
       {
-         // Get the list of all the active components on the Target, and populate a list with their names
-         Component[] _comps = Target.GetComponents(typeof(Component));
-
-         List<string> _compNameList = new List<string>();
-         foreach (Component tmpComp in _comps)
+         if (typeof(Behaviour).IsAssignableFrom(comp.GetType()))
          {
-            _compNameList.Add(tmpComp.GetType().ToString().Replace("UnityEngine.", "").ToLower());
+            m_IsActive = ((Behaviour)comp).enabled;
          }
-
-         // Check to see if the component is on the Target
-         if (!_compNameList.Contains(component))
+         else if (typeof(ParticleEmitter).IsAssignableFrom(comp.GetType()))
          {
-            m_NotFound = true;
+            m_IsActive = ((ParticleEmitter)comp).enabled;
+         }
+         else if (typeof(Collider).IsAssignableFrom(comp.GetType()))
+         {
+            m_IsActive = ((Collider)comp).enabled;
+         }
+         else if (typeof(Collider).IsAssignableFrom(comp.GetType()))
+         {
+            m_IsActive = ((Collider)comp).enabled;
+         }
+         else if (typeof(MeshRenderer).IsAssignableFrom(comp.GetType()))
+         {
+            m_IsActive = ((MeshRenderer)comp).enabled;
          }
          else
          {
-            // Component was found, now let's see if it is active or not and fire out the correct out socket
-            bool _isActive = false;
-            int _compIndex = _compNameList.FindIndex(pred => pred.Contains(component));
-
-            if (typeof(Behaviour).IsAssignableFrom(_comps[_compIndex].GetType()))
-            {
-               _isActive = ((Behaviour)_comps[_compIndex]).enabled;
-            }
-
-            if (_isActive)
-            {
-               m_IsActive = true;
-            }
-            else
-            {
-               m_IsNotActive = true;
-            }
+             uScriptDebug.Log("Unrecognized component type: " + component, uScriptDebug.Type.Error);
          }
-      }
-      else
-      {
-         uScriptDebug.Log("The Target specified was NULL. Components could not be checked.", uScriptDebug.Type.Warning);
       }
    }
 }
