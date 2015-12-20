e284b583b12b44c8d8908c5ae2fca121c465e0d1
Scott Blinn <scott@detoxstudios.com>
2015-12-19 20:23:02 -0500

    * added conditional compile for the joint mode (removed as of 5.3) to avoid compile error of uScript under Unity 5.3.

--- uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Physics/uScriptAct_SetJointDrive.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Physics/uScriptAct_SetJointDrive.cs
@@ -1,12 +1,12 @@
 // uScript Action Node
-// (C) 2011 Detox Studios LLC
+// (C) 2015 Detox Studios LLC
 
 using UnityEngine;
 using System.Collections;
 
 [NodePath("Actions/Physics")]
 
-[NodeCopyright("Copyright 2011 by Detox Studios LLC")]
+[NodeCopyright("Copyright 2015 by Detox Studios LLC")]
 [NodeToolTip("Sets the components of a JointDrive structure.")]
 [NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
 [NodeHelp("http://docs.uscript.net/#3-Working_With_uScript/3.4-Nodes.htm")]
@@ -17,28 +17,30 @@ public class uScriptAct_SetJointDrive : uScriptLogic
    public bool Out { get { return true; } }
 
    public void In(
+#if (UNITY_3_5 || UNITY_4_6 || UNITY_4_7 || UNITY_5_0 || UNITY_5_1 || UNITY_5_2)
       [FriendlyName("Mode", "The JointDriveMode.")]
       JointDriveMode mode,
-      
+#endif
       [FriendlyName("Position Spring", "The JointDrive PositionSpring.")]
       float positionSpring,
-      
+
       [FriendlyName("Position Damper", "The PositionDamper of the JointDrive.")]
       float positionDamper,
 
       [FriendlyName("Maximum Force", "The MaximumForce of the JointDrive.")]
       float maximumForce,
-      
+
       [FriendlyName("Joint Drive", "The newly created JointDrive.")]
       out JointDrive jointDrive
       )
    {
-      JointDrive j = new JointDrive( );
-      
+      JointDrive j = new JointDrive();
+#if (UNITY_3_5 || UNITY_4_6 || UNITY_4_7 || UNITY_5_0 || UNITY_5_1 || UNITY_5_2)
       j.mode = mode;
+#endif
       j.positionSpring = positionSpring;
       j.positionDamper = positionDamper;
-      j.maximumForce   = maximumForce;
+      j.maximumForce = maximumForce;
 
       jointDrive = j;
    }
