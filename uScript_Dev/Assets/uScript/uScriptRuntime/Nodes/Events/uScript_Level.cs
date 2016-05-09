fc667ff8e664713be099d1600b10d4f6fb42c14b
Trapper McFerron <trapzz@trapzz.com>
2016-05-08 21:17:43 -0500

    Add support for Unity 5.4

--- uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Events/uScript_Level.cs
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Events/uScript_Level.cs
@@ -4,6 +4,10 @@
 using UnityEngine;
 using System.Collections;
 
+#if !(UNITY_3_5 || UNITY_4_6 || UNITY_4_7 || UNITY_5_0 || UNITY_5_1 || UNITY_5_2 || UNITY_5_3)
+using UnityEngine.SceneManagement;
+#endif
+
 [NodeAutoAssignMasterInstance(true)]
 
 [NodePath("Events/Game Events")]
@@ -14,6 +18,7 @@ using System.Collections;
 [NodeHelp("http://docs.uscript.net/#3-Working_With_uScript/3.4-Nodes.htm")]
 
 [FriendlyName("Level Load", "Fires an event signal when a level is finished loading.")]
+
 #if (UNITY_3_5 || UNITY_4_6 || UNITY_4_7 || UNITY_5_0 || UNITY_5_1 || UNITY_5_2 || UNITY_5_3)
 public class uScript_Level : uScriptEvent
 {
@@ -41,21 +46,42 @@ public class uScript_Level : uScriptEvent
    }
 }
 #else
+
 public class uScript_Level : uScriptEvent
 {
    public delegate void uScriptEventHandler(object sender, System.EventArgs args);
 
-   void Start( )
-   {
-      UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
-   }
-
    [FriendlyName("On Level Was Loaded")]
    public event uScriptEventHandler LevelWasLoaded;
 
-   void OnSceneLoaded()
+   public class LevelWasLoadedEventArgs : System.EventArgs
    {
-      if ( LevelWasLoaded != null ) LevelWasLoaded(this, System.EventArgs.Empty);
+      private Scene m_Scene;
+      private LoadSceneMode m_Mode;
+      
+      [FriendlyName("Scene", "The Scene which was loaded.")]
+      public Scene Scene { get { return m_Scene; } }
+
+      [FriendlyName("Load Scene Mode", "How the scene was loaded; Additive or Single.")]
+      public LoadSceneMode LoadSceneMode{ get { return m_Mode; } }
+
+      public LevelWasLoadedEventArgs(Scene scene, LoadSceneMode mode)
+      {
+         m_Scene = scene;
+         m_Mode = mode;
+      }
    }
+
+   void Start( )
+   {
+      SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+   }
+
+   private void SceneManager_sceneLoaded( UnityEngine.SceneManagement.Scene arg0, UnityEngine.SceneManagement.LoadSceneMode arg1 )
+   {
+      if ( LevelWasLoaded != null )
+         LevelWasLoaded( this, new LevelWasLoadedEventArgs( arg0, arg1 ) );
+   }
+
 }
 #endif
