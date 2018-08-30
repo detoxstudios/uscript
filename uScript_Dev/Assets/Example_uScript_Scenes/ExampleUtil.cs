a6424bacccd914a0747fcb9e8a02a17903b0e65f
Anthony Silva <anthony@detoxstudios.com>
2018-08-29 21:40:48 -0700

    Add ExampleUtil to all example scenes which creates a uScript_MasterComponent.  Woot!

---
+++ uScript_Dev/Assets/Example_uScript_Scenes/ExampleUtil.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Detox
+{
+    public class ExampleUtil : MonoBehaviour
+    {
+        void Start()
+        {
+            // Add the master component if it doesn't exist yet
+            if (GameObject.FindObjectOfType<uScript_MasterComponent>() == null)
+            {
+                var go = GameObject.Find(uScriptRuntimeConfig.MasterObjectName);
+                if (go == null) go = new GameObject(uScriptRuntimeConfig.MasterObjectName);
+                go.AddComponent<uScript_MasterComponent>();
+            }
+        }
+    }
+}
