ba99a11550083f9bb7f362d1a8ce815fd56b3825
Detox Jenkins <jenkins@detoxstudios.com>
2018-12-10 23:27:19 -0700

    uScriptBuild.cs from Jenkins build #3110 ().

--- uScript_Dev/Assets/uScript/uScriptEditor/Editor/uScriptBuild.cs
+++ uScript_Dev/Assets/uScript/uScriptEditor/Editor/uScriptBuild.cs
@@ -19,12 +19,14 @@ public static class uScriptBuild
 
    // Version Name and Version Data
    // Set version - format is MAJOR.MINOR.FOUR-DIGIT-BUILD-NUMBER
-   public static string Number { get { return "1.0.3109"; } }
+   public static string Number { get { return "1.0.3110"; } }
    private static string productNameAppendText = "";
 
 
 #if DETOX_STORE_PLE || UNITY_STORE_PLE
    public static string Name { get { return "Personal Learning Edition" + productNameAppendText; } }
+#elif DETOX_STORE_BASIC || UNITY_STORE_BASIC
+   public static string Name { get { return "Basic Edition" + productNameAppendText; } }
 #else
    public static string Name { get { return "Professional Edition" + productNameAppendText; } }
 #endif
@@ -42,8 +44,14 @@ public static class uScriptBuild
 #if UNITY_STORE_PRO
    public const EditionType Edition = EditionType.Pro;
    public const SourceType Source = SourceType.Unity;
+#elif UNITY_STORE_BASIC
+   public const EditionType Edition = EditionType.Basic;
+   public const SourceType Source = SourceType.Unity;
 #elif DETOX_STORE_PRO
    public const EditionType Edition = EditionType.Pro;
+   public const SourceType Source = SourceType.Detox;
+#elif DETOX_STORE_BASIC
+   public const EditionType Edition = EditionType.Basic;
    public const SourceType Source = SourceType.Detox;
 #else
    public const EditionType Edition = EditionType.PLE;
