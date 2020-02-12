99d9a45d862652e9fe7485863d58dc5c315c889a
Anthony Silva <anthony@detoxstudios.com>
2020-02-12 11:52:05 -0700

    Add SetTransformPosition and FindAllFiles nodes.

---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/GameObjects/uScriptAct_SetTransformPosition.cs
@@ -0,0 +1,65 @@
+﻿// uScript Action Node
+// (C) 2020 Detox Studios LLC
+
+using UnityEngine;
+
+[NodePath("Actions/GameObjects/Movement")]
+
+[NodeCopyright("Copyright 2020 by Detox Studios LLC")]
+[NodeToolTip("Sets the position (Vector3) of a Transform as world or local coordinates.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://docs.uscript.net/#3-Working_With_uScript/3.4-Nodes.htm")]
+
+[FriendlyName("Set Position (Transform)", "Sets the position (Vector3) of a Transform as world or local coordinates.")]
+public class uScriptAct_SetTransformPosition : uScriptLogic
+{
+
+    public bool Out { get { return true; } }
+
+    public void In(
+      [FriendlyName("Target", "The Target Transform(s) to set the position of."), AutoLinkType(typeof(Transform))]
+      Transform[] Target,
+
+      [FriendlyName("Position", "The position to set the Target GameObjects to. If \"As Offset\" is enabled, this value will be used as an offest from the target's current position.")]
+      Vector3 Position,
+
+      [FriendlyName("As Offset", "Whether or not to use Position as an offset from the Target GameObjects' position(s).")]
+      [SocketState(false, false)]
+      bool AsOffset,
+
+      [FriendlyName("As Local", "Whether or not to set the local (instead of world) position(s) of the Target GameObjects'.")]
+      [SocketState(false, false)]
+      [DefaultValue(false)]
+      bool AsLocal
+      )
+    {
+       foreach (Transform currentTarget in Target)
+       {
+          if (currentTarget != null)
+          {
+             if (AsOffset)
+             {
+                if (AsLocal)
+                {
+                   currentTarget.localPosition += Position;
+                }
+                else
+                {
+                   currentTarget.position += Position;
+                }
+             }
+             else
+             {
+                if (AsLocal)
+                {
+                   currentTarget.localPosition = Position;
+                }
+                else
+                {
+                   currentTarget.position = Position;
+                }
+             }
+          }
+       }
+    }
+}
---
+++ uScript_Dev/Assets/uScript/uScriptRuntime/Nodes/Actions/Utilities/uScriptAct_FindAllFiles.cs
@@ -0,0 +1,59 @@
+﻿// uScript Action Node
+// (C) 2020 Detox Studios LLC
+
+using System.Collections.Generic;
+using System.IO;
+
+[NodePath("Actions/Utilities")]
+
+[NodeCopyright("Copyright 2020 by Detox Studios LLC")]
+[NodeToolTip("Recursively find files of type extension in root path.")]
+[NodeAuthor("Detox Studios LLC", "http://www.detoxstudios.com")]
+[NodeHelp("http://docs.uscript.net/#3-Working_With_uScript/3.4-Nodes.htm")]
+
+[FriendlyName("Find All Files", "Recursively find files of type extension in root path.")]
+public class uScriptAct_FindAllFiles: uScriptLogic
+{
+   int m_NumberCount = 0;
+
+   public bool Out { get { return true; } }
+
+   public void In(
+      [FriendlyName("Root Path", "The root directory to search in for files.")]
+      string rootPath,
+
+      [FriendlyName("Extension", "The extension to search for (must include the '.').")]
+      string extension,
+
+      [FriendlyName("File List", "List of file paths of found files.")]
+      out string[] fileList
+   )
+   {
+      fileList = _findAllFiles(rootPath, extension);
+   }
+
+   private string[] _findAllFiles(string rootPath, string extension)
+   {
+      List<string> paths = new List<string>();
+
+      DirectoryInfo directory = new DirectoryInfo(rootPath);
+
+      FileInfo[] files = directory.GetFiles();
+
+      foreach (FileInfo file in files)
+      {
+         if (file.Extension == extension)
+         {
+            paths.Add(file.FullName);
+         }
+      }
+
+      foreach (DirectoryInfo subDirectory in directory.GetDirectories())
+      {
+         string[] results = _findAllFiles(subDirectory.FullName, extension);
+         paths.AddRange(results);
+      }
+
+      return paths.ToArray();
+   }
+}
